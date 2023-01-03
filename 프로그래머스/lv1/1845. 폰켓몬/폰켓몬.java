719c72dbfcb9405db9ec898e6881346fb6a8e88f
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-03 12:37:53 +0900

    [level 1] Title: 폰켓몬, Time: 3.47 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -3,8 +3,9 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> set = new HashSet<>(); // HashSet을 이용하여 배열의 중복값 제거
         
+        // nums배열의 길이만큼 반복문 돌려서 중복제거한 값을 HashSet에 넣는다
         for(int i=0; i<nums.length; i++) {
             set.add(nums[i]);
         }
