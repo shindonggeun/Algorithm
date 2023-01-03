5c084f6c603fa14315a2c8eb8972758e082503cf
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-03 14:38:13 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 91.87 ms, Memory: 95 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,33 +1,26 @@
 import java.util.HashMap;
 
-
 class Solution {
-    public String solution(String[] participant, String[] completion) {  
-
+    public String solution(String[] participant, String[] completion) {
         String answer = "";
-        HashMap<String, Integer> answerMap = new HashMap<>();
-        for(int i=0; i<participant.length;i++) {
-            String name = participant[i];
-            if (answerMap.containsKey(name)) {
-                int count = (int)answerMap.get(name);
-                answerMap.put(name, ++count);                
-            } else {    
-                answerMap.put(name, 1);
+        HashMap<String, Integer> map = new HashMap<>();
+        // 참가자 map에 집어넣기
+        for (String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
+        }
+        //System.out.println(map);
+        // 완주자 map에 집어넣기 (value값 1씩 줄어듬) -> 완주자는 value값이 0임
+        for (String player : completion) {
+            map.put(player, map.get(player) - 1);
+        }
+        //System.out.println(map);
+        // map에 저장된 key값들 뽑기(반복문 이용)
+        for (String key : map.keySet()) {
+            // key값을 이용해서 value값을 뽑는데 0이 아닌경우(완주 못한 경우)
+            if (map.get(key) != 0){
+                answer = key;
             }
         }
-
-        for(int i=0; i<completion.length;i++) {            
-            int count = (int)answerMap.get(completion[i]);
-            String name = completion[i];
-            if (count == 1) {
-                answerMap.remove(name);
-            } else {                 
-                answerMap.put(name, --count); 
-            }  
-        }    
-        String temp = answerMap.keySet().toString();
-        answer = temp.substring(1, temp.length()-1);
-
         return answer;
     }
 }
