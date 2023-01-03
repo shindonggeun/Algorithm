66021ab9242193a0f66a598ff8ba36a126ccfde5
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-03 16:45:23 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -2,7 +2,6 @@ class Solution {
     public int solution(int[] common) {
         int answer = 0;
         int result = 0;
-        int length = common.length;
         
         // 등차수열인 경우
         if(common[1] - common[0] == common[2] - common[1]) {
