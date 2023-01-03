c40ffe214f617008f1f1a35a495dff93890b205e
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-03 12:36:09 +0900

    [level 1] Title: 폰켓몬, Time: 2.31 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -3,12 +3,21 @@ import java.util.*;
 class Solution {
     public int solution(int[] nums) {
         int answer = 0;
-        int[] resultArr = Arrays.stream(nums).distinct().toArray();
+        HashSet<Integer> set = new HashSet<>();
+        
+        for(int i=0; i<nums.length; i++) {
+            set.add(nums[i]);
+        }
+        answer = nums.length / 2;
+        if(set.size() < answer) {
+            answer = set.size();
+        }
+        /*int[] resultArr = Arrays.stream(nums).distinct().toArray();
         
         answer = nums.length / 2;
         if(resultArr.length < answer) {
             answer = resultArr.length;
-        }
+        }*/
         
         return answer;
     }
