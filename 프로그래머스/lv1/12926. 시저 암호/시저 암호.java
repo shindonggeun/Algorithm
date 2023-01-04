d4ad64023fe521e6a64c16c31052dcfd4ebcc4d2
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-04 14:24:13 +0900

    [level 1] Title: 시저 암호, Time: 33.94 ms, Memory: 99.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -5,12 +5,22 @@ class Solution {
         for(int i=0; i<s.length(); i++) {
             char ch = s.charAt(i);
             // 소문자인 경우
-            if(Character.isLowerCase(ch)) { 
-                ch = (char) ((ch - 'a' + n) % 26 + 'a');
+            if(Character.isLowerCase(ch)) {
+                if(ch+n > 'z') {
+                    ch = (char)(ch + n - 26);
+                }
+                else {
+                    ch = (char)(ch + n);
+                }
             }
             // 대문자인 경우
             else if(Character.isUpperCase(ch)) {
-                ch = (char) ((ch - 'A' + n) % 26 + 'A');
+                if(ch+n > 'Z') {
+                    ch = (char)(ch + n - 26);
+                }
+                else {
+                    ch = (char)(ch + n);
+                }
             }
             answer+=ch;
         }
