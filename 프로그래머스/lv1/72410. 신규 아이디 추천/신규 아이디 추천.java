209634c9dc4994d7a12041aa5c60fff7ea989b72
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-04 13:01:04 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.20 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -2,64 +2,58 @@ class Solution {
     public String solution(String new_id) {
         String answer = "";
         
-        for(int i=1; i<=7; i++){
-            if(i == 1) {
-                new_id = new_id.toLowerCase();
-            }
-            if(i == 2) {    // '-', '_', '.'를 제외한 모든 문자 제거
-                new_id = new_id.replaceAll("[^0-9a-z-_.]", "");
-            }
-            if(i == 3) {
-                if(new_id.length() - new_id.replace(String.valueOf('.'), "").length() >= 2) {
-                    new_id = new_id.replaceAll("[.]{2,}", ".");
-                }
-            }
-            if(i == 4) {
-                /*if(new_id.indexOf('.') == 0 ) {
-                    new_id = new_id.substring(1, new_id.length());
-                }
-                if(new_id.equals("") == false && new_id.lastIndexOf('.') == new_id.length() - 1) {
-                    //System.out.println("찾았다");
-                    new_id = new_id.substring(0, new_id.length() - 1);
-                }*/
-                
-                new_id = new_id.replaceAll("^[.]", "");
-                new_id = new_id.replaceAll("[.]$", "");
-            }
-            if(i == 5) {
-                if(new_id.equals("")) {
-                    new_id = "a";
-                }
-            }
-            if(i == 6) {
-                if(new_id.length() >= 16) {
-                    new_id = new_id.substring(0, 15).replaceAll("[.]$","");;   // 0번 인덱스부터 15번 인덱스 전 까지 (즉 0 ~ 14 -> 15개 문자)
-                    /*if(new_id.lastIndexOf('.') == new_id.length() - 1) {
-                        new_id = new_id.replace(".", "");
-                    }*/
-                }
-            }
-            if(i == 7) {
-                if(new_id.length() <= 2) {
-                    char ch = new_id.charAt(new_id.length() - 1);
-                    while(new_id.length() != 3) {
-                        new_id+=ch;
-                    }
-                }
-            }
-        }
+        new_id = changeToLowerCase(new_id);
+        new_id = deleteCharater(new_id);
+        new_id = toSingleDot(new_id);
+        new_id = noStartEndDot(new_id);
+        new_id = noEmptyMakeA(new_id);
+        new_id = noLengthGraterThan16(new_id);
+        new_id = nolessThan2(new_id);
             
         //System.out.println(new_id);
         answer = new_id;
         return answer;
     }
     
+    // 1단계
     public String changeToLowerCase(String new_id) {
         return new_id.toLowerCase();
     }
-    
+    // 2단계 (알파벳 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.)를 제외한 모든 문자 제거)
     public String deleteCharater(String new_id) {
-        return new_id.replaceAll("[~!@#$%^&*()=+[{]}:?,<>/]", "");
+        return new_id.replaceAll("[^0-9a-z-_.]", "");
+    }
+    // 3단계
+    public String toSingleDot(String new_id) {
+        return new_id.replaceAll("[.]{2,}", ".");
+    }
+    // 4단계
+    public String noStartEndDot(String new_id) {
+        return new_id.replaceAll("^[.]|[.]$", "");
+    }
+    // 5단계
+    public String noEmptyMakeA(String new_id) {
+        if(new_id.equals("")) {
+            new_id = "a";
+        }
+        return new_id;
+    }
+    // 6단계
+    public String noLengthGraterThan16(String new_id) {
+        if(new_id.length() >= 16) {
+            new_id = new_id.substring(0, 15).replaceAll("[.]$","");
+        }
+        return new_id;
+    }
+    // 7단계
+    public String nolessThan2(String new_id) {
+        if(new_id.length() <= 2) {
+            char ch = new_id.charAt(new_id.length() - 1);
+            while(new_id.length() != 3) {
+                new_id+=ch;
+            }
+        }
+        return new_id;
     }
     
 }
