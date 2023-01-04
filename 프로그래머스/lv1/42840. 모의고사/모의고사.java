4f5ea90fb02e84d001debf0548ec82c75b41a9c9
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-04 16:45:54 +0900

    [level 1] Title: 모의고사, Time: 0.98 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -1,12 +1,13 @@
 import java.util.*;
 
 class Solution {
-    public ArrayList<Integer> solution(int[] answers) {
-        //int[] answer = {};
+    public int[] solution(int[] answers) {
+        int[] answer = {};
         int[] one = {1, 2, 3, 4, 5};                     // 5개의 패턴(1번 수포자)
         int[] two = {2, 1, 2, 3, 2, 4, 2, 5};           // 8개의 패턴(2번 수포자)
         int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};   // 10개의 패턴(3번 수포자)
         int[] score = new int[3];
+        ArrayList<Integer> list = new ArrayList<>();
         
         for(int i=0; i<answers.length; i++) {
             if(answers[i] == one[i % 5]) {
@@ -20,10 +21,20 @@ class Solution {
             }
         }
         int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
-        ArrayList<Integer> list = new ArrayList<>();
-        if(maxScore == score[0]) {list.add(1);}
-        if(maxScore == score[1]) {list.add(2);}
-        if(maxScore == score[2]) {list.add(3);}
-        return list;
+        if(maxScore == score[0]) {
+            list.add(1);
+        }
+        if(maxScore == score[1]) {
+            list.add(2);
+        }
+        if(maxScore == score[2]) {
+            list.add(3);
+        }
+        
+        answer = new int[list.size()];
+        for(int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
     }
 }
