4f76742a44f0ea648ac57a715bba81f2f82671f4
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-04 15:24:06 +0900

    [level 1] Title: 3진법 뒤집기, Time: 1.72 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String thirdDigit = "";
+        
+        while(n != 0) {
+            int digit = n % 3;
+            thirdDigit+=Integer.toString(digit);
+            n = n /3;
+        }
+        
+        for(int i=0; i<thirdDigit.length(); i++) {
+            if(thirdDigit.charAt(i) != '0') {
+                answer+=((int)thirdDigit.charAt(i) - '0') * Math.pow(3, thirdDigit.length() - 1 - i);
+            }
+        }
+        
+        return answer;
+    }
+}
