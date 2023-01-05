f32ac961c9d55e906941297bc25559b3c940defc
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-05 17:02:05 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 17.91 ms, Memory: 69 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -3,24 +3,24 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         String answer = "";
-        //s = s.replace(" ", ",");
-        //System.out.println(s);
         s = s.toLowerCase();
     	String[] strArr = s.split(" ");
         
         for(String str: strArr) {
-            System.out.println(str);
-            // 빈 문자열인 경우 ()
+            // split해서 저장한 문자열 배열에서 각 인덱스에 있는 문자열값이 빈 문자열인 경우 -> ex) " for the last week " 
+            // 여기서 strArr[0] 값은 빈 문자열(null)이 들어간다. -> strArr[0] = "" -> 이때 문자열 길이는 0임!!(중요)
             if(str.equals("")) {
+                //System.out.println(str.length());
                 answer+=" ";
             }
+            // 문자열 배열에서 저장된 문자열값이 빈 문자열이 아닌 경우
             else {
-                answer+=Character.toUpperCase(str.charAt(0)) + str.substring(1, str.length());
-                answer+=" ";
+                answer+=Character.toUpperCase(str.charAt(0)) + str.substring(1, str.length());  // 첫 글자 대문자로 변환 + 문자열의 1번 인덱스부터 문자열의 길이 전 인덱스까지 추출한거 더해서 answer에 저장
+                answer+=" ";    // 그다음 다시 띄어쓰기 
             }
         }
         
-        //원래 문자열 마지막이 공백이 아닐 경우
+        //원래 문자열 마지막이 공백이 아닐 경우 answer에 저장된 문자열에서 공백 제거할 수 있도록 substring
         if(s.substring(s.length() -1, s.length()).equals(" ") == false) {
             answer = answer.substring(0, answer.length() - 1);
         }
--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/README.md
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] JadenCase 문자열 만들기 - 12951 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12951) 
 
 ### 성능 요약
 
-메모리: 80.5 MB, 시간: 12.68 ms
+메모리: 69 MB, 시간: 17.91 ms
 
 ### 구분
 
