bd9634380015e99242d4df9f686bba7b7a5b8079
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-05 17:32:36 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 16.66 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     public String solution(String s) {
         String answer = "";
@@ -7,6 +5,7 @@ class Solution {
     	String[] strArr = s.split(" ");
         
         for(String str: strArr) {
+            //System.out.println(str);
             // split해서 저장한 문자열 배열에서 각 인덱스에 있는 문자열값이 빈 문자열인 경우 -> ex) " for the last week " 
             // 여기서 strArr[0] 값은 빈 문자열(null)이 들어간다. -> strArr[0] = "" -> 이때 문자열 길이는 0임!!(중요)
             if(str.equals("")) {
@@ -15,13 +14,14 @@ class Solution {
             }
             // 문자열 배열에서 저장된 문자열값이 빈 문자열이 아닌 경우
             else {
-                answer+=Character.toUpperCase(str.charAt(0)) + str.substring(1, str.length());  // 첫 글자 대문자로 변환 + 문자열의 1번 인덱스부터 문자열의 길이 전 인덱스까지 추출한거 더해서 answer에 저장
+                // 첫 글자 대문자로 변환 + 문자열의 1번 인덱스부터 문자열의 길이 전 인덱스까지 추출한거 더해서 answer에 저장
+                answer+=Character.toUpperCase(str.charAt(0)) + str.substring(1, str.length());  
                 answer+=" ";    // 그다음 다시 띄어쓰기 
             }
         }
         
         //원래 문자열 마지막이 공백이 아닐 경우 answer에 저장된 문자열에서 공백 제거할 수 있도록 substring
-        if(s.substring(s.length() -1, s.length()).equals(" ") == false) {
+        if(s.substring(s.length() - 1, s.length()).equals(" ") == false) {
             answer = answer.substring(0, answer.length() - 1);
         }
         
