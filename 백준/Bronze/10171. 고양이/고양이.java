33da058d4b620b009d2dd1d51f2b515c5929bca7
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-11 15:16:35 +0900

    [Bronze V] Title: 고양이, Time: 120 ms, Memory: 13924 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,9 @@
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("\\" + "    /" + "\\");    // \\ -> \로 출력
+        System.out.println(" )  ( " + "\'" + ")");     // \' -> '으로 출력
+        System.out.println("(  " + "/" + "  ) ");
+        System.out.println(" \\" + "(__)| ");
+    }
+}
