3c4c16d58ea2ef616a53a0383df64101f2baaacb
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-12 15:24:56 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(st.nextToken());
+        
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=n; j++) {
+            	if(j <= n - i) {
+            		sb.append(" ");
+            	}
+            	else {
+            		sb.append("*");
+            	}
+            }
+            sb.append("\n");
+        }
+        
+        System.out.println(sb);
+        
+    }
+}
