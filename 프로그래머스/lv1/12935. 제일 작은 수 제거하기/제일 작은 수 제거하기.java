2bb6ff6e6f9ea225eaf6cc5be5644c993d780dd9
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-19 15:57:45 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.40 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -10,18 +10,16 @@ class Solution {
         }
         int min = list.get(0);
         
+        // ArrayList에서 최소값 찾는 과정
         for(int i=0; i<list.size(); i++)  {
             if(list.get(i) < min) {
                 min = list.get(i);
             }
         }
         
-        if(list.contains(min) && list.size() >= 2) {
-            // list의 min값에 해당하는 원소 제거
-            list.remove(Integer.valueOf(min));
-        }
-        else if(list.size() == 1) {
-            list.remove(list.size() - 1);
+        list.remove(Integer.valueOf(min));   // list의 min값에 해당하는 원소 제거
+        // 삭제하고 나서 list의 길이가 0이면 원소 -1 추가
+        if(list.size() == 0) {
             list.add(-1);
         }
         
