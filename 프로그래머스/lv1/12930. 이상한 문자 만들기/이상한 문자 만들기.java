c742d1ea96676e2a4622766b1398383603ea2155
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-19 15:26:32 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.25 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -2,9 +2,34 @@ class Solution {
     public String solution(String s) {
         String answer = "";
         
+        String[] str = s.split("");
+        int cnt = 0;
+
+        for(String ss : str) {
+            if(ss.contains(" ")) {
+                cnt = 0;
+            }
+            else {
+                cnt+=1;
+            }
+            
+            if(cnt%2 == 0) {
+                answer+=ss.toLowerCase();
+            }
+            else {
+                answer+=ss.toUpperCase();
+            }
+            //cnt = ss.contains(" ") ? 0 : cnt + 1;
+            //answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); 
+        }
+        
+        
+        // 밑에 방법은 이중 반복문 사용하므로 시간과 메모리 사용량이 크다
+        // split()에서 -1 인자를 넣었을 경우만 잘 알아두자!
+        
         // 공백문자로 문자열 쪼개기
         // 뒤에 -1 인자를 넣어야 문자열 맨뒤에 공백이 나와도 그것을 단어로 취급해서 잘라줌
-        String[] str = s.split(" ", -1);    
+        /*String[] str = s.split(" ", -1);    
                                    
         for(int i=0; i<str.length; i++) {
             for(int j=0; j<str[i].length(); j++) {
@@ -20,17 +45,9 @@ class Solution {
             if(i!=str.length-1) {
                 answer+=" ";
             }
-        }
+        }*/
         
         return answer;
         
-        /*int cnt = 0;
-        String[] array = s.split("");
-
-        for(String ss : array) {
-            cnt = ss.contains(" ") ? 0 : cnt + 1;
-            answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); 
-        }
-        return answer;*/
     }
 }
