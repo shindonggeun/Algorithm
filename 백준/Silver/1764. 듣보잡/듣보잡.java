2913094ff9ae9d33b44201b6f155289741f57dd6
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-24 17:32:31 +0900

    [Silver IV] Title: 듣보잡, Time: 400 ms, Memory: 33352 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -10,7 +10,7 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		Map<String, Integer> map = new HashMap<>();	// key: 사람 이름, value: 명단에 몇번 불렸는지(듣도 못한 사람 명단, 보도 못한 사람)
-		
+		List<String> list = new ArrayList<>();	// 명단에 2번이상 불린 사람 리스트
 		
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -22,9 +22,22 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			String s = st.nextToken();
 			map.put(s, map.getOrDefault(s, 0) + 1);
+			if(map.get(s) == 2) {
+				list.add(s);
+			}
 		}
 		
-		List<String> keySet = new ArrayList<>(map.keySet());
+		Collections.sort(list);	// 명단 2번이상 불린사람 리스트 오름차순 정렬
+		StringBuilder sb = new StringBuilder();
+		sb.append(list.size()).append("\n");
+		
+		for(String str : list) {
+			sb.append(str).append("\n");
+		}
+		
+		System.out.println(sb);
+		
+		/*List<String> keySet = new ArrayList<>(map.keySet());
 		Collections.sort(keySet);	// ArrayList 오름차순 정렬
 		StringBuilder sb = new StringBuilder();
 		int count = 0;
@@ -36,7 +49,7 @@ public class Main {
 			}
 		}
 		System.out.println(count);
-		System.out.println(sb);
+		System.out.println(sb);*/
 		
 		//System.out.println(map);
 		
