2a3bef023ca50a5b30d3af468af7b66ffc0c7372
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-01-25 14:52:42 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.52 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -5,15 +5,18 @@ class Solution {
         int answer = 0;
         List<Integer> list = new ArrayList<>();
         
+        // 0 ~ 9까지 list에 숫자 추가
         for(int i=0; i<=9; i++) {
             list.add(i);
         }
         
-        Arrays.sort(numbers);
+        Arrays.sort(numbers);   // 리스트 정렬
         
         for(int num: numbers) {
+            // 리스트에 해당 숫자가 있으면
             if(list.contains(num)) {
-                list.remove(Integer.valueOf(num));
+                // Integer.valueOf 안쓰고 그냥 num만 쓰면 num에 해당하는 인덱스값 삭제
+                list.remove(Integer.valueOf(num));  // 리스트에 숫자에 해당하는 값 삭제 
             }
         }
         
