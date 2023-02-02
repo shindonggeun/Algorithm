9c9b251d595dd6f5115766a4a78be30004f24973
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-02-02 18:20:34 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 852 ms, Memory: 58924 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static class Coordinate {
+		int x = 0;
+		int y = 0;
+		
+		public Coordinate(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		List<Coordinate> xyList = new ArrayList<>();
+		
+		for(int i=0; i<N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			
+			xyList.add(new Coordinate(x, y));
+		}
+		
+		Collections.sort(xyList, new Comparator<Coordinate>() {
+
+			@Override
+			public int compare(Coordinate c1, Coordinate c2) {
+				// y좌표가 같으면 x좌표 오름차순 정렬
+				if(c1.y == c2.y) {
+					return c1.x - c2.x;
+				}
+				// y좌표가 같지 않으면 y좌표 증가하는 순으로(y좌표 오름차순 정렬)
+				else {
+					return c1.y - c2.y;
+				}
+				
+			}
+
+		});
+		
+		StringBuilder sb = new StringBuilder();
+		for(Coordinate coor: xyList) {
+			sb.append(coor.x + " " + coor.y).append("\n");
+		}
+		System.out.print(sb);
+	}
+
+}
