e91f60d95be59924d0eaff76a4d7709a916ce861
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-02-03 15:47:31 +0900

    [unrated] Title: 둘만의 암호, Time: 13.00 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -3,22 +3,26 @@ import java.util.*;
 class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
-        Set<Character> set = new HashSet<>();
+        Set<Character> set = new HashSet<>();   // 중복 X, 검색 빠름
         
+        // 건너뛰어야하는 알파벳들 HashSet에 집어넣기 
         for(int i=0; i<skip.length(); i++) {
             set.add(skip.charAt(i));
         }
         
         for (int i = 0; i<s.length(); i++) {
-            char ch = s.charAt(i);
+            char ch = s.charAt(i); // 단어에서 각 자리의 알파벳 뽑기
             
+            // 알파벳들 인덱스만큼 증가
             for (int j=0; j<index; j++) {
-                ch++;
+                ch++;   
+                // 알파벳이 z를 넘어가면 a로 바꿔줌
                 if (ch > 'z') {
                     ch = 'a';
                 }
+                // 건너뛰어야하는 알파벳인 경우 (HashSet은 검색 빠름)
                 if (set.contains(ch)) {
-                    j--;
+                    j--;    // 반복문 변수 감소
                 }
             }
             answer+=ch;
