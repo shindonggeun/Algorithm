182f4035c3d55ee4f269f5b9e9a01fe210e691b4
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-02-03 16:36:36 +0900

    [level 2] Title: [3차] n진수 게임, Time: 21.68 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        String answer = "";
+        
+        StringBuilder convert = new StringBuilder();
+        
+        // convert의 길이가 미리 구할 숫자 갯수 * 게임 참가 인원만큼 될 때 까지 n진수로 변환
+        for(int i = 0; convert.length() <= t * m; i++){
+            convert.append(Integer.toString(i, n));
+        }
+        System.out.println(convert);
+        
+        // 튜브의 순서에 해당하는 글자들만 추출
+        for(int i = p - 1;  answer.length() < t; i += m){
+            answer+=convert.charAt(i);
+        }
+        answer = answer.toUpperCase();
+        
+        return answer;
+    }
+}
