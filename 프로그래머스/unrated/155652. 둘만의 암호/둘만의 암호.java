9408319aca81c5e3242ea6922b48208ed568f9cd
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-02-03 15:43:06 +0900

    [unrated] Title: 둘만의 암호, Time: 13.53 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.java
@@ -1,17 +1,28 @@
+import java.util.*;
+
 class Solution {
     public String solution(String s, String skip, int index) {
-        char[] words = s.toCharArray();
-        for(int i = 0 ; i < words.length ; i++){
-            for(int j = 0 ; j < index ; j++){
-                do{
-                    words[i]++;
-                    if(words[i] > 'z'){ //범위 초과시 a로 돌아옴
-                        words[i] -= 26; // == 'a'와 동일
-                    }
-                }while(skip.contains(String.valueOf(words[i])));
-            }
+        String answer = "";
+        Set<Character> set = new HashSet<>();
+        
+        for(int i=0; i<skip.length(); i++) {
+            set.add(skip.charAt(i));
         }
-        String answer = String.valueOf(words);
+        
+        for (int i = 0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            
+            for (int j=0; j<index; j++) {
+                ch++;
+                if (ch > 'z') {
+                    ch = 'a';
+                }
+                if (set.contains(ch)) {
+                    j--;
+                }
+            }
+            answer+=ch;
+        }
         return answer;
     }
 }
