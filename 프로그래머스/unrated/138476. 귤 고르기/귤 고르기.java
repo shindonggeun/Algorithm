18b65ee38f321c257f8c285a2d3a1c64f5e06b8e
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-02-09 16:12:11 +0900

    [unrated] Title: 귤 고르기, Time: 96.43 ms, Memory: 92.6 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -9,15 +9,17 @@ class Solution {
             map.put(i, map.getOrDefault(i, 0) + 1);
         }
         
-        List<Integer> list=new ArrayList<>(map.values());   // 귤의 개수를 담은 list
-        Collections.sort(list, Collections.reverseOrder()); // 귤의 개수 내림차순 정렬
+        List<Integer> list=new ArrayList<>(map.keySet());   // 귤의 번호를 담은 list
         
-        for(int i: list) {
-            k-=i;   // 귤의 개수 많은것부터 순회해서 귤 개수 차감
+        // 귤 번호에 따른 개수 내림차순 정렬
+        Collections.sort(list, (o1, o2) -> map.get(o2) - map.get(o1)); 
+        
+        for(int key: list) {
+            k-=map.get(key);   // 귤의 개수 많은것부터 순회해서 귤 개수 차감
             answer++;   // 종류수 증가
             
-            // 귤의 개수가 1보다 작아지면 반복문 빠져나옴
-            if(k<1) {
+            // 귤의 개수가 0이하인 경우 반복문 빠져나옴
+            if(k<=0) {
                 break;
             }
         }
