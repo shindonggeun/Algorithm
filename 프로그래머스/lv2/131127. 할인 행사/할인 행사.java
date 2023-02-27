8d2592e0850a9caf25b088aabb936728584bc469
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-02-27 15:34:31 +0900

    [level 2] Title: 할인 행사, Time: 0.04 ms, Memory: 65.7 MB -BaekjoonHub

--- 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/lv2/131127. 할인 행사/할인 행사.java
@@ -1,58 +1,57 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class Solution {
-    final int USER_QUALIFICATION_DURATION = 10;
-
+    
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
+        HashMap<String, Integer> wishList = new HashMap<>();     // key: 구매할 물품, value: 상품 수
+        HashMap<String, Integer> map = new HashMap<>();
 
-        //wantMap 초기화 및 필요한 상품 추가
-        Map<String, Integer> wantMap = new HashMap<String, Integer>();
-        for (int i = 0; i < want.length; i++) {
-            String s = want[i];
-            int count = number[i];
-
-            wantMap.put(s, count);
+        // 구매할 상품과 그거에 맞는 수 저장해주기
+        for(int i=0; i<want.length; i++){
+            wishList.put(want[i], number[i]);
         }
 
-        //discountMap 초기화 및 처음 10일치의 상품 추가
-        Map<String, Integer> discountMap = new HashMap<String, Integer>();
-        for (int i = 0; i < USER_QUALIFICATION_DURATION; i++) {
-            String addItem = discount[i];
-            discountMap.put(addItem, discountMap.getOrDefault(addItem, 0) + 1);
+        // 첫날부터 10일까지의 할인되는 물품 탐색
+        for(int i=0; i<10; i++){
+            map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
         }
-
-        //처음 10일치의 상품이 추가된 상태에서 isEqual 메서드를 실행하여 answer의 값을 변경한다.
-        if (isEqual(wantMap, discountMap)) {
+        
+        // 이 날짜에 구매가능한지 확인해서 가능하면 가능한날 + 해줌
+        if(want(wishList, map)) {
             answer++;
         }
 
-        //두번째날부터는 삭제할 상품 과 추가할 상품을 지정하여 discountMap을 업데이트 하고
-        //업데이트 된 내용을 바탕으로 isEqual 메서드를 실행하여 answer의 값을 변경한다.
-        for (int i = 1; i <= discount.length - USER_QUALIFICATION_DURATION; i++) {
-            String deleteItem = discount[i - 1];
-            discountMap.put(deleteItem, discountMap.get(deleteItem) - 1);
+        // 만약 할인물품이 10일까지만 판매한다면 구매가능한 날짜 리턴해줌
+        if(discount.length==10) {
+            return answer;
+        }
 
-            String addItem = discount[i - 1 + USER_QUALIFICATION_DURATION];
-            discountMap.put(addItem, discountMap.getOrDefault(addItem, 0) + 1);
+        // 10일이후(11일 부터) 할인 마지막날까지 탐색
+        for(int i=10; i<discount.length; i++){
+            map.put(discount[i-10], map.getOrDefault(discount[i-10], 0) - 1);
+            map.put(discount[i], map.getOrDefault(discount[i], 0) + 1);
 
-            if (isEqual(wantMap, discountMap)) {
+
+            if(want(wishList, map)) {
                 answer++;
             }
         }
 
-
         return answer;
     }
 
-    public boolean isEqual(Map<String, Integer> wantMap, Map<String, Integer> compareMap) {
-        for (String key : wantMap.keySet()) {
-            if (!compareMap.containsKey(key) || wantMap.get(key) != compareMap.get(key)) {
+    private boolean want(HashMap<String, Integer> wishList, HashMap<String, Integer> map){
+
+        // 구매할 물품을 조회해서
+        for(String key : wishList.keySet()){
+            // 구매할 물품의 개수가 할인 물품 개수와 다른 경우 구매 못함 
+            if(map.get(key) != wishList.get(key)) {
                 return false;
             }
         }
-
+        
+        // 구매가능
         return true;
     }
 }
