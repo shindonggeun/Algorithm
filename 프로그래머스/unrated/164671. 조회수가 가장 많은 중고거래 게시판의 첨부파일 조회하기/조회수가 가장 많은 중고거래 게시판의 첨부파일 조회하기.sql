51ba8e102d22e1fa326da9a058791b33584584e4
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-03-16 16:01:02 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,11 +1,11 @@
 -- 코드를 입력하세요
 SELECT '/home/grep/src/' || B.BOARD_ID || '/' || B.FILE_ID || B.FILE_NAME || B.FILE_EXT AS FILE_PATH
+        -- || 연산자를 이용해서 문자열 결합해줌
 FROM (SELECT BOARD_ID
       FROM USED_GOODS_BOARD
       WHERE VIEWS = (SELECT MAX(VIEWS) 
-                     FROM USED_GOODS_BOARD)
+                     FROM USED_GOODS_BOARD)     -- 조회수가 가장 높은 중고거래 게시물의 게시판 ID 추출
      ) A, USED_GOODS_FILE B
-WHERE A.BOARD_ID = B.BOARD_ID
-ORDER BY B.FILE_ID DESC
+WHERE A.BOARD_ID = B.BOARD_ID   -- 조인조건
+ORDER BY B.FILE_ID DESC         -- FILE ID를 기준으로 내림차순 정렬
 ;
-
