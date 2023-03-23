dcab07333bad3868b5f939b42995af62ec903464
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-03-23 16:40:26 +0900

    [Gold V] Title: 행복 유치원, Time: 812 ms, Memory: 66788 KB -BaekjoonHub

--- 백준/Gold/13164. 행복 유치원/행복 유치원.java
+++ 백준/Gold/13164. 행복 유치원/행복 유치원.java
@@ -7,28 +7,28 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());	// N명의 유치원 원생 수
+		int K = Integer.parseInt(st.nextToken());	// K개의 조가 되게끔 입력
 		
-		List<Integer> list = new ArrayList<>();
+		List<Integer> list = new ArrayList<>();		// 유치원 원생수의 키를 담은 리스트
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
-			int height = Integer.parseInt(st.nextToken());
+			int height = Integer.parseInt(st.nextToken());	// 키 입력
 			list.add(height);
 		}
 		
-		Collections.sort(list);
-		int temp = list.get(0);
-		List<Integer> diffList = new ArrayList<>();
+		Collections.sort(list);	// 키 오름차순 정렬
+		int temp = list.get(0);	
+		List<Integer> diffList = new ArrayList<>();	// 인접한 원생수의 키 차이를 뺀 리스트
 		
 		for(int i=1; i<N; i++) {
 			int diff = list.get(i) - temp;
 			diffList.add(diff);
 			temp = list.get(i);
 		}
-		Collections.sort(diffList);
-		int sum = 0;
+		Collections.sort(diffList);	// 인접값끼리 뺸(키 차이) 값을 담은 리스트 오름차순 정렬
+		int sum = 0;	// 티셔츠 만드는 비용의 최소 값
 		
 		for(int i=0; i<N-K; i++) {
 			sum+=diffList.get(i);
