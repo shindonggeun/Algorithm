054b47bc247e2b9e08619a34cad60304e9a717a2
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-04-06 18:07:06 +0900

    [unrated] Title: 덧칠하기, Time: 5.54 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/README.md
+++ 프로그래머스/unrated/161989. 덧칠하기/README.md
@@ -1,10 +1,10 @@
 # [unrated] 덧칠하기 - 161989 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/161989) 
 
 ### 성능 요약
 
-메모리: 82.4 MB, 시간: 3.40 ms
+메모리: 86.7 MB, 시간: 5.54 ms
 
 ### 구분
 
--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.java
@@ -11,16 +11,20 @@ class Solution {
             paint[section[i]] = false;  // 페인트 안칠해진 곳 설정 (false)
         }
         
+        // 페인트 안칠해진 구역 탐색
         for(int i=0; i<section.length; i++) {
+            // 페인트칠 배열에서 페인트가 안칠해졌다면
             if(paint[section[i]] != true) {
-                int count = 0;
+                int count = 0;  // 롤러 길이(배열 한 방당 1씩 증가)
+                // 정해진 롤러길이가 되기전까지 반복해줌
                 while(count < m) {
+                    // 페인트칠 배열길이 넘어가지 않도록 조건 걸어줌
                     if(section[i] + count < paint.length) {
-                        paint[section[i] + count] = true;
+                        paint[section[i] + count] = true;   // 페인트칠 완료
                     }
-                    count++;
+                    count++;    // 롤러 길이 증가(다음 배열 탐색하게끔)
                 }
-                answer++;
+                answer++;   // 페인트칠 횟수 증가
             }
         }
         
