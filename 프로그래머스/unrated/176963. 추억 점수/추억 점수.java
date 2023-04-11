55164cfe4d5219bdcc850af2c85abdcbecbeae01
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-04-11 16:42:38 +0900

    [unrated] Title: 추억 점수, Time: 0.04 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -3,16 +3,18 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] name, int[] yearning, String[][] photo) {
         int[] answer = new int[photo.length];
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<String, Integer> map = new HashMap<>(); // key: 사진 속 인물, value: 인물에 해당하는 추억점수
         
+        // 사진 속 인물에 따른 추억점수 저장하기
         for(int i=0; i<name.length; i++) {
             map.put(name[i], yearning[i]);
         }
         
         for(int i=0; i<photo.length; i++) {
             for(int j=0; j<photo[i].length; j++) {
+                // 사진속에 인물이 존재하면
                 if(map.containsKey(photo[i][j])) {
-                    answer[i]+=map.get(photo[i][j]);
+                    answer[i]+=map.get(photo[i][j]);    // 그 인물에 해당하는 추억점수 더해주기
                 }
             }
         }
