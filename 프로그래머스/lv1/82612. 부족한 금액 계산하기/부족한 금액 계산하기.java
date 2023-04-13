feed586b9372ab658e9773d78c38bf1d450796d3
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-04-13 13:12:34 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.11 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
+++ 프로그래머스/lv1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -9,11 +9,14 @@ class Solution {
         for(int i=1; i<=count; i++) {
             sum+=price * i;
         }
+        
+        // 자신이 가지고 있는 금액이 부족하지 않은 경우(놀이기구 이용료 다 내도 남는 경우)
         if(money > sum) {
-            answer = 0;
+            answer = 0; // 0으로 초기화
         }
+        // 금액이 부족한 경우
         else {
-            answer = sum - money;
+            answer = sum - money;   // 부족한 금액 계산
         }
         
         return answer;
