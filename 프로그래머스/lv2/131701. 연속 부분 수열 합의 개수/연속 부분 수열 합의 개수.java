ce33aa9f712826fd13400a38855df3ce180879e6
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-04-19 16:53:39 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 130.10 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -5,31 +5,33 @@ class Solution {
     public int solution(int[] elements) {
         int answer = 0;
         Set<Integer> set = new HashSet<>();
-        int[] arr = new int[elements.length * 2];
+        int[] arr = new int[elements.length * 2];   // 원순열이므로 원래 배열에서 2배해서 이어붙여줌
+        // 원래 배열 이어붙여줌
         for(int i=0; i<arr.length; i++) {
             arr[i] = elements[i % elements.length];
         }
         
-        int count = 1; 
+        
+        int count = 1;  // 슬라이딩 윈도우 알고리즘 이용하기 위한 count 변수
         
         // 슬라이딩 윈도우 알고리즘 이용
         while(true) {
-            int sum = 0;
-            
+            int sum = 0;    // 구간 합
+            // 탈출조건 (원래 배열보다 길이가 크면 탈출함)
             if(count == elements.length+1) {
                 break;
             }
-            
+            // 슬라이딩 윈도우 알고리즘 -> 시간복잡도: O(N)
             for(int i=0; i<arr.length; i++) {
                 sum+=arr[i];
-                
+                // 처음 길이를 벗어났을 때 부터 한칸씩 밀어준다
                 if(i >= count) {
                     sum-=arr[i-count];
                 }
-                set.add(sum);
+                set.add(sum);   // 중복되는 값을 제외해줘야하므로 set에 저장
             }
             
-            count++;
+            count++;    // 연속 부분 수열의 길이 증가
         }
         //System.out.println(set);
         answer = set.size();
