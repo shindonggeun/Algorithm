ef2ba7cf5fca2f8100d53f487e4a11b4981a04a0
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-04-19 15:57:20 +0900

    [level 2] Title: 스킬트리, Time: 6.87 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.java
@@ -4,9 +4,12 @@ class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
         
+        // 스킬트리 전부 탐색
         for(String sk_tr: skill_trees) {
-            sk_tr = sk_tr.replaceAll("[^"+ skill +"]", "");
+            // 정규식을 이용하여 선행스킬이 아닌 스킬들은 전부 빈 문자열 처리
+            sk_tr = sk_tr.replaceAll("[^"+ skill +"]", ""); 
             
+            // 스킬트리에서 선행스킬이 처음 나온경우 가능한 스킬트리임
             if(skill.indexOf(sk_tr) == 0) {
                 answer++;
             }
