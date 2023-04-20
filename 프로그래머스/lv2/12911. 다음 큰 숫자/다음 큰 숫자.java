002d587b1a9e1f4f6603c1fa67674b4768d73763
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-04-20 14:39:52 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.08 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -2,17 +2,18 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        String binaryString_n = Integer.toBinaryString(n);
-        String noZero_n = binaryString_n.replace("0", "");
+        String binaryString_n = Integer.toBinaryString(n);  // n을 이진수로 바꾼 뒤
+        String noZero_n = binaryString_n.replace("0", "");  // 바꾼 이진수에서 0을 모두 제거
         //System.out.println(nozero_n);
         answer = n;
         
         while(true) {
-            answer++;
+            answer++;   // n 다음의 수
             
-            String binaryString_answer = Integer.toBinaryString(answer);
-            String noZero_answer = binaryString_answer.replace("0", "");
+            String binaryString_answer = Integer.toBinaryString(answer);    // n 다음의 수를 이진수로 바꾼뒤
+            String noZero_answer = binaryString_answer.replace("0", "");    // 바꾼 이진수에서 0을 모두 제거
             
+            // 0을 제거한 n 다음의 수가 n과 1의 개수가 같으면 무한반복 빠져나옴
             if(noZero_n.equals(noZero_answer)) {
                 break;
             }
