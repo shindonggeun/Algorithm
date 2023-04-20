7331124058549fdf2335daeecbdabed6bdd1c94e
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-04-20 14:41:10 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.07 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -5,10 +5,9 @@ class Solution {
         String binaryString_n = Integer.toBinaryString(n);  // n을 이진수로 바꾼 뒤
         String noZero_n = binaryString_n.replace("0", "");  // 바꾼 이진수에서 0을 모두 제거
         //System.out.println(nozero_n);
-        answer = n;
+        answer = n + 1; // n 다음의 수
         
         while(true) {
-            answer++;   // n 다음의 수
             
             String binaryString_answer = Integer.toBinaryString(answer);    // n 다음의 수를 이진수로 바꾼뒤
             String noZero_answer = binaryString_answer.replace("0", "");    // 바꾼 이진수에서 0을 모두 제거
@@ -17,6 +16,8 @@ class Solution {
             if(noZero_n.equals(noZero_answer)) {
                 break;
             }
+            
+            answer++;
         }
         
         return answer;
