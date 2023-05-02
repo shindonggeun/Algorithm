20aa46c5ba6badd6d614f09b57af1f84c50af177
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-05-02 15:29:33 +0900

    [level 2] Title: 예상 대진표, Time: 0.02 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int a, int b) {
+        int answer = 0;
+
+        // 한 게임에 들어있을 때 까지 반복
+        while(a != b) {
+			a = (a / 2) + (a % 2); 
+			b = (b / 2) + (b % 2);
+			
+			answer++; // 게임 수 증가
+		}
+
+        return answer;
+    }
+}
