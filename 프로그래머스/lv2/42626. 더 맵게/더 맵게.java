e5d8c302b8d0ddd5a266ffbd9231b6e5ac1db2b9
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-06-16 17:19:48 +0900

    [level 2] Title: 더 맵게, Time: 1751.26 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.java
@@ -12,22 +12,26 @@ class Solution {
         
         int count = 0;
         while(true) { 
-            int hot1 = pq.poll();
+            int hot1 = pq.poll();   // 가장 맵지 않은 음식의 스코빌 지수
+            // 가장 맵지않음 음식의 스코빌 지수가 K 이상인 경우 무한반복 빠져나옴
+            // 즉 모든 음식의 스코빌 지수가 K 이상이 된거임
             if(hot1 >= K) {
                 break;
             }
-            
+            // 우선순위 큐 크기가 0인 경우
             if(pq.size() == 0) {
+                // 모든 음식의 스코빌 지수를 K이상으로 만들 수 없는 경우
+                // 즉 가장 맵지 않은 음식의 스코빌 지수가 K 미만인 경우 -1 리턴할 수 있게끔
                 if(hot1 < K) {
                     count = -1;
                     break;
                 }
             }
             
-            int hot2 = pq.poll();
+            int hot2 = pq.poll();   // 두번째로 맵지 않은 음식의 스코빌 지수
             int sum = hot1 + (hot2 * 2);
             pq.add(sum);
-            count++;
+            count++;    // 섞은 횟수 증가
         }
         
         answer = count;
