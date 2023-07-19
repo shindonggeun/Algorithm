2d7e11f15e502957e137a9b7bbe9d482a5b2c795
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-07-20 00:08:44 +0900

    [Bronze III] Title: 최댓값, Time: 120 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,29 +1,29 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        // 9개의 서로 다른 자연수 저장
-        int[] arr = new int[9];
-        
-        for(int i=0; i<arr.length; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        int max = 0;
-        int index = 0;
-        for(int i=0; i<arr.length; i++) {
-            if(max < arr[i]) {
-                max = arr[i];
-                index = i+1;
-            }
-        }
-        
-        System.out.println(max);
-        System.out.println(index);
-    }
+		int[] arr = new int[9];	// 서로 다른 9개의 자연수 저장하기 위한 배열
+		
+		int max = Integer.MIN_VALUE;
+		for(int i=0; i<9; i++) {
+			int num = Integer.parseInt(br.readLine());
+			arr[i] = num;
+			max = Math.max(max, arr[i]);
+		}
+		
+		int index = 0;	
+		for(int i=0; i<9; i++) {
+			if(max == arr[i]) {
+				index = i;
+			}
+		}
+		
+		System.out.println(max);		// 최대값
+		System.out.println(index+1);	// 몇번째 수인지
+
+	}
+
 }
