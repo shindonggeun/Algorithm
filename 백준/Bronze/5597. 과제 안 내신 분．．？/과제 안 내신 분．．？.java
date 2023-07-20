9e24a4988c17eabba996d16d08c80eaf92421fe3
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-07-20 22:12:55 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 120 ms, Memory: 14052 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,24 +1,28 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        
-        boolean[] number = new boolean[30];	// 30명의 학생 (1 ~ 30) (1번학생은 0번 인덱스에 있음)
-        
-        for(int i=0; i<28; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	int num = Integer.parseInt(st.nextToken());	// ex) 1을 입력시 -> 0번 인덱스를 true로 바꿔야됨
-        	number[num-1] = true;
-        }
-        
-        for(int i=0; i<number.length; i++) {
-        	if(number[i] == false) {
-        		System.out.println(i+1);
-        	}
-        }
-    }
+		boolean[] homework = new boolean[31];	// 1 ~ 30번 까지 출석번호 중 과제 제출한 사람 저장하는 배열
+		
+		// 28명만 과제 제출했으므로 입력 28번 받음
+		for(int i=0; i<28; i++) {
+			int num = Integer.parseInt(br.readLine());
+			homework[num] = true;
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		// 1번부터 30번까지 순차적으로 탐색
+		for(int i=1; i<=30; i++) {
+			// 숙제 안낸 사람인 경우 해당 번호 출력
+			if(!homework[i]) {
+				sb.append(i).append("\n");
+			}
+		}
+		System.out.print(sb);
+
+	}
+
 }
