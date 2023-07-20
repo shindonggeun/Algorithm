0570197e6a28216e072c8c9c13dcff0d99fec39d
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-07-21 00:48:51 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.39 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();    // key: 해당 숫자, value: 해당 숫자가 나온 횟수
+        
+        for(int i=0; i<array.length; i++) {
+            map.put(array[i], map.getOrDefault(array[i], 0) + 1);
+        }
+        
+        List<Integer> list = new ArrayList<>(map.values()); // map에 저장된 value값들 리스트화
+        Collections.sort(list, Collections.reverseOrder()); // 내림차순 정렬
+        
+        List<Integer> resultList = new ArrayList<>();       // 최빈값들 저장한 리스트
+        // 맵에 저장된 key값들을 이용하여 탐색
+        for(int key: map.keySet()) {
+            // key값(해당 숫자)에 해당하는 value값(해당 숫자가 나온 횟수)이 가장 많이 나온 것이면
+            if(map.get(key) == list.get(0)) {
+                resultList.add(key);    // 최빈값들 저장한 리스트에 추가
+            }
+        }
+        
+        // 최빈값들 저장한 리스트의 길이(사이즈)가 1보다 크면 (최빈값이 여러개라는 뜻)
+        if(resultList.size() > 1) {
+            answer = -1;    
+        }
+        // 최빈값을 저장한 리스트의 길이(사이즈)가 1보다 작거나 같은 경우 (즉, 최빈값 1개)
+        else {
+            answer = resultList.get(0);
+        }
+        
+        return answer;
+    }
+}
