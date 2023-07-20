f60eaba6077bf293532602427b3e9cf162a1c81c
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-07-21 00:09:08 +0900

    [unrated] Title: qr code, Time: 10.29 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181903. qr code/qr code.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(int q, int r, String code) {
+        String answer = "";
+        
+        for(int i=0; i<code.length(); i++) {
+            // 문자열의 각 인덱스를 q로 나누었을 때 나머지가 r인 경우
+            if(i % q == r) {
+                char ch = code.charAt(i);   // 해당 문자 뽑아내기
+                answer += ch;   // 순서대로 붙이기
+            }
+        }
+        return answer;
+    }
+}
