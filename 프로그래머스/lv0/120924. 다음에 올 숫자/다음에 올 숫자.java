6973ce28aefd3624fc40e9698b3ce8c8b235205f
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-07-22 15:26:58 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 71.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -1,17 +1,17 @@
 class Solution {
     public int solution(int[] common) {
         int answer = 0;
-        int result = 0;
         
-        // 등차수열인 경우
+        // common의 길이가 3부터 시작!
+        // 등차수열인 경우 
         if(common[1] - common[0] == common[2] - common[1]) {
-            result = common[1] - common[0];
-            answer = common[common.length - 1] + result;
-        } 
+            // common 배열의 맨 마지막 원소에서 공차만큼 더해주면 된다
+            answer = common[common.length - 1] + (common[1] - common[0]);
+        }
         // 등비수열인 경우
         else {
-            result = common[1] / common[0];
-            answer = common[common.length - 1] * result;
+            // common 배열의 맨 마지막 원소에서 공비만큼 곱해주면 된다 
+            answer = common[common.length - 1] * (common[1] / common[0]);
         }
         
          
