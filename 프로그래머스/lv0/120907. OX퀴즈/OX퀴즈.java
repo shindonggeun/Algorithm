cc58101960cb5015c6eaf7cbe9f6eefbef84efc3
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-07-27 18:49:42 +0900

    [level 0] Title: OX퀴즈, Time: 0.09 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.java
@@ -5,9 +5,12 @@ class Solution {
         String[] answer = new String[quiz.length];
         
         for(int i=0; i<quiz.length; i++) {
-            String[] quizSplit = quiz[i].split(" "); // 문자열 공백단위로 쪼개서 저장
+            // 문자열 공백단위로 쪼개서 저장
+            // 문자열.split("쪼갤 문자열 단위"); => return값은 문자열 배열로 리턴
+            String[] quizSplit = quiz[i].split(" ");    
             // System.out.println(Arrays.toString(quizSplit));
             
+            // [0] -> 숫자1, [1] -> 연산자, [2] -> 숫자2, [3] -> "=", [4] -> 연산해서 나온 결과값
             int num1 = Integer.parseInt(quizSplit[0]);
             int num2 = Integer.parseInt(quizSplit[2]);
             int result = Integer.parseInt(quizSplit[4]);
