697fb1246ef7534bbee051c8fa93ac3b3ba6504a
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-07-29 23:21:52 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 604 ms, Memory: 61116 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/README.md
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/README.md
@@ -4,11 +4,11 @@
 
 ### 성능 요약
 
-메모리: 67056 KB, 시간: 656 ms
+메모리: 61116 KB, 시간: 604 ms
 
 ### 분류
 
-자료 구조(data_structures), 해시를 사용한 집합과 맵(hash_set)
+자료 구조, 해시를 사용한 집합과 맵
 
 ### 문제 설명
 
@@ -100,17 +100,17 @@
 
 <p><img alt="" src="https://www.acmicpc.net/upload/201004/pc.PNG" style="height:183px; width:274px"></p>
 
-<p><img alt="" src="" style="height:182px; width:274px"></p>
+<p><img alt="" src="https://www.acmicpc.net/upload/201004/pd.PNG" style="height:182px; width:274px"></p>
 
-<p><img alt="" src="" style="height:183px; width:274px"></p>
+<p><img alt="" src="https://www.acmicpc.net/upload/201004/pe.PNG" style="height:183px; width:274px"></p>
 
-<p><img alt="" src="" style="height:183px; width:274px"></p>
+<p><img alt="" src="https://www.acmicpc.net/upload/201004/pf.PNG" style="height:183px; width:274px"></p>
 
-<p><img alt="" src="" style="height:181px; width:274px"></p>
+<p><img alt="" src="https://www.acmicpc.net/upload/201004/pg.PNG" style="height:181px; width:274px"></p>
 
-<p><img alt="" src="" style="height:184px; width:274px"></p>
+<p><img alt="" src="https://www.acmicpc.net/upload/201004/ph.PNG" style="height:184px; width:274px"></p>
 
-<p><img alt="" src="" style="height:184px; width:274px"></p>
+<p><img alt="" src="https://www.acmicpc.net/upload/201004/pi.PNG" style="height:184px; width:274px"></p>
 
 <p>이다솜 : 휴... 이겼다.</p>
 
--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -10,71 +10,32 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 		
-		// 순서 보장하는 HashMap
-		Map<String, Integer> map_name = new LinkedHashMap<>();	// key: 포켓몬 이름, value: 번호
-		Map<Integer, String> map_number = new LinkedHashMap<>();	// key: 번호, value: 포켓몬 이름
+		Map<String, String> nameMap = new HashMap<>();	// key: 포켓몬 이름, value: 해당 포켓몬의 이름에 따른 번호(String)
+		Map<String, String> numberMap = new HashMap<>();	// key: 포켓몬 번호(String), value: 해당 포켓몬의 번호에 따른 이름
 		
-		for(int i=1;i<=N; i++) {
-			st = new StringTokenizer(br.readLine());
-			//map.put(st.nextToken(), Integer.toString(i));
-			String name = st.nextToken();
-			map_name.put(name, i);
-			map_number.put(i, name);
+		for(int i=1; i<=N; i++) {
+			String name = br.readLine();
+			nameMap.put(name, String.valueOf(i));
+			numberMap.put(String.valueOf(i), name);
 		}
+
 		StringBuilder sb = new StringBuilder();
-		
 		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			String str = st.nextToken();
-			// 입력값이(첫글자가) 숫자인 경우 -> map_number 이용해서 포켓몬 이름 추출
-			if(isNumber(str.charAt(0))) {
-				// 입력값인 숫자(key)를 이용해서 value값인 포켓몬 이름 추출
-				sb.append(map_number.get(Integer.parseInt(str))).append("\n");
-			}
-			// 입력값이(첫글자가) 문자열인 경우(포켓몬 이름인 경우) -> map_name 이용해서 포켓몬 숫자 추출
-			else {
-				// 입력값인 문자열(key)를 이용해서 value값인 포켓몬 숫자 추출
-				sb.append(map_name.get(str)).append("\n");
-			}
+			String numberOrName = br.readLine();
 			
-			// 아래 방식대로 하면 시간 초과남
-			//String keyOrValue = st.nextToken();
-			
-			/*if(map.containsKey(keyOrValue)) {
-				sb.append(map.get(keyOrValue)).append("\n");
+			// 포켓몬 이름에 따른 번호를 저장한 맵에 해당 key값이 존재하면
+			if(nameMap.containsKey(numberOrName)) {
+				sb.append(nameMap.get(numberOrName)).append("\n");
 			}
-			else if(map.containsValue(keyOrValue)) {
-				sb.append(getKey(map, keyOrValue)).append("\n");
-			}*/
-		}
-		System.out.println(sb);
-
-	}
-	
-	public static boolean isNumber(char ch) {
-		boolean check = false;
-		
-		// 숫자인경우
-		if(Character.isDigit(ch)) {
-			check = true;
-		}
-		// 숫자 아닌경우
-		else {
-			check = false;
+			// 포켓몬 번호에 따른 이름을 저장한 맵에 해당 key값이 존재하면
+			else if(numberMap.containsKey(numberOrName)) {
+				sb.append(numberMap.get(numberOrName)).append("\n");
+			}
 		}
 		
-		return check;
+		System.out.print(sb);
+		
+		
 	}
-
-	// hashmap에 value 로 key 찾기
-	public static <K, V> K getKey(Map<K, V> map, V value) {
-		for (K key : map.keySet()) {
-			if (value.equals(map.get(key))) {
-				return key;
-			}
-		}
-		return null;
-	}
-	
 
 }
