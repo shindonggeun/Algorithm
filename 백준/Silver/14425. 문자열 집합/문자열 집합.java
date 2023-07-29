eff3abbd0a65880c1221c0f813ff752310656b62
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-07-29 13:53:51 +0900

    [Silver III] Title: 문자열 집합, Time: 416 ms, Memory: 31108 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -9,24 +9,23 @@ public class Main {
 		
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
-		Set<String> set = new HashSet<>();
-		int count = 0;	
+		Set<String> set = new HashSet<>();	// 문자열 담을 자료구조 HashSet, 중복허용 X
 		
 		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			set.add(st.nextToken());	// 입력값 set에 추가 (중복값 허용 X)
+			String input = br.readLine();
+			set.add(input);
 		}
 		
+		int count = 0;	// M개의 문자열 중에 총 몇개가 집합 S에 포함되어 있는지 세는 변수
 		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			// 입력값이 HashSet에 있는 경우
-			if(set.contains(st.nextToken())) {
-				count++;
+			String input = br.readLine();
+			// HashSet에 해당 입력한 문자열이 포함되어 있는 경우 (즉, 집합 S에 포함되어 있는 경우)
+			if(set.contains(input)) {
+				count++;	// 카운트 증가
 			}
 		}
 		
 		System.out.println(count);
-
 	}
 
 }
