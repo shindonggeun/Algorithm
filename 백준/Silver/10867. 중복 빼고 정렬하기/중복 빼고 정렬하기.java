e4929319d6ac0d6d132ec37e8aa48dda0cd4a5c7
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-07-30 13:11:00 +0900

    [Silver V] Title: 중복 빼고 정렬하기, Time: 336 ms, Memory: 25140 KB -BaekjoonHub

--- 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.java
+++ 백준/Silver/10867. 중복 빼고 정렬하기/중복 빼고 정렬하기.java
@@ -8,22 +8,22 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int N = Integer.parseInt(st.nextToken());
-		Set<Integer> set = new HashSet<>();	// 중복 허용안하는 자료구조
+		Set<Integer> set = new HashSet<>();	// 숫자를 담은 자료구조 HashSet (중복허용 X)
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
-			set.add(Integer.parseInt(st.nextToken()));
+			int num = Integer.parseInt(st.nextToken());
+			set.add(num);
 		}
 		
 		List<Integer> list = new ArrayList<>(set);	// set을 list화
-		Collections.sort(list);
+		Collections.sort(list);	// 리스트 오름차순 정렬(숫자 오름차순 정렬)
 		
 		StringBuilder sb = new StringBuilder();
-		for(int i: list) {
-			sb.append(i + " ");
+		for(int num: list) {
+			sb.append(num).append(" ");
 		}
 		System.out.println(sb);
-
 	}
 
 }
