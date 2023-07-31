2474e82ee9e465537c79d47a0bdb5a4e4e45edaa
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-07-31 23:42:29 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -2,22 +2,22 @@ class Solution {
     public int solution(int num) {
         int answer = 0;
         long number = num;
-        // 입력된 수가 1이 아닐때까지 반복(1이 될떄까지 반복)
+        // 입력된 수가 1이 아닐때까지 반복(1이 될 때까지 반복)
         while(number!=1) {
-            
             // 짝수인 경우
             if(number%2 == 0) {
-                number/=2;
+                number/=2;  // 2로 나눠줌
             }
             // 홀수인 경우
             else {
-                number = number*3 + 1;
+                number = number*3 + 1;  // 3을 곱하고 1을 더해줌
             }
-            answer++;
+            answer++;   // 해당 횟수 증가
             
+            // 해당 횟수가 500번이 넘어가면
             if(answer > 500) {
-                answer = -1;
-                break;
+                answer = -1;    // -1 저장
+                break;  // while문 빠져나옴
             }
         }
         
