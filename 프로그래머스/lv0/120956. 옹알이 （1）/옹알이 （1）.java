7f4304b14d4293fff9fcaf3ff84e6981dd1bd24b
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-07-31 23:35:22 +0900

    [level 0] Title: 옹알이 (1), Time: 1.20 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,22 +1,22 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
+        String[] word = {"aya", "ye", "woo", "ma"}; // 조카가 발음할 수 있는 것들
         
-        // String 배열 선언 및 초기화 (문제에서 주어진 네가지 발음)
-        String[] result = {"aya", "ye", "ma", "woo"};
-        
-        // 향상된 for문 이용 
-        for(String bab: babbling) {
-            // 4가지 발음 향상된 for문 이용
-            for(String str: result) {
-                // 애기가 발음한 문자열에서 주어진 발음 처음 나온것을 "1"로
-                // ex) ayaye -> "1ye" -> "11"
-                bab = bab.replaceFirst(str, "1");   
+        // 완전탐색 이용
+        // 향상된 for문 이용
+        // babbling 문자열 배열을 탐색해서
+        for(String babb: babbling) {
+            // 조카가 발음할 수 있는 것들 돌리기
+            for(String wo: word) {
+                babb = babb.replaceFirst(wo, "1");   // 조카가 발음할 수 있는 것(문자열)이 처음 나온것들만 문자열 "1"로 대체
             }
-            System.out.println(bab);
-            if(bab.replace("1", "").equals("")) {
-                answer++;
-            } 
+            babb = babb.replaceAll("1", "");  // 최종으로 문자열 "1"이 들어간 것들은 전부 빈 문자열로 대체
+            
+            // 위의 과정을 거쳐 빈 문자열인 경우(즉, 발음할 수 있는 것인 경우)
+            if(babb.equals("")) {
+                answer++;   // 조카가 발음할 수 있는 단어임
+            }
         }
         return answer;
     }
