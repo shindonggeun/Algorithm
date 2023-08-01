9425373a5fba7cb9fa876649ca51b5b8f313ebac
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-08-01 23:30:10 +0900

    [Silver IV] Title: 접미사 배열, Time: 168 ms, Memory: 17860 KB -BaekjoonHub

--- 백준/Silver/11656. 접미사 배열/접미사 배열.java
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.java
@@ -5,22 +5,21 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		String input = br.readLine();
 		
-		String str = st.nextToken();
-		List<String> list = new ArrayList<>();
-		
-		// list에 접미사 만들것들 집어넣기
-		for(int i=0; i<str.length(); i++) {
-			String subStr = str.substring(i, str.length());
-			list.add(subStr);
+		List<String> list = new ArrayList<>();	// 접미사들을 저장할 리스트
+		for(int i=0; i<input.length(); i++) {
+			// 접미사 만들어주기
+			// 문자열.substring(문자열의 해당 인덱스); -> 문자열의 해당 인덱스부터 문자열의 끝까지 잘라서 문자열 return 
+			String makeStr = input.substring(i);	
+			list.add(makeStr);	// 접미사 만든 것 리스트에 추가
 		}
 		
-		Collections.sort(list);	// list에 저장된 문자열들 사전순으로 정렬(오름차순)
+		Collections.sort(list);	// 접미사들을 저장한 리스트 오름차순 정렬
 		
 		StringBuilder sb = new StringBuilder();
-		for(String s: list) {
-			sb.append(s).append("\n");
+		for(String str: list) {
+			sb.append(str).append("\n");
 		}
 		System.out.print(sb);
 
