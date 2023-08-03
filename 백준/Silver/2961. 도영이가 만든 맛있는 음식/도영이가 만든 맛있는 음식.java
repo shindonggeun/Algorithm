6375cb0d44bda788ee97c1a6ff29ce97edaf6178
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-08-04 00:18:17 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 120 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -2,56 +2,59 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
+	
 	static int N;
 	static int[] taste1;	// 신맛을 담은 배열
 	static int[] taste2;	// 쓴맛을 담은 배열
 	static int[] output1;	// 신맛의 재료 조합 결과
 	static int[] output2;	// 쓴맛의 재료 조합 결과
-	static int min = Integer.MAX_VALUE;
-	
+	static int minResult = Integer.MAX_VALUE;	// 신맛과 쓴맛의 차이가 가장 작은 요리의 차이 결과값
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(br.readLine());
 		taste1 = new int[N];
 		taste2 = new int[N];
 		output1 = new int[N];
 		output2 = new int[N];
 		
 		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			taste1[i] = Integer.parseInt(st.nextToken());
-			taste2[i] = Integer.parseInt(st.nextToken());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			taste1[i] = Integer.parseInt(st.nextToken());	// 신맛 입력 받음
+			taste2[i] = Integer.parseInt(st.nextToken());	// 쓴맛 입력 받음
 		}
 		
+		// 완전탐색 이용
 		// 모든 조합 뽑아내기
 		for(int i=0; i<N; i++) {
-			dfs(0, i+1, 0);
+			backTracking(0, i+1, 0);
 		}
-		
-		System.out.println(min);
+		System.out.println(minResult);
 	}
 	
-	// 조합 메서드
-	public static void dfs(int depth, int count, int idx) {
-		// 해당 조합 개수가 충족되면 재귀 종료
+	// 백트래킹
+	// 조합 메서드 이용
+	public static void backTracking(int depth, int count, int idx) {
+		// 깊이가 해당 조합 개수만큼 도달했으면 (즉, 조합 개수 충족되면)
 		if(depth == count) {
-			int result1 = 1;
-			int result2 = 0;
+			int result1 = 1;	// 신맛 재료 섞어서 나오는 결과 값
+			int result2 = 0;	// 쓴맛 재료 섞어서 나오는 결과 값
+			// 해당 조합 개수까지 돌려줌
 			for(int i=0; i<count; i++) {
-				result1*=output1[i];	// 신맛들의 재료 조합은 곱해서 결과값 뽑아냄
-				result2+=output2[i];	// 쓴맛들의 재료 조합은 더해서 결과값 뽑아냄
+				result1 *= output1[i];	// 신맛 조합은 곱
+				result2 += output2[i];	// 쓴맛 조합은 합
 			}
-			min = Math.min(min, Math.abs(result1 - result2));	// 신맛과 쓴맛의 차이의 최소값 저장
-			return;
+			
+			// 신맛과 쓴맛의 차이가 가장 작은 요리의 결과값을 갱신해줌
+			minResult = Math.min(minResult, Math.abs(result1 - result2));
+			return;	// 메서드 종료
 		}
 		
+		// 재귀 호출 이용해서 재료들 조합 구해줌
 		for(int i=idx; i<N; i++) {
 			output1[depth] = taste1[i];
 			output2[depth] = taste2[i];
-			dfs(depth+1, count, i+1);
+			backTracking(depth+1, count, i+1);	
 		}
 	}
 
