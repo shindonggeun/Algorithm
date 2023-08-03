531bdbd6b94c1739a2e9c0d284e38c2f340592ff
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-08-04 00:21:14 +0900

    [Silver III] Title: 걸그룹 마스터 준석이, Time: 124 ms, Memory: 14032 KB -BaekjoonHub

--- 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.java
+++ 백준/Silver/16165. 걸그룹 마스터 준석이/걸그룹 마스터 준석이.java
@@ -7,60 +7,48 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());	// 걸그룹 수
-		int M = Integer.parseInt(st.nextToken());	// 맞춰야할 문제 수
+		int N = Integer.parseInt(st.nextToken());	// 입력 받을 걸그룹의 수
+		int M = Integer.parseInt(st.nextToken());	// 맞혀야 할 문제의 수
+		Map<String, String> girlGroupMap = new HashMap<>();	// key: 걸그룹 멤버 이름, value: 걸그룹 명
 		
-		Map<String, String> map = new HashMap<>();	// key: 멤버 이름, value: 걸그룹 이름
-		
+		// 걸그룹 수에 따른 걸그룹 명과 걸그룹 멤버 이름들 입력 받는 부분
 		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			String girlGroup = st.nextToken();
-			st = new StringTokenizer(br.readLine());
-			int memberNum = Integer.parseInt(st.nextToken());
+			String girlGroup = br.readLine();	// 걸그룹 이름 입력 받아오기
+			int count = Integer.parseInt(br.readLine());	// 걸그룹 수 입력받기
 			
-			for(int j=0; j<memberNum; j++) {
-				st = new StringTokenizer(br.readLine());
-				String memeberName = st.nextToken();
-				map.put(memeberName, girlGroup);
+			for(int j=0; j<count; j++) {
+				String name = br.readLine();	// 걸그룹 멤버 이름 입력 받아오기
+				girlGroupMap.put(name, girlGroup);	// 맵에 저장해주기
 			}
 		}
-		//System.out.println(map);
 		
-		// 문제 낸 순서에 따른 출력값 나와야하므로 LinkedHashMap 이용!!
-		Map<String, Integer> quiz = new LinkedHashMap<>();	// key: 걸그룹 이름 or 그룹명, value: 퀴즈 종류(0 or 1)
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			String quiz1 = st.nextToken();
-			st = new StringTokenizer(br.readLine());
-			int quiz2 = Integer.parseInt(st.nextToken());
-			quiz.put(quiz1, quiz2);
-		}
 		StringBuilder sb = new StringBuilder();
-		ArrayList<String> memberList = new ArrayList<>(map.keySet());	// 걸그룹 멤버들 이름 담은 리스트들(모든 걸그룹)
-		Collections.sort(memberList);	// 이름 오름차순 정렬
 		
-		// 문제 풀기
-		for(String key: quiz.keySet()) {
-			// 퀴즈 종류가 0인 경우 (그룹명 나오면 그 걸그룹에 따른 멤버들 이름 전부 출력)
-			if(quiz.get(key) == 0) {
+		// 맵의 keySet() 메서드를 이용해서 걸그룹 멤버 이름을 전부다 저장한 리스트
+		List<String> memberList = new ArrayList<>(girlGroupMap.keySet());	
+		Collections.sort(memberList);	// 걸그룹 멤버 이름 오름차순 정렬
+		// 문제 맞추는 부분 입력받기
+		for(int i=0; i<M; i++) {
+			String question = br.readLine();	// 걸그룹 멤버 이름인지 걸그룹 명인지 입력받기 
+			int quizType = Integer.parseInt(br.readLine());	// 퀴즈 종류 입력받기
+			
+			// 퀴즈 종류가 0일 경우 해당 팀의 속한 멤버 이름 뽑기
+			if(quizType == 0) {
 				for(String memberName: memberList) {
-					if(map.get(memberName).equals(key)) {
+					// map에서 해당 멤버 이름(key 값)에 따른 걸그룹명(value 값)이 동일한 경우
+					if(girlGroupMap.get(memberName).equals(question)) {
 						sb.append(memberName).append("\n");
 					}
 				}
 			}
-			// 퀴즈 종류가 1인 경우 (걸그룹 멤버 이름 나오면 그 걸그룹명 출력)
-			else {
-				for(String memberName: map.keySet()) {
-					if(key.equals(memberName)) {
-						sb.append(map.get(memberName)).append("\n");
-					}
-				}
+			// 퀴즈 종류가 1인 경우 해당 멤버가 속한 팀의 이름 출력
+			else if(quizType == 1) {
+				sb.append(girlGroupMap.get(question)).append("\n");
 			}
 		}
 		
-		System.out.println(sb);
-		
+		System.out.print(sb);
+
 	}
 
 }
