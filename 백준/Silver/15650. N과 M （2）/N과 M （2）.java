8f61d88c663e891a481ce37f2fae8401e1cb9d31
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-08-04 00:21:41 +0900

    [Silver III] Title: N과 M (2), Time: 136 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
+	
 	static int N;
 	static int M;
-	static int[] arr;	// 선택하고자 하는 대상 집합(숫자들)
+	static int[] arr;	// 선택하고자 하는 대상 집합
 	static int[] output;	// 대상 숫자를 담아올 배열
-	static StringBuilder sb;
+	static StringBuilder sb = new StringBuilder();
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,32 +18,32 @@ public class Main {
 		
 		arr = new int[N];
 		output = new int[N];
-		sb = new StringBuilder();
-		
+
+		// 대상 집합에 숫자 저장
 		for(int i=0; i<N; i++) {
 			arr[i] = i+1;
 		}
 		
-		dfs(0, 0);
+		backTracking(0, 0);
 		System.out.print(sb);
 	}
 	
-	// 조합 메서드(depth는 선택 횟수, idx는 다음 대상을 선택할때 집합에서 탐색하는 인덱스)
-	public static void dfs(int depth, int idx) {
+	// 백트래킹
+	// 조합 메서드
+	public static void backTracking(int depth, int idx) {
 		// 해당 깊이가 선택횟수와 같아지면 결과를 출력하고 재귀 종료
 		if(depth == M) {
 			for(int i=0; i<M; i++) {
-				sb.append(output[i] + " ");
+				sb.append(output[i]).append(" ");
 			}
 			sb.append("\n");
 			return;
 		}
 		
-		// 대상 집합을 주어진 인덱스부터 순회하며 숫자를 하나 선택함
 		for(int i=idx; i<N; i++) {
 			output[depth] = arr[i];
-			// 자신 이전의 수를 중복선택하지 않도록 인덱스를 +1하여 재귀호출함
-			dfs(depth+1, i+1);	
+			// 자신 이전의 수를 중복선택하지 않도록 인덱스를 +1 하여 재귀 호출함
+			backTracking(depth+1, i+1);
 		}
 	}
 
