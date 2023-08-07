eaecda421ab17b4b65938d6a2f5bcfc9e8c2860b
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-08-07 19:46:20 +0900

    [Silver IV] Title: 카드2, Time: 200 ms, Memory: 53360 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -5,24 +5,24 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		Queue<Integer> queue = new LinkedList<>();
-		
+		Queue<Integer> queue = new LinkedList<>();	// Queue LinkedList객체로 선언
+		// 1부터 N까지의 수 큐에 저장
 		for(int i=1; i<=N; i++) {
 			queue.add(i);
 		}
 		
 		while(queue.size() != 1) {
-			// 1. 맨위에 있는 카드 버림
-			queue.remove();	// 큐에서 첫번째 값 제거
-		
-			// 2. 그다음 제일 위의 있는 카드를 제일 아래에 있는 카드 밑으로 감
-			int card = queue.poll();	// 큐에서 첫번째 값 반환
-			queue.add(card);	// 다시 큐에 집어넣음
+			// 1. 맨 앞에 있는 카드를 버림
+			queue.poll();
+			// 2. 그다음 수를 맨 뒤로 옮김
+			int num = queue.poll();
+			queue.add(num);
 		}
-		System.out.println(queue.poll());	// 남게 되는 카드 번호 출력
+		// 마지막에 저장된 숫자 출력
+		System.out.println(queue.poll());
+		
 
 	}
 
