f4139d6758de007ec0596c043348f3b3d2b4bdbc
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-08-10 00:15:34 +0900

    [Silver III] Title: 바이러스, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,58 +2,47 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-	static int N;	// 컴퓨터의 수 (정점의 개수)
-	static int M;	// 네트워크 상에 직접 연결되어 있는 컴퓨터 쌍의 개수 (간선의 개수)
+	
+	static int N;	// 정점의 개수(컴퓨터의 수)
+	static int M;	// 간선의 개수(네트워크 상에 연결되어 있는 컴퓨터 쌍의 수)
 	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-	static boolean[] visited;
-	//static StringBuilder sb;
-	static int count;
+	static boolean[] visited;	// 각 정점 방문여부를 저장하는 배열
+	static int virusCount;	// 바이러스에 걸리게 된 컴퓨터의 수
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
 		visited = new boolean[N+1];
-		//sb = new StringBuilder();
-		count = 0;
+		virusCount = 0;
 		
+		// 그래프 정점(노드) 생성해주기
 		for(int i=0; i<=N; i++) {
 			graph.add(new ArrayList<>());
 		}
 		
 		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());	// 시작정점
-			int b = Integer.parseInt(st.nextToken());	// 끝정점
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int fromVertex = Integer.parseInt(st.nextToken());
+			int toVertex = Integer.parseInt(st.nextToken());
 			
-			// 정점간 간선 연결해주기 (양방향 그래프)
-			graph.get(a).add(b);
-			graph.get(b).add(a);
+			// 네트워크로 연결 (양방향 간선)
+			graph.get(fromVertex).add(toVertex);
+			graph.get(toVertex).add(fromVertex);
 		}
 		
-		for(int i=1; i<=N; i++) {
-			Collections.sort(graph.get(i));	// 정점에 연결된 간선들 오름차순 정렬
-		}
-		
-		dfs(1);	// 1번 정점에서부터 시작
-		System.out.println(count-1);	// 1번 정점을 뺀 바이러스에 걸린 컴퓨터의 개수 출력
-
+		dfs(1);	// 1번 바이러스가 웜 바이러스 걸렸을 때 깊이우선탐색 실시
+		System.out.println(virusCount);
 	}
 	
-	// 깊이우선탐색 알고리즘 이용
-	public static void dfs(int index) {
-		visited[index] = true;
-		//sb.append(index+ " ");	// 현재 방문한 값 저장
-		count++;		// 방분한 정점 개수 증가
+	public static void dfs(int vertex) {
+		visited[vertex] = true;	// 해당 정점(컴퓨터) 방문 처리
 		
-		// 현재 방문한 노드에 해당하는 리스트에 담겨있는 값들 하나씩 뽑아내기
-		for(int i: graph.get(index)) {
-			// 방문안한 노드인 경우
+		for(int i: graph.get(vertex)) {
+			// 방문하지 않은 정점(컴퓨터)인 경우
 			if(!visited[i]) {
-				dfs(i);	// 그 노드에 해당하는 리스트들 탐색할 수 있도록 재귀 호출
+				virusCount++;	// 바이러스에 걸린 컴퓨터의 수 증가
+				dfs(i);	// 깊이우선탐색 실시
 			}
 		}
 	}
