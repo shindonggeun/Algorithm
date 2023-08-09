c2b8860d525deb59f96afed4684423240978fd0a
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-08-09 15:18:37 +0900

    [Silver IV] Title: 주몽, Time: 388 ms, Memory: 21540 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -7,11 +7,11 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
+		int N = Integer.parseInt(st.nextToken());	
 		st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());	
 		
-		int[] num = new int[N];
+		int[] num = new int[N];			// 갑옷을 만드는데 필요한 재료들(고유번호)
 		
 		st = new StringTokenizer(br.readLine());
 		for(int i=0; i<N; i++) {
@@ -19,8 +19,10 @@ public class Main {
 		}
 		
 		int count = 0;
+		// 갑옷 2개만 추출이므로 브루트포스 사용
 		for(int i=0; i<N-1; i++) {
 			for(int j=i+1; j<N; j++) {
+				// 갑옷의 고유번호 합쳐서 M이면 count 증가
 				if(num[i] + num[j] == M) {
 					count++;
 				}
