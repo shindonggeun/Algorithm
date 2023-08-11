5f9d0fcd20e043e467a917aeeca0939765c52f65
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-08-11 16:04:18 +0900

    [Silver II] Title: 유기농 배추, Time: 156 ms, Memory: 15968 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -4,7 +4,8 @@ import java.io.*;
 public class Main {
 	
 	static class Position {
-		int x, y;
+		int x;
+		int y;
 		
 		public Position(int x, int y) {
 			this.x = x;
@@ -17,41 +18,40 @@ public class Main {
 	static int K;	// 배추가 심어져 있는 위치의 개수
 	static int[][] map;
 	static boolean[][] visited;
-	// 4가지 방향 (상, 하, 좌, 우)
-	static int[] dx = {0, 0, 1, -1};	// x방향 배열 (상, 하)
-	static int[] dy = {1, -1, 0, 0};	// y방향 배열 (좌, 우)
-	static int wormCount;	// 배추흰지렁이 마리 수
-	
+	// 4가지 방향 탐색 (상, 하, 좌, 우)
+	static int[] dx = {1, -1, 0, 0};	// x축이 고정되어 있을 때 y좌표가 움직이는 방향 배열(상, 하)
+	static int[] dy = {0, 0, -1, 1};	// y축이 고정되어 있을 때 x좌표가 움직이는 방향 배열(좌, 우)
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int testCase = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		for(int tc=0; tc<testCase; tc++) {
-			st = new StringTokenizer(br.readLine());
+		for(int tc=0; tc<T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			M = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
 			
-			M = Integer.parseInt(st.nextToken());	// 가로 길이
-			N = Integer.parseInt(st.nextToken());	// 세로 길이
-			K = Integer.parseInt(st.nextToken());	// 배추가 심어져 있는 위치의 개수
-			wormCount = 0;
-			map = new int[N][M];
+			map = new int[N][M];	// (0, 0) ~ (M-1, N-1)
 			visited = new boolean[N][M];
 			
+			// 배추 심어져있는 위치의 개수만큼 반복문 돌리기
 			for(int i=0; i<K; i++) {
 				st = new StringTokenizer(br.readLine());
-				int tempX = Integer.parseInt(st.nextToken());
-				int tempY = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());	// y좌표 (행) 입력
+				int x = Integer.parseInt(st.nextToken());	// x좌표 (열)	입력
 				
-				map[tempY][tempX] = 1;	// 해당 위치에 배추 심어져있음
+				map[x][y] = 1;	// 해당 좌표에 배추 심기(1)
 			}
 			
+			int wormCount = 0;	// 배추흰지렁이 개수
+			// 배추밭 탐색하기 
 			for(int i=0; i<N; i++) {
 				for(int j=0; j<M; j++) {
-					// 방문처리가 되지 않았으면서 배추가 심어져 있는 곳인 경우
+					// 해당 좌표가 탐색 안한 좌표면서 동시에 배추가 심어져 있는 좌표(1)인 경우 
 					if(!visited[i][j] && map[i][j] == 1) {
-						wormCount++;	// 배추흰지렁이 개수 증가
-						bfs(i, j);	// 너비우선탐색 호출
+						bfs(i, j);	// 너비우선탐색 실시
+						wormCount++;
 					}
 				}
 			}
@@ -59,13 +59,15 @@ public class Main {
 		}
 		
 		System.out.print(sb);
+
 	}
 	
 	public static void bfs(int startX, int startY) {
-		Queue<Position> queue = new LinkedList<>();
-		queue.add(new Position(startX, startY));
-		visited[startX][startY] = true;
+		Queue<Position> queue = new LinkedList<>();	// 너비우선탐색을 하기 위해 큐 선언 및 생성
+		queue.add(new Position(startX, startY));	// 시작좌표 큐에 집어넣기
+		visited[startX][startY] = true;	// 시작 좌표 방문처리
 		
+		// 큐가 빌때까지 반복
 		while(!queue.isEmpty()) {
 			Position now = queue.poll();
 			int nowX = now.x;
@@ -76,19 +78,18 @@ public class Main {
 				int nextX = nowX + dx[i];
 				int nextY = nowY + dy[i];
 				
-				// 이동한 x, y좌표가 범위를 벗어난 경우 (음수좌표 또는 (M-1, N-1) 좌표 넘어간 경우)
-                // 맵의 좌표는 (0,0) ~ (M-1, N-1) 까지이다
+				// (0, 0) ~ (M-1, N-1) 이외의 좌표를 탐색한 경우
 				if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
 					continue;
 				}
 				
-				// 방문처리가 됐거나 또는 배추가 심어져있지 않은 땅이면(0) 넘어감
+				// 탐색한 좌표가 이미 방문했거나 또는 배추가 심어져있지 않은 땅(0)인 경우
 				if(visited[nextX][nextY] || map[nextX][nextY] == 0) {
 					continue;
 				}
 				
-				queue.add(new Position(nextX, nextY));
-				visited[nextX][nextY] = true;   // 해당좌표 방문처리
+				queue.add(new Position(nextX, nextY));	// 큐에 탐색한 좌표 집어넣기
+				visited[nextX][nextY] = true;	// 탐색한 좌표 방문처리
 			}
 		}
 	}
