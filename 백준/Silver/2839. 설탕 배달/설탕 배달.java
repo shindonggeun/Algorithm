f05873d577ee57a854e925586d86c4d508629f33
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-08-11 11:36:39 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -5,24 +5,23 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
+		int fiveKilogram = N / 5;	// 사용한 3킬로그램 봉지 수
+		int threeKilogram = N  / 3;		// 사용한 5킬로그램 봉지 수
 		
-		int fiveLimit = N / 5;
-		int threeLimit = N / 3;
 		int count = 0;
 		
 		// 브루트포스 알고리즘 이용
 		// 3킬로그램과 5킬로그램 봉지를 쓰는데 두 봉지 쓸때 두 봉지를 합한 최소 봉지수를 구해야하므로
 		// 3킬로그램을 탐색하는 것은 이중for문에서 바깥, 5킬로그램 탐색하는 것은 이중for문의 안쪽
-		for(int i=0; i<=threeLimit; i++) {
-			// 5키로그램 설탕부터 먼저 써야 된다!!!
-			for(int j=0; j<=fiveLimit; j++) {
+		for(int i=0; i<=threeKilogram; i++) {
+			// 5킬로그램 설탕부터 먼저 써야 된다!!!
+			for(int j=0; j<=fiveKilogram; j++) {
 				if((i*3) + (j*5) == N) {
 					count = i+j;
 					System.out.println(count);
-					return;
+					return;	//메서드  종료
 				}
 			}
 		}
