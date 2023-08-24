0e9bd866efebe567f8a5b6ba47a08bd688c1c5e1
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-08-24 14:00:24 +0900

    [Gold IV] Title: ⚾, Time: 496 ms, Memory: 61256 KB -BaekjoonHub

--- 백준/Gold/17281. ⚾/⚾.java
+++ 백준/Gold/17281. ⚾/⚾.java
@@ -3,168 +3,196 @@ import java.io.*;
 
 public class Main {
 	
-	static int inning;
-	static int[][] playerStat;	// 입력받는 선수의 이닝 별 타구능력(안타인지 2루타인지 등)
-								// 안타 = 1, 2루타 = 2, 3루타 = 3, 홈런 = 4
-	
-	static boolean[] isPlayerChecked;	// 조합에서 사용할 체크 배열
-	static int[] lineUp;	// 선수 순서 조합한 배열
-	static int outCount;	// 아웃 카운트
-	static int maxScore = Integer.MIN_VALUE;	// 최대 점수
+	static int inning;	// 이닝 수
+	static int[][] playerAbility;	// 입력받는 선수의 이닝 별 타구 능력을 담을 배열 (안타인지, 2루타인지 등등)
+									// 안타 = 1, 2루타 = 2, 3루타 = 3, 홈런 = 4, 아웃 = 0
+	static int[] playerLineUp;	// 1번 선수부터 9번 선수까지 각 타순 저장할 배열 (순열)
+	static boolean[] isPlayerSelected;	// 순열에서 사용할 선수들 선택 여부 체크할 배열
+	static int maxScore;	// 아인타팀이 얻을 수 있는 최대 득점 점수
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		inning = Integer.parseInt(br.readLine());	// 이닝 수 입력받기
+		playerAbility = new int[inning][10];	// 입력받는 선수의 이닝 별 타구 능력 배열 (0번방은 사용 X)
 		
-		inning = Integer.parseInt(st.nextToken());	// 이닝수 입력
-		playerStat = new int[inning][10];	// 입력받는 선수의 이닝 별 타구능력(안타인지 2루타인지 등) (0번방은 사용 X)
 		
 		for(int i=0; i<inning; i++) {
-			st = new StringTokenizer(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			// 이닝별 선수들의 능력 입력받기
 			for(int j=1; j<=9; j++) {
-				playerStat[i][j] = Integer.parseInt(st.nextToken());
+				playerAbility[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		isPlayerChecked = new boolean[10];	// 선수 순서 정할 체크 배열 (1번부터 9번까지 쓸거임) (0번방은 사용 X)
-		lineUp = new int[10];				// 1번타자 ~ 9번타자까지 (0번방 즉 0번타자는 사용 X)
+		playerLineUp = new int[10];	// 0번방 사용 X (1번 타자 선수 ~ 9번 타자 선수)
+		isPlayerSelected = new boolean[10];	// 0번방 사용 X (1번 타자 선수 ~ 9번 타자 선수)
+		maxScore = Integer.MIN_VALUE;
 		
-		lineUp[4] = 1;		// 4번타자로 1번선수를 배치함
-		isPlayerChecked[4] = true;	// 4번타자 자리 미리 넣었으므로 체크 처리함
+		// 문제조건 => 아인타는 자신이 가장 좋아하는 선수인 1번 선수를 4번 타자로 미리 결정
+		playerLineUp[4] = 1;	// 4번타자에 1번 선수를 기용 
+		isPlayerSelected[4] = true;	// 4번타자 자리 미리 넣었으므로 선택(방문) 처리
+		playerSequence(2);	// 1번 선수의 자리는 이미 선정됐으므로(4번 타순) 2번 선수부터 시작
 		
-		playerSeq(2);	// 1번 선수의 자리는 이미 있으므로 2번 선수부터 시작
-		
-		System.out.println(maxScore);	// 최대 점수 출력
-		
-		
-
+		System.out.println(maxScore);
 	}
 	
-	// 선수 순서 정해줄 메서드
-	static void playerSeq(int num) {
-		// 9명 다 채워졌으면 종료함
-		if(num == 10) {
-			int score = play();
-			maxScore = Math.max(maxScore, score);
-			
-			return;
+	// 백트래킹 메서드 (순열)
+	// 선수 순서 정해줄 메서드	=> 4번 타자 자리는 1번 선수가 가지고 있으므로 8명의 순서를 정하면 됨 => 8! = 40320가지 경우의 수 
+	public static void playerSequence(int playerNum) {
+		// 선수 9명 다 채워진 경우 (종료조건)
+		if(playerNum == 10) {
+			int score = playGame();	// 게임 시작해서 득점한 점수 저장
+			maxScore = Math.max(maxScore, score);	// 최대로 득점한 점수 갱신
+			return;	// 메서드 종료
 		}
 		
-		// 현재 선수를 1번 자리부터 9번 자리까지 넣어보며 재귀 호출함
-		// 백트래킹 알고리즘 이용
+		// 현재 선수(선수번호)를 1번타자 자리부터 9번타자 자리까지 넣어보며 재귀 호출함
+		// 위에서 4번타자 자리는 이미 고정시켜놨기 때문에 4번타자 자리는 제외한다
 		for(int i=1; i<=9; i++) {
-			if(!isPlayerChecked[i]) {
-				isPlayerChecked[i] = true;
-				
-				lineUp[i] = num;
-				playerSeq(num + 1);
-				isPlayerChecked[i] = false;	
+			// 해당 타순 이미 선택된 경우 (방문처리 됐으면)
+			if(!isPlayerSelected[i]) {
+				isPlayerSelected[i] = true;	// 해당 타순 선택 처리 (방문처리)
+				playerLineUp[i] = playerNum;	// 해당 타순에 배치할 선수 번호 넣어줌
+				playerSequence(playerNum + 1);	// 타순 배치한 선수 명수 증가
+				isPlayerSelected[i] = false;	// 해당 타순 선택 처리 (방문 처리) 해제
 			}
 		}
+		
 	}
 	
-	static int play() {
-		int sum = 0;	// 총 게임 점수(여러 이닝 거쳐서 나온 획득 점수)
+	// 선수들 순서 정해졌으면 경기 시작하는 메서드 
+	public static int playGame() {
+		int totalScore = 0;	// 경기에서 획득한 점수 (이닝 전체 순회해서 얻은 점수)
+		int startPlayer = 1;	// 1번 타자(1번 타순)부터 시작
 		
-		int idx = 1;	// 1번 타자부터 시작
+		// 이닝수만큼 순회
 		for(int i=0; i<inning; i++) {
-			int inningScore = 0;	// 현재 이닝에서 얻는 점수
-			outCount = 0;	// 현재 이닝에서 아웃카운트 수
-			boolean[] base = new boolean[4];	// true: 해당 베이스에 선수가 있음, false: 해당 베이스에 선수가 없음
+			int inningScore = 0;	// 현재 이닝에서 획득한 점수
+			int outCount = 0;	// 아웃카운트 (3개 되면 이닝 종료)
+			boolean[] base = new boolean[4];	// 0번방 사용 X (true: 해당 방(베이스)에 선수 있음, false: 해당 방(베이스)에 선수 없음)
 			
 			// 아웃카운트 3개 되기전까지 반복
 			while(outCount < 3) {
-				// 타자가 아웃당한 경우
-				if(playerStat[i][lineUp[idx]] == 0) {
+				int hitOrOut = playerAbility[i][playerLineUp[startPlayer]];	// 해당 타순의 타자가 안타(안타, 2루타, 3루타, 홈런) 쳤는지 또는 아웃인지 여부
+				
+				// 해당 타순의 타자가 아웃당한 경우
+				if(hitOrOut == 0) {
 					outCount++;	// 아웃카운트 증가
 				}
-				// 타자가 안타친 경우(1루타)
-				else if(playerStat[i][lineUp[idx]] == 1) {
-					// 3루 베이스에 주자가 있는 경우
+				// 해당 타순의 타자가 안타친 경우
+				else if(hitOrOut == 1) {
+					// 3루 베이스에 선수 있는 경우
 					if(base[3]) {
-						inningScore++;		// 3루베이스에 있는 주자 들어왔으므로 점수 획득
-						base[3] = false;	// 주자 다시 없음
+						// 3루베이스에 주자 홈으로 들어옴
+						inningScore++;	// 현재 이닝에서 점수 획득
+						base[3] = false;	// 3루 주자 홈으로 들어왔으므로 3루 베이스에 사람 없음 
 					}
-					// 2루 베이스에 주자 있는 경우
+					
+					// 2루 베이스에 선수 있는 경우
 					if(base[2]) {
-						base[3] = true;		// 2루 베이스 주자 3루로 이동
-						base[2] = false;	// 2루 베이스 비워짐
+						// 2루 베이스 주자 3루로 이동
+						base[2] = false;	
+						base[3] = true;		
 					}
-					// 1루 베이스에 주자 있는 경우
+					
+					// 1루 베이스에 선수 있는 경우
 					if(base[1]) {
-						base[2] = true;		// 1루 베이스 주자 2루로 이동
+						//1루 베이스 주자 2루로 이동
+						base[1] = false;
+						base[2] = true;
 					}
 					base[1] = true;	// 타자가 안타 쳤으므로 1루 베이스로 이동
 				}
-				// 타자가 2루타 친 경우
-				else if(playerStat[i][lineUp[idx]] == 2) {
-					// 3루 베이스에 주자가 있는 경우
+				// 해당 타순의 타자가 2루타를 친 경우
+				else if(hitOrOut == 2) {
+					// 3루 베이스에 선수 있는 경우
 					if(base[3]) {
-						inningScore++;		// 3루베이스에 있는 주자 들어왔으므로 점수 획득
-						base[3] = false;	// 주자 다시 없음
+						// 3루베이스에 주자 홈으로 들어옴
+						inningScore++;	// 현재 이닝에서 점수 획득
+						base[3] = false;	// 3루 주자 홈으로 들어왔으므로 3루 베이스에 사람 없음 
 					}
-					// 2루 베이스에 주자 있는 경우
+					
+					// 2루 베이스에 선수 있는 경우
 					if(base[2]) {
-						inningScore++;		// 2루베이스에 있는 주자 들어왔으므로 점수 획득
+						// 2루 베이스 주자 홈으로 들어옴
+						inningScore++;	// 현재 이닝에서 점수 획득
+						base[2] = false;	// 2루 주자 홈으로 들어왔으므로 2루 베이스에 사람 없음
 					}
-					// 1루 베이스에 주자 있는 경우
+					
+					// 1루 베이스에 선수 있는 경우
 					if(base[1]) {
-						base[3] = true;		// 1루 베이스 주자 3루로 이동
-						base[1] = false;	// 1루 베이스 비워짐
+						//1루 베이스 주자 3루로 이동
+						base[1] = false;
+						base[3] = true;
 					}
-					base[2] = true;	// 타자가 2루타 쳤으므로 2루 베이스로 이동
+					
+					base[2] = true;	// 타자가 2루타를 쳤으므로 2루 베이스로 이동
 				}
-				// 타자가 3루타 친 경우
-				else if(playerStat[i][lineUp[idx]] == 3) {
-					// 3루 베이스에 주자가 있는 경우
+				// 해당 타순의 타자가 3루타를 친 경우
+				else if(hitOrOut == 3) {
+					// 3루 베이스에 선수 있는 경우
 					if(base[3]) {
-						inningScore++;		// 3루베이스에 있는 주자 들어왔으므로 점수 획득
+						// 3루베이스에 주자 홈으로 들어옴
+						inningScore++;	// 현재 이닝에서 점수 획득
+						base[3] = false;	// 3루 주자 홈으로 들어왔으므로 3루 베이스에 사람 없음 
 					}
-					// 2루 베이스에 주자 있는 경우
+					
+					// 2루 베이스에 선수 있는 경우
 					if(base[2]) {
-						inningScore++;		// 2루베이스에 있는 주자 들어왔으므로 점수 획득
-						base[2] = false;	// 2루 베이스 비워짐
+						// 2루 베이스 주자 홈으로 들어옴
+						inningScore++;	// 현재 이닝에서 점수 획득
+						base[2] = false;	// 2루 주자 홈으로 들어왔으므로 2루 베이스에 사람 없음
 					}
-					// 1루 베이스에 주자 있는 경우
+					
+					// 1루 베이스에 선수 있는 경우
 					if(base[1]) {
-						inningScore++;		// 1루베이스에 있는 주자 들어왔으므로 점수 획득
-						base[1] = false;	// 1루 베이스 비워짐
+						//1루 베이스 주자 홈으로 이동
+						inningScore++;	// 현재 이닝에서 점수 획득
+						base[1] = false;	// 1루 주자 홈으로 들어왔으므로 1루 베이스에 사람 없음
 					}
-					base[3] = true;	// 타자가 3루타 쳤으므로 3루 베이스로 이동
+					
+					base[3] = true;	// 타자가 3루타를 쳤으므로 3루 베이스로 이동
 				}
-				// 타자가 홈런 친 경우
-				else if(playerStat[i][lineUp[idx]] == 4) {
-					// 3루 베이스에 주자가 있는 경우
+				// 해당 타순의 타자가 홈런을 친 경우
+				else if(hitOrOut == 4) {
+					// 3루 베이스에 선수 있는 경우
 					if(base[3]) {
-						inningScore++;		// 3루베이스에 있는 주자 들어왔으므로 점수 획득
-						base[3] = false;	// 3루 베이스 비워짐
+						// 3루베이스에 주자 홈으로 들어옴
+						inningScore++;	// 현재 이닝에서 점수 획득
+						base[3] = false;	// 3루 주자 홈으로 들어왔으므로 3루 베이스에 사람 없음 
 					}
-					// 2루 베이스에 주자 있는 경우
+					
+					// 2루 베이스에 선수 있는 경우
 					if(base[2]) {
-						inningScore++;		// 2루베이스에 있는 주자 들어왔으므로 점수 획득
-						base[2] = false;	// 2루 베이스 비워짐
+						// 2루 베이스 주자 홈으로 들어옴
+						inningScore++;	// 현재 이닝에서 점수 획득
+						base[2] = false;	// 2루 주자 홈으로 들어왔으므로 2루 베이스에 사람 없음
 					}
-					// 1루 베이스에 주자 있는 경우
+					
+					// 1루 베이스에 선수 있는 경우
 					if(base[1]) {
-						inningScore++;		// 1루베이스에 있는 주자 들어왔으므로 점수 획득
-						base[1] = false;	// 1루 베이스 비워짐
+						//1루 베이스 주자 홈으로 이동
+						inningScore++;	// 현재 이닝에서 점수 획득
+						base[1] = false;	// 1루 주자 홈으로 들어왔으므로 1루 베이스에 사람 없음
 					}
-					inningScore++;		// 타자가 홈런 쳤으므로 점수 획득
+					
+					inningScore++;	// 타자가 홈런 쳤으므로 점수 획득
 				}
 				
-				idx++;	// 다음 타자로 이동
-				// 만약 10번 타자가 되면(10번타자는 없으므로 1번 타자로 다시)
-				if(idx >= 10) {
-					idx = 1;
+				startPlayer++;	// 다음 타자로 이동
+				// 만약 10번 타순이 되면 (10번 타순은 없으므로 1번 타순으로 다시 시작)
+				if(startPlayer >= 10) {
+					startPlayer = 1;	// 1번 타순으로 다시 재조정
 				}
 			}
 			
-			// 해당 이닝에서 얻은 점수를 총 게임 점수에 더해줌
-			sum += inningScore;
-			
+			// 해당 이닝에서 얻은 점수를 총 게임 점수에 더해줌 (경기에서 득점한 점수)
+			totalScore += inningScore;	
 		}
-		// 이번 게임에서 얻은 점수를 반환
-		return sum;	
+		
+		return totalScore;	// 경기에서 득점한 점수 반환
 	}
+	
+	
 
 }
