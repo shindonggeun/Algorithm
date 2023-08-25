3edf4cdd437621e89d8c40ec942dfb5977496cb1
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-08-25 16:18:37 +0900

    [level 2] Title: 문자열 압축, Time: 0.11 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/lv2/60057. 문자열 압축/문자열 압축.java
@@ -15,29 +15,29 @@ class Solution {
             length = s.length() / 2 + 1;
         }
         
-        int compressMinLength = Integer.MAX_VALUE;
-        StringBuilder sb;
+        int compressMinLength = Integer.MAX_VALUE;  // 문자열 잘라 압축하여 표현한 문자열 중 가장 짧은 것의 길이
+        StringBuilder sb;   // 문자열에서 해당 인덱스에 반복되어 나타난 문자열의 개수를 쉽게 넣기 위해 StringBuilder 이용
+        
+        // 문자열 최대 압축 길이까지 반복문 돌림
         for(int i=1; i<=length; i++) {
-            sb = new StringBuilder();
-            int leftIdx = 0;
-            int rightIdx = leftIdx + i;
-            int duplicateCount = 1;
+            sb = new StringBuilder();   
+            int leftIdx = 0;    // 왼쪽 부분 인덱스
+            int rightIdx = leftIdx + i; // 오른쪽 부분 인덱스
+            int duplicateCount = 1; // 문자열 반복해서 나타난 횟수 
             
-            String compressStr1 = s.substring(leftIdx, rightIdx);
-            sb.append(compressStr1);
+            String compressStr1 = s.substring(leftIdx, rightIdx);   // 왼쪽부분 문자열 압축
+            sb.append(compressStr1);    // StringBuilder에 왼쪽부분 문자열 압축한것 저장
             
             while(rightIdx + i <= s.length()) {
                 String compressStr2 = s.substring(rightIdx, rightIdx+i);
-                
                 if(!compressStr1.equals(compressStr2)) {
-                    leftIdx = rightIdx;
                     compressStr1 = compressStr2;
                     
                     if(duplicateCount > 1) {
                         sb.insert(sb.length()-i, duplicateCount);
                     }
                     
-                    sb.append(compressStr1);
+                    sb.append(compressStr2);
                     duplicateCount = 1;
                 }
                 else {
