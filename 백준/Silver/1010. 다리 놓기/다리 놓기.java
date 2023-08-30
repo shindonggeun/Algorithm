c62c4912dfe28ee8e1b5f1db2fd740ae03531280
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-08-30 13:55:17 +0900

    [Silver V] Title: 다리 놓기, Time: 76 ms, Memory: 11380 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -2,20 +2,24 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-	static int[][] dp;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		long[][] dp = new long[31][31];	// [1][1] ~ [29][29]
+		long[][] dp = new long[30][30];	// [0][0] ~ [29][29]
 		
-		for(int i=1; i<=30; i++) {
+		// 다리를 겹치게 놓을 수 없다 => 다리 왼쪽 사이트의 개수 N, 오른쪽 사이트의 개수 M (N <= M)
+		// 다리를 놓는 방법의 개수는 M개중 N개를 순서 상관없이 뽑는 방법의 개수와 같음 => 즉, 조합 공식 이용
+		
+		// 동적계획법 상향식 풀이
+		// 초기화 작업
+		// 조합성질 1. => nC0 = nCn = 1
+		for(int i=1; i<30; i++) {
+			dp[i][0] = 1;
+			dp[i][i] = 1;
 			dp[i][1] = i;	// M개중 1개만 뽑는 경우의 수 => M개
-			
 		}
 		
-		// 조합성질 1. => nC0 = nCn = 1
-		// 조합성질 2. => n+1Cr+1 = nCr + nCr-1
+		// 조합성질 2. => nCr = n-1Cr-1 + n-1Cr
 		for(int i=2; i<30; i++) {
 			for(int j=2; j<30; j++) {
 				dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
