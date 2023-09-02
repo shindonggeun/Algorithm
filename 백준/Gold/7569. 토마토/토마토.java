533e282e7d457cc0673c533a364a8857a31e1a23
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-09-02 13:03:55 +0900

    [Gold V] Title: 토마토, Time: 656 ms, Memory: 121908 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.java
+++ 백준/Gold/7569. 토마토/토마토.java
@@ -3,124 +3,133 @@ import java.io.*;
 
 public class Main {
 	
-	static class Position {
-		int z, x, y;
+	// 토마토의 좌표 정보를 담고있는 내부 클래스
+	static class Tomato {
+		int z;
+		int x;
+		int y;
 		
-		public Position(int z, int x, int y) {
+		public Tomato(int z, int x, int y) {
 			this.z = z;
 			this.x = x;
 			this.y = y;
 		}
 	}
 	
-	// 3차원 배열
-	static int N;	// 가로 길이 (x 좌표)
-	static int M; 	// 세로 길이 (y 좌표)
-	static int H;	// 높이 (z 좌표)
+	static int M;	// 가로 칸 수
+	static int N;	// 세로 칸 수
+	static int H;	// 상자의 수
 	static int[][][] map;
-	static boolean[][][] visited;
-	static Queue<Position> queue = new LinkedList<>();
-	// 6가지 방향 탐색 (상, 하, 좌, 우, 위(z 좌표만 이동), 아래(z 좌표만 이동) )
-	static int[] dx = {1, -1, 0, 0, 0, 0};	// x축과 z축이 고정되어 있을 때 y좌표만 이동하는 방향 배열 (상, 하)
-	static int[] dy = {0, 0, -1, 1, 0, 0};	// y축과 z축이 고정되어 있을 때 x좌표만 이동하는 방향 배열 (좌, 우)
-	static int[] dz = {0, 0, 0, 0, 1, -1};	// x축과 y축이 고정되어 있을 때 z좌표만 이동하는 방향 배열 (위, 아래)
+	static boolean[][][] visited;	// 3차원 방문 배열
+	// 6가지 방향 배열 (상, 하, 좌, 우, 위 (z좌표만 이동), 아래 (z좌표만 이동))
+	static int[] dx = {1, -1, 0, 0, 0, 0};	// x축과 z축이 고정되어 있을 때 y좌표가 움직이는 방향 배열
+	static int[] dy = {0, 0, -1, 1, 0, 0};	// y축과 z축이 고정되어 있을 때 x좌표가 움직이는 방향 배열
+	static int[] dz = {0, 0, 0, 0, 1, -1};	// x축과 y축이 고정되어 있을 때 z좌표가 움직이는 방향 배열
+	static Queue<Tomato> queue = new LinkedList<>();
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		H = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());	// 가로 칸 수 입력
+		N = Integer.parseInt(st.nextToken());	// 세로 칸 수 입력
+		H = Integer.parseInt(st.nextToken());	// 상자의 수 입력
 		
-		map = new int[H][M][N];	// (0, 0, 0) ~ (N-1, M-1, H-1)
-		visited = new boolean[H][M][N];
+		map = new int[H][N][M];	// [0][0][0] ~ [H-1][N-1][M-1]
+		visited = new boolean[H][N][M];
 		
 		for(int i=0; i<H; i++) {
-			for(int j=0; j<M; j++) {
+			for(int j=0; j<N; j++) {
 				st = new StringTokenizer(br.readLine());
-				for(int k=0; k<N; k++) {
+				for(int k=0; k<M; k++) {
 					map[i][j][k] = Integer.parseInt(st.nextToken());
-					// 처음에 토마토가 익은(1) 위치 queue에 저장 (토마토가 익은 칸들 너비우선탐색 돌리기 위해)
+					// 맵에서 해당 좌표가 익은 토마토 칸(1)인 경우
 					if(map[i][j][k] == 1) {
-						queue.add(new Position(i, j, k));
+						queue.add(new Tomato(i, j, k));	// 큐에 저장
 					}
 				}
 			}
 		}
 		
-		int minDays = bfs();
-		System.out.println(minDays);
+		int minDay = bfs();
+		System.out.println(minDay);
 	}
 	
+	// 너비우선탐색 메서드
 	public static int bfs() {
+		// 큐가 빌때까지 반복
 		while(!queue.isEmpty()) {
-			Position now = queue.poll();
+			Tomato now = queue.poll();
 			int nowZ = now.z;
 			int nowX = now.x;
 			int nowY = now.y;
 			
 			// 6가지 방향 탐색
 			for(int i=0; i<6; i++) {
-				int nextZ = nowZ + dz[i];
 				int nextX = nowX + dx[i];
 				int nextY = nowY + dy[i];
+				int nextZ = nowZ + dz[i];
 				
-				// 탐색한 좌표가 (0, 0, 0) ~ (N-1, M-1, H-1) 이외의 좌표인 경우
-				if(nextZ < 0 || nextX < 0 || nextY < 0 || nextZ >= H || nextX >= M || nextY >= N) {
-					continue;
+				// 탐색한 좌표가 [0][0][0] ~ [H-1][N-1][M-1] 이외의 좌표인 경우
+				if(nextZ < 0 || nextX < 0 || nextY < 0 || nextZ >= H || nextX >= N || nextY >= M) {
+					continue;	// 넘어감
 				}
 				
+				// 탐색한 좌표가 이미 방문했거나 또는 토마토가 들어있지 않은 칸(-1)인 경우
 				if(visited[nextZ][nextX][nextY] || map[nextZ][nextX][nextY] == -1) {
-					continue;
+					continue;	// 넘어감
 				}
 				
-				// 익지않은 토마토만 탐색해줘야한다!!
+				// 탐색한 좌표가 익지않은 토마토(0)인 경우
 				if(map[nextZ][nextX][nextY] == 0) {
-					map[nextZ][nextX][nextY] = map[nowZ][nowX][nowY] + 1;
-					visited[nextZ][nextX][nextY] = true;
-					queue.add(new Position(nextZ, nextX, nextY));
+					map[nextZ][nextX][nextY] = map[nowZ][nowX][nowY] + 1;	// 맵에 토마토 익게끔 걸린 시간 저장
+					visited[nextZ][nextX][nextY] = true;	// 탐색한 좌표 방문처리 (토마토 익었음)
+					queue.add(new Tomato(nextZ, nextX, nextY));	// 큐에 탐색한 좌표 저장
 				}
 			}
 		}
 		
-		// 위의 탐색과정 다 거치면 map에서 최대값을 뽑아내야 함
-		// 만약 map에서 0이 존재하면 -1을 출력해야함
-		if(!checkZero(map)) {
-			return -1;
+		// 위의 너비우선탐색 과정 다 거쳤으면 map에서 최대값을 뽑아내야 한다
+		// 토마토가 모두 익은 경우
+		if(checkZeroTomato()) {
+			int day = findday();
+			return day-1;
 		}
-		else {
-			int max = findMax(map);
-			return max-1;
-		}
+		// 그 이외의 경우는 -1 반환하게끔 (즉, 토마토가 모두 익지 않은 상황이면)
+		return -1;
+		
 	}
-	// map에서 0이 존재하는지 여부를 판단해주는 메서드
-	public static boolean checkZero(int[][][] map) {
+	
+	// 토마토가 모두 익었는지 안익었는지 판단해주는 메서드
+	public static boolean checkZeroTomato() {
 		for(int i=0; i<H; i++) {
-			for(int j=0; j<M; j++) {
-				for(int k=0; k<N; k++) {
-					// 토마토가 익지 않은게 존재하면 false
+			for(int j=0; j<N; j++) {
+				for(int k=0; k<M; k++) {
+					// 토마토가 익지 않았으면(0)
 					if(map[i][j][k] == 0) {
-						return false;
+						return false;	// false 반환
 					}
 				}
 			}
 		}
-		// 위 과정 다 탐색했는데도 0 없으면 true (토마토 모두 익은 상태임)
+		
+		// 토마토가 모두 익었으면 true 반환
 		return true;
 	}
-	// 3차원 배열에서 최대값 뽑아내는 메서드
-	public static int findMax(int[][][] map) {
-		int max = Integer.MIN_VALUE;
-			
-		for(int[][] twoArray: map) {
-			for(int[] oneArray: twoArray) {
-				for(int element: oneArray) {
-					max = Math.max(max, element);
+	
+	// 토마토가 모두 익었을 때 맵에서 익은지 며칠 됐는지 확인해주는 메서드
+	public static int findday() {
+		int maxDay = Integer.MIN_VALUE;
+		
+		for(int i=0; i<H; i++) {
+			for(int j=0; j<N; j++) {
+				for(int k=0; k<M; k++) {
+					maxDay = Math.max(maxDay, map[i][j][k]);
 				}
 			}
 		}
-		return max;
+		
+		return maxDay;
 	}
 
 }
