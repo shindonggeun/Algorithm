cf60e710242ebdae90822702a506e822c4a512bb
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-09-11 09:00:28 +0900

    [Silver III] Title: 수열, Time: 200 ms, Memory: 22332 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,42 +1,41 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		int[] arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int max = 0;
-		int sum = 0;
-	
-		// 슬라이딩 윈도우 알고리즘 방식 이용
-		for(int i=0; i<N; i++) {
-			sum+=arr[i];
-			
-			// 최초에 나온 합을 최대값으로 잡아놓음
-			if(i == K - 1) {
-				max = sum;
-			}
-			
-			// 처음 길이 벗어났을 때 부터 한칸씩 밀어주면서 최대값 비교
-			if(i >= K) {
-				sum-=arr[i-K];
-				max = Math.max(max, sum);
-			}
-		}
-		System.out.println(max);
-		
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	}
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        int sum = 0;
+        int cnt = 0;
+        int max = Integer.MIN_VALUE;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            arr[i] = num;
+
+            if (cnt == K) {
+                max = Math.max(max, sum);
+                sum -= arr[i - K];
+                cnt--;
+            }
+
+            if (cnt != K) {
+                sum += num;
+                cnt++;
+            }
+        }
+        System.out.println(Math.max(max, sum));
+
+    }
 }
