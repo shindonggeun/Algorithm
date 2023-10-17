cc468b1edc1dad4bd7fa1a9a1e911bd26253f586
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-10-17 21:48:49 +0900

    [Silver I] Title: 떡 먹는 호랑이, Time: 120 ms, Memory: 11776 KB -BaekjoonHub

--- 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.java
+++ 백준/Silver/2502. 떡 먹는 호랑이/떡 먹는 호랑이.java
@@ -16,16 +16,19 @@ public class Main {
 		int result = 1;	// 초기 떡 개수
 		while(true) {
 			dp[1] = result;	// 1일차에 해당 떡 개수 설정하고 시작
+			// 2일차 떡 개수 늘려가는 과정
 			for(int i=result; i<K; i++) {
-				dp[2] = i;
+				dp[2] = i;	// 2일차의 떡 개수
+				
 				for(int j=3; j<D; j++) {
+					// 각 날에 준 떡의 개수를 피보나치 수열을 이용하여 계산
 					dp[j] = dp[j-1] + dp[j-2];
 				}
-				
+				// D째날의 하루 전과 이틀전의 떡 준 개수 합이 D째날에 준 떡의 개수와 같아지는 경우
 				if(dp[D] == dp[D-1] + dp[D-2]) {
-					System.out.println(dp[1]);
+					System.out.println(dp[1]);	
 					System.out.println(dp[2]);
-					return;
+					return;	// 메서드 종료
 				}
 			}
 			
