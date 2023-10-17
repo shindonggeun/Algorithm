9e27adb4d09861467888208b9ce7a30d4f0064d7
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-10-17 09:49:06 +0900

    [Silver II] Title: 앵무새, Time: 264 ms, Memory: 22800 KB -BaekjoonHub

--- 백준/Silver/14713. 앵무새/앵무새.java
+++ 백준/Silver/14713. 앵무새/앵무새.java
@@ -5,41 +5,47 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		Queue[] qlist = new LinkedList[N];	// 큐 배열 생성(앵무새들이 저장할 문장(단어 리스트들))
+		int N = Integer.parseInt(br.readLine());
+		// 각 앵무새가 한 문장(여러 단어로 이루어진 것)을 저장할 큐 배열 이용
+		// [0] => 0번 앵무새의 문장 저장, [1] => 1번 앵무새의 문장 저장, ... , [N-1]
+		Queue<String>[] qlist = new Queue[N];	
 		
 		for(int i=0; i<N; i++) {
-			qlist[i] = new LinkedList<String>();
-			st = new StringTokenizer(br.readLine());
+			qlist[i] = new LinkedList<>();	// 배열 한 원소(방)마다 큐 이용할 수 있게끔 LinkedList로 초기화
+			String input = br.readLine();	// 앵무새가 말한 문장 저장
+			String[] inputSplit = input.split(" ");	// 앵무새가 말한 문장 여러 단어로 쪼개기 (공백단위)
 			
-			// 입력값을 공백단위로 자른 토큰값들 계속해서 각 큐(앵무새)에 저장
-			while(st.hasMoreTokens()) {
-				qlist[i].add(st.nextToken());
+			// 앵무새가 말한 문장 단어별로 쪼갠것들 저장해주는 과정
+			for(String in: inputSplit) {
+				qlist[i].add(in);
 			}
 		}
 		
-		String input = br.readLine();
-		String[] tmp = input.split(" ");
+		String L = br.readLine();
+		String[] result = L.split(" ");
 		
-		boolean isOk = true;	// 규칙에 의해 나올 수 있는 문장인지 여부
-		for(String word: tmp) {
-			boolean match = false;
+		boolean isOk = true;	// 규칙에 의해 가능한 문장(L)인지 여부를 판단해주는 변수  (일단 만들 수 있다고 표시)
+		
+		// 완전탐색 이용
+		// 문장 L 단어별로 쪼갠것 탐색
+		for(String str: result) {
+			boolean match = false;	// 해당 번호의 앵무새가 순서대로 말한 단어인지 파악해주는 변수
+			// 해당 번호의 앵무새 탐색하기
 			for(int i=0; i<N; i++) {
 				if(!qlist[i].isEmpty()) {
-					if(qlist[i].peek().equals(word)) {
-						qlist[i].poll();
-						match = true;
-						break;
+					// 해당 번호의 앵무새가 말한 단어와 같은 경우
+					if(qlist[i].peek().equals(str)) {
+						match = true;	// 해당 번호의 앵무새가 순서대로 말한 단어임
+						qlist[i].poll();	// 해당 앵무새가 말한 단어를 큐에서 제거
+						break;	// 다음 앵무새 탐색 
 					}
 				}
 			}
 			
-			// 앵무새가 기억하고 있는 단어 리스트들에 매치가 안된 경우
+			// 해당 번호의 앵무새가 순서대로 말한 단어가 아닌 경우
 			if(!match) {
-				isOk = false;	// 규칙에 의해 나올 수 없는 문장임
-				break;
+				isOk = false;	// 맞지 않는 단어가 발견되면 불가능한 문장으로 표시
+				break;	// 더이상 탐색할 필요 없이 단어 못만들음
 			}
 		}
 		
@@ -55,16 +61,12 @@ public class Main {
 			}
 		}
 		
-		// 규칙에 의해서 나올 수 없는 문장인 경우
 		if(!isOk) {
 			System.out.println("Impossible");
 		}
-		// 규칙에 의해 나올 수 있는 문장인 경우
 		else {
 			System.out.println("Possible");
 		}
-		
-
 	}
 
 }
