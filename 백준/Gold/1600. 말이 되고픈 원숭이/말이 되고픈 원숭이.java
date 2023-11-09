4ec334952e8621b679e7591c0ae4e8aa3289bb59
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-11-09 14:20:52 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 556 ms, Memory: 88312 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -2,42 +2,44 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
-	static class Position {
-		int x, y;
-		int distance;	// 원숭이가 갈 수 있는 거리
-		int count;		// 원숭이가 말처럼 움직일 수 있는 능력 몇개까지 사용했는지 저장할 변수
+	
+	// 원숭이의 정보를 저장하는 내부 클래스
+	static class Monkey {
+		int x;
+		int y;
+		int move;	// 움직임 수
+		int ability;	// 말처럼 움직이기 위해 사용한 능력 수
 		
-		public Position(int x, int y, int distance, int count) {
+		public Monkey(int x, int y, int move, int ability) {
 			this.x = x;
 			this.y = y;
-			this.distance = distance;
-			this.count = count;
+			this.move = move;
+			this.ability = ability;
 		}
 	}
 	
 	static int K;	// 능력 수
-	static int W;	// 가로 길이 (x 좌표)
-	static int H;	// 세로 길이 (y 좌표)
+	static int W;	// 가로 길이
+	static int H;	// 세로 길이
 	static int[][] map;
-	static boolean[][][] visited;	// 3차원 방문배열 이용
+	static boolean[][][] visited;	// 3차원 방문 배열 (맨 앞 차원 능력 사용 여부)
+	// 4가지 방향 배열 (하, 상, 좌, 우)
+	static int[] fourDx = {1, -1, 0, 0};
+	static int[] fourDy = {0, 0, -1, 1};
 	// 8가지 방향 배열 
-	static int[] eigthDx = {2, 1, -1, -2, -2, -1, 1, 2};	// x축이 고정되어 있을 때 y좌표가 움직이는 방향 배열
-	static int[] eightDy = {1, 2, 2, 1, -1, -2, -2, -1};	// y축이 고정되어 있을 때 x좌표가 움직이는 방향 배열
-	
-	// 4가지 방향 배열 (상, 하, 좌, 우)
-	static int[] fourDx = {1, -1, 0, 0};	// x축이 고정되어 있을 때 y좌표가 움직이는 방향 배열 (상, 하)
-	static int[] fourDy = {0, 0, -1, 1};	// y축이 고정되어 있을 때 x좌표가 움직이는 방향 배열 (좌, 우)
+	static int[] eightDx = {-2, -1, 1, 2, 2, 1, -1, -2};
+	static int[] eightDy = {1, 2, 2, 1, -1, -2, -2, -1};
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		K = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		W = Integer.parseInt(st.nextToken());
 		H = Integer.parseInt(st.nextToken());
-		map = new int[H][W];	// (0, 0) ~ (W-1, H-1)
-		visited = new boolean[K+1][H][W];	// [0] -> 능력 사용 X, [1] ~ [K] 능력 사용한 횟수
+		
+		map = new int[H][W];	// [0][0] ~ [H-1][W-1]
+		visited = new boolean[K+1][H][W];
 		
 		for(int i=0; i<H; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -46,67 +48,79 @@ public class Main {
 			}
 		}
 		
-		int result = bfs(0, 0);
-		System.out.println(result);
+		int monkeyMoveCount = monkeyMove(0, 0);	// 원숭이 시작지점에서 시작해서 도착지까지 이동시킨 뒤 원숭이 움직임 수 저장
+		
+		System.out.println(monkeyMoveCount);
 
 	}
 	
-	public static int bfs(int startX, int startY) {
-		Queue<Position> queue = new LinkedList<>();
-		queue.add(new Position(startX, startY, 0, 0));
-		visited[0][startX][startY] = true;
+	// 원숭이가 시작지점에서 도착지까지 이동하는 메서드 (너비우선탐색 알고리즘 이용)
+	public static int monkeyMove(int startX, int startY) {
+		// 너비우선탐색 알고리즘을 이용하기 위해 큐 선언 및 생성
+		Queue<Monkey> queue = new LinkedList<>();
+		queue.add(new Monkey(startX, startY, 0, 0));	// 시작좌표 정보 및 원숭이 움직임 수, 사용한 능력 수 큐에 저장
+		visited[0][startX][startY] = true;	// 원숭이 능력 수에 따른 시작 좌표 방문 처리
 		
+		// 너비우선탐색 알고리즘 이용
 		while(!queue.isEmpty()) {
-			Position now = queue.poll();
+			// 현재 좌표 정보 및 원숭이의 정보 (움직임 수, 능력수) 큐에서 뽑아냄
+			Monkey now = queue.poll();
 			int nowX = now.x;
 			int nowY = now.y;
-			int nowDistance = now.distance;
-			int nowCount = now.count;
+			int nowMoveCount = now.move;
+			int nowAbilityCount = now.ability;
 			
-			// 끝좌표에 도달했으면 메서드 종료
+			// 현재 좌표가 도착지점에 도달한 경우
 			if(nowX == H-1 && nowY == W-1) {
-				return nowDistance;
+				return nowMoveCount;	// 현재 원숭이가 움직인 수 반환
 			}
-
-			// 4가지 방향 탐색 (상, 하, 좌, 우)
-			for (int i = 0; i < 4; i++) {
+			
+			// 먼저 4가지 방향 탐색하기
+			for(int i=0; i<4; i++) {
 				int nextX = nowX + fourDx[i];
 				int nextY = nowY + fourDy[i];
-
-				if (nextX < 0 || nextY < 0 || nextX >= H || nextY >= W) {
-					continue;
+				
+				// 탐색한 좌표가 [0][0] ~ [H-1][W-1] 이외의 좌표인 경우
+				if(nextX < 0 || nextY < 0 || nextX >= H || nextY >= W) {
+					continue;	// 넘어감
 				}
-
-				if (visited[nowCount][nextX][nextY] || map[nextX][nextY] == 1) {
-					continue;
+				
+				// 해당 능력수에 따른 탐색한 좌표가 이미 방문처리 됐거나 또는 장애물(1)인 경우
+				if(visited[nowAbilityCount][nextX][nextY] || map[nextX][nextY] == 1) {
+					continue;	// 넘어감
 				}
-
-				queue.add(new Position(nextX, nextY, nowDistance + 1, nowCount));
-				visited[nowCount][nextX][nextY] = true;
+				
+				// 탐색한 좌표 정보 및 원숭이의 이동수 및 사용한 능력 수 큐에 저장
+				queue.add(new Monkey(nextX, nextY, nowMoveCount + 1, nowAbilityCount));
+				visited[nowAbilityCount][nextX][nextY] = true;	// 사용한 능력수에 따른 탐색한 좌표 방문처리
 			}
 			
-			if(nowCount < K) {
-				// 8가지 방향 탐색
+			// 현재 사용한 능력 수가 K 미만인 경우 (말의 능력 사용 가능)
+			// 말의 능력 사용했으므로 nowAbility + 1
+			if(nowAbilityCount < K) {
+				// 8가지 방향 탐색하기
 				for(int i=0; i<8; i++) {
-					int nextX = nowX + eigthDx[i];
+					int nextX = nowX + eightDx[i];
 					int nextY = nowY + eightDy[i];
 					
+					// 탐색한 좌표가 [0][0] ~ [H-1][W-1] 이외의 좌표인 경우
 					if(nextX < 0 || nextY < 0 || nextX >= H || nextY >= W) {
-						continue;
+						continue;	// 넘어감
 					}
 					
-					// 탐색한 좌표가 이미 방문했거나 또는 벽(1)인 경우
-					if(visited[nowCount+1][nextX][nextY] || map[nextX][nextY] == 1) {
-						continue;
+					// 해당 능력수+1에 따른 탐색한 좌표가 이미 방문처리 됐거나 또는 장애물(1)인 경우
+					if(visited[nowAbilityCount+1][nextX][nextY] || map[nextX][nextY] == 1) {
+						continue;	// 넘어감
 					}
 					
-					queue.add(new Position(nextX, nextY, nowDistance + 1, nowCount + 1));
-					visited[nowCount + 1][nextX][nextY] = true;
+					// 탐색한 좌표 정보 및 원숭이의 이동수 및 사용한 능력 수 큐에 저장
+					queue.add(new Monkey(nextX, nextY, nowMoveCount + 1, nowAbilityCount+1));
+					visited[nowAbilityCount+1][nextX][nextY] = true;	// 사용한 능력수에 따른 탐색한 좌표 방문처리
 				}
-			}			
+			}
 		}
 		
-		// 탐색을 다 했는데도 이동할 수 없는 경우 -1 리턴
+		// 위의 너비우선탐색을 다 진행했는데도 도착지점에 도달하지 못한 경우 -1 반환
 		return -1;
 	}
 
