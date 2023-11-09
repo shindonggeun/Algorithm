3817edc9a4d5d6b1051a975ea03d43901d0cdc0d
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-11-09 13:40:37 +0900

    [Gold V] Title: 적록색약, Time: 168 ms, Memory: 17104 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -3,34 +3,29 @@ import java.io.*;
 
 public class Main {
 	
-	// 좌표들의 정보를 저장할 내부 클래스
 	static class Position {
-		int x;
-		int y;
+		int x, y;
 		
 		public Position(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
 	}
-
+	
 	static int N;
 	static char[][] map;
-	static boolean[][] visited;	// 2차원 방문 배열
-	// 4가지 방향 배열 (상, 하, 좌, 우) -> 배열에서는 하, 상, 좌, 우
+	static boolean[][] visited;
+	// 4가지 방향을 나타내는 방향배열 (상, 하, 좌, 우)
 	static int[] dx = {1, -1, 0, 0};	// x축이 고정되어 있을 때 y좌표가 움직이는 방향 배열 (상, 하)
 	static int[] dy = {0, 0, -1, 1};	// y축이 고정되어 있을 때 x좌표가 움직이는 방향 배열 (좌, 우)
-	static int normalAreaCount;	// 적록색약이 아닌 사람이 봤을 때 구역의 수
-	static int abnormalAreaCount;	// 적록색약인 사람이 봤을 때 구역의 수
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());	
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		map = new char[N][N];	// (0, 0) ~ (N-1, N-1) => [0][0] ~ [N-1][N-1]
+		N = Integer.parseInt(st.nextToken());
+		map = new char[N][N];	// (0, 0) ~ (N-1, N-1)
 		visited = new boolean[N][N];
-		normalAreaCount = 0;
-		abnormalAreaCount = 0;
 		
 		for(int i=0; i<N; i++) {
 			String input = br.readLine();
@@ -39,74 +34,72 @@ public class Main {
 			}
 		}
 		
-		
-		
-		// 적록색약이 아닌 사람이 봤을 때 구역의 수 찾는 과정
+		int normalColorCount = 0;
+		// 적록색약이 아닌경우 실시하는 과정
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<N; j++) {
-				// 방문하지 않은 좌표 인 경우
 				if(!visited[i][j]) {
-					bfs(i, j);	// 너비우선탐색 실시
-					normalAreaCount++;	// 구역의 수 증가
+					bfs(i, j);
+					normalColorCount++;
 				}
 			}
 		}
 		
-		// 적록색약인 사람이 RGB 영역 봤을 때
+		// 적록색약인 경우 너비우선탐색 실시하는 과정
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<N; j++) {
-				// 초록('G')인 구역을
+				// 적록색약은 빨간색과 초록색의 차이를 거의 느끼지 못하므로
+				// 해당 좌표가 초록색인 경우
 				if(map[i][j] == 'G') {
-					map[i][j] = 'R';	// 빨강('R') 구역으로 바꿔줌
+					map[i][j] = 'R';	// 빨간색으로 바꿔줌
 				}
 			}
 		}
 		
-		visited = new boolean[N][N];	// 2차원 방문 배열 다시 초기화
 		
-		// 적록색약인 사람이 봤을 때 구역의 수 찾는 과정
+		int abnormalColorCount = 0;
+		visited = new boolean[N][N];
+		// 적록색약인 경우 너비우선탐색 실시하는 과정
 		for(int i=0; i<N; i++) {
 			for(int j=0; j<N; j++) {
-				// 방문하지 않은 좌표 인 경우
 				if(!visited[i][j]) {
-					bfs(i, j);	// 너비우선탐색 실시
-					abnormalAreaCount++;	// 구역의 수 증가
+					bfs(i, j);
+					abnormalColorCount++;
 				}
 			}
 		}
 		
-		System.out.println(normalAreaCount + " " + abnormalAreaCount);
+		System.out.println(normalColorCount + " " + abnormalColorCount);
 	}
 	
-	// 너비우선탐색메서드
 	public static void bfs(int startX, int startY) {
 		Queue<Position> queue = new LinkedList<>();
-		queue.add(new Position(startX, startY));	// 해당 시작좌표 정보 큐에 저장
-		visited[startX][startY] = true;	// 해당 시작좌표 방문처리
-		char tempColor = map[startX][startY];	// 해당 시작좌표의 색상
+		queue.add(new Position(startX, startY));
+		visited[startX][startY] = true;
+		char tempColor = map[startX][startY];
 		
 		while(!queue.isEmpty()) {
 			Position now = queue.poll();
 			int nowX = now.x;
 			int nowY = now.y;
 			
-			// 4가지 방향 탐색 (상, 하, 좌, 우) => 배열에서는 하, 상, 좌, 우
+			// 4가지 방향 탐색 (상, 하, 좌, 우)
 			for(int i=0; i<4; i++) {
 				int nextX = nowX + dx[i];
 				int nextY = nowY + dy[i];
 				
-				// (0, 0) ~ (N-1, N-1) => [0][0] ~ [N-1][N-1] 이외의 좌표를 탐색한 경우
+				// (0, 0) ~ (N-1, N-1) 이외의 좌표를 탐색한 경우
 				if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) {
 					continue;
 				}
 				
-				// 이미 방문한 좌표거나 또는 해당 시작좌표의 컬러와 탐색한 좌표의 컬러값이 다른 경우
+				// 이미 방문한 좌표거나 탐색한 좌표가 인접한 컬러의 색상과 다른 경우
 				if(visited[nextX][nextY] || map[nextX][nextY] != tempColor) {
 					continue;
 				}
 				
-				queue.add(new Position(nextX, nextY));	// 탐색한 좌표정보 큐에 저장
-				visited[nextX][nextY] = true;	// 탐색한 좌표 방문처리
+				queue.add(new Position(nextX, nextY));
+				visited[nextX][nextY] = true;
 			}
 		}
 	}
