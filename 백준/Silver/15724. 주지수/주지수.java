34279fc131f4ae173fefae10537d175d98cc4b31
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-12-12 16:55:22 +0900

    [Silver I] Title: 주지수, Time: 632 ms, Memory: 120348 KB -BaekjoonHub

--- 백준/Silver/15724. 주지수/주지수.java
+++ 백준/Silver/15724. 주지수/주지수.java
@@ -3,10 +3,10 @@ import java.io.*;
 
 public class Main {
 	
-	static int N;
-	static int M;
-	static int K;
-	static int[][] sumMap;
+	static int N;	// 영토의 세로 크기
+	static int M;	// 영토의 가로 크기
+	static int K;	// 직사각형 범위의 개수를 저장할 변수
+	static int[][] dp;	// 누적합을 저장할 2차원 배열 dp
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,12 +15,14 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		sumMap = new int[N+1][M+1];
+		dp = new int[N+1][M+1];	// 누적합을 저장할 배열 초기화 [1][1] ~ [N][M]
 		for(int i=1; i<=N; i++) {
 			st = new StringTokenizer(br.readLine());
 			for(int j=1; j<=M; j++) {
 				int num = Integer.parseInt(st.nextToken());
-				sumMap[i][j] = sumMap[i][j-1] + num;
+				// 각 셀에 대해 왼쪽, 위쪽, 왼쪽 대각선 위쪽 셀의 누적합을 현재 셀값에 더한 후 적용
+				// 위 + 왼쪽 - 왼쪽 대각선 위 + 현재 셀 
+				dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + num;
 			}
 		}
 		
@@ -29,15 +31,13 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		for(int i=0; i<K; i++) {
 			st = new StringTokenizer(br.readLine());
-			int startX = Integer.parseInt(st.nextToken());
-			int startY = Integer.parseInt(st.nextToken());
-			int endX = Integer.parseInt(st.nextToken());
-			int endY = Integer.parseInt(st.nextToken());
-			int result = 0;
+			int startX = Integer.parseInt(st.nextToken());	// 시작 x 좌표
+			int startY = Integer.parseInt(st.nextToken());	// 시작 y 좌표
+			int endX = Integer.parseInt(st.nextToken());	// 끝 x 좌표
+			int endY = Integer.parseInt(st.nextToken());	// 끝 y 좌표
+			// 범위 내 사람 수 계산
+			int result = dp[endX][endY] - dp[startX-1][endY] - dp[endX][startY-1] + dp[startX-1][startY-1];
 			
-			for(int j=startX; j<=endX; j++) {
-				result += (sumMap[j][endY] - sumMap[j][startY - 1]);
-			}
 			sb.append(result).append("\n");
 			
 		}
