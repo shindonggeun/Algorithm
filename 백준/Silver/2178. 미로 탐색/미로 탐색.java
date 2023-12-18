5bd46a99fa5313f41cab283bdc406f6cd8074b2e
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-12-18 22:55:14 +0900

    [Silver I] Title: 미로 탐색, Time: 92 ms, Memory: 12312 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -3,11 +3,11 @@ import java.io.*;
 
 public class Main {
 	
-	// 좌표 및 이동한 거리를 저장해주는 내부 클래스
+	// 좌표 정보 및 이동한 칸 수를 담고있는 내부 클래스
 	static class Position {
 		int x;
 		int y;
-		int distance;
+		int distance;	// 이동한 칸수 (거리)
 		
 		public Position(int x, int y, int distance) {
 			this.x = x;
@@ -16,15 +16,14 @@ public class Main {
 		}
 	}
 	
-	static int N;	// 행의 개수
-	static int M;	// 열의 개수
+	static int N;	// 세로 크기
+	static int M;	// 가로 크기
 	static int[][] map;
-	static boolean[][] visited;
+	static boolean[][] visited;	// 각 좌표 방문여부를 나타내는 방문배열
+	// 4가지 방향 배열 (하, 상, 좌, 우)
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
 	
-	// 4가지 방향 배열 (상, 하, 좌, 우) -> 배열에서는 하, 상, 좌, 우
-	static int[] dx = {1, -1, 0, 0};	// x축이 고정되어 있을 때 y좌표가 움직이는 방향 배열 (상, 하)
-	static int[] dy = {0, 0, -1, 1};	// y축이 고정되어 있을 때 x좌표가 움직이는 방향 배열 (좌, 우)
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -32,7 +31,7 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		map = new int[N][M];	// (0, 0) ~ (M-1, N-1) -> [0][0] ~ [N-1][M-1]
+		map = new int[N][M];	// [0][0] ~ [N-1][M-1]
 		visited = new boolean[N][M];
 		
 		for(int i=0; i<N; i++) {
@@ -42,52 +41,54 @@ public class Main {
 			}
 		}
 		
-		int minDistance = bfs(0, 0);	// 시작위치 (0, 0)부터 너비우선탐색 시작
-		System.out.println(minDistance);
+		// 시작위치([0][0])에서 도착위치([N-1][M-1])까지의 최소 이동 거리 구하기
+		int resultDistance = bfs(0, 0);	
+		System.out.println(resultDistance);
 	}
 	
-	// 너비우선탐색 메서드
+	// 시작위치에서 도착위치까지의 최소 이동거리를 구하는 너비우선탐색 메서드
 	public static int bfs(int startX, int startY) {
+		// 너비우선탐색 알고리즘을 이용하기 위해 큐 선언 및 생성
 		Queue<Position> queue = new LinkedList<>();
-		queue.add(new Position(startX, startY, 1));	// 큐에 해당 시작좌표와 거리 정보 넣음
-		visited[startX][startY] = true;	// 시작좌표 방문처리
+		queue.add(new Position(startX, startY, 1));	// 시작좌표 정보 및 이동거리 큐에 저장
+		visited[startX][startY] = true;	// 시작 위치 방문처리
 		
-		// 큐가 빌때까지 반복
+		// 너비우선탐색 실시
 		while(!queue.isEmpty()) {
+			// 현재 좌표 정보 뽑아내기
 			Position now = queue.poll();
 			int nowX = now.x;
 			int nowY = now.y;
 			int nowDistance = now.distance;
 			
-			// 끝 좌표에 도달한 경우
+			// 현재 좌표 정보가 도착위치([N-1][M-1])에 도달한 경우 
 			if(nowX == N-1 && nowY == M-1) {
-				return nowDistance;	// 이동한 거리 반환
+				return nowDistance;	// 현재까지의 이동 거리 반환
 			}
 			
-			// 4가지 방향 탐색 (상, 하, 좌, 우) -> 배열에서는 하, 상, 좌, 우
+			// 4가지 방향 탐색 (하, 상, 좌, 우)
 			for(int i=0; i<4; i++) {
 				int nextX = nowX + dx[i];
 				int nextY = nowY + dy[i];
 				
-				// 탐색한 좌표가 (0, 0) ~ (M-1, N-1) -> [0][0] ~ [N-1][M-1] 이외의 좌표를 탐색한 경우 
+				// 탐색한 좌표가 [0][0] ~ [N-1][M-1] 이외의 좌표인 경우
 				if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
 					continue;	// 넘어감
 				}
 				
-				// 이미 방문한 좌표거나 또는 이동할 수 없는 칸(0)인 경우
+				// 탐색한 좌표가 이미 방문했거나 또는 이동할수 없는 칸(0)인 경우
 				if(visited[nextX][nextY] || map[nextX][nextY] == 0) {
 					continue;	// 넘어감
 				}
 				
-				queue.add(new Position(nextX, nextY, nowDistance+1));
-				visited[nextX][nextY] = true;	// 탐색한 좌표 방문처리
+				// 탐색한 좌표 정보 큐에 저장
+				queue.add(new Position(nextX, nextY, nowDistance + 1));
+				visited[nextX][nextY] = true;	// 탐색한 좌표 방문 처리
 			}
-			
 		}
 		
-		// 너비우선탐색을 다 거쳤는데도 끝 좌표에 도달하지 못한 경우 0 반환 
-		// 근데 문제에서 도착위치로 이동할 수 있는 경우만 입력으로 주어진다고 했으므로 여기 과정은 거치지 않는다
-		return 0;
+		// 너비우선탐색을 실시했는데도 도착위치로 이동할 수 없는 경우 -1 반환 (문제에서는 항상 도착위치로 이동함)
+		return -1;
 	}
 
 }
