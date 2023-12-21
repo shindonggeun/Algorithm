2e52c70656f2241e4d195bdc6c363ca911be0efd
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-12-21 17:39:05 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 112 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -13,55 +13,50 @@ public class Main {
 		}
 	}
 	
-	static int N;	// 수빈이가 있는 위치
-	static int K;	// 동생이 있는 위치
+	static int N;
+	static int K;
 	static int[] dist;
-	static boolean[] visited;
-	static final int MAX = 100000;
 	static final int INF = Integer.MAX_VALUE;
+	static final int MAX = 100000;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		
-		dist = new int[MAX + 1];
-		visited = new boolean[MAX + 1];
+		dist = new int[MAX+1];
 		Arrays.fill(dist, INF);
 		
-		dijkstra(N);
+		bfsZeroOne(N);
+		
 		System.out.println(dist[K]);
 	}
 	
-	public static void dijkstra(int start) {
-		PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
-		pq.add(new Edge(start, 0));
+	public static void bfsZeroOne(int start) {
+		Deque<Edge> deque = new ArrayDeque<>();
+		deque.add(new Edge(start, 0));
 		dist[start] = 0;
 		
-		while(!pq.isEmpty()) {
-			Edge now = pq.poll();
+		while(!deque.isEmpty()) {
+			Edge now = deque.poll();
 			int nowPosition = now.toVertex;
 			int nowTime = now.weight;
 			
-			if(!visited[nowPosition]) {
-				visited[nowPosition] = true;
-				
-				if(nowPosition - 1 >= 0 && dist[nowPosition -1] > nowTime + 1) {
-					dist[nowPosition - 1] = nowTime + 1;
-					pq.add(new Edge(nowPosition - 1, nowTime + 1));
-				}
-				
-				if(nowPosition + 1 <= MAX && dist[nowPosition + 1] > nowTime + 1) {
-					dist[nowPosition + 1] = nowTime + 1;
-					pq.add(new Edge(nowPosition + 1, nowTime + 1));
-				}
-				
-				if(nowPosition * 2 <= MAX && dist[nowPosition * 2] > nowTime) {
-					dist[nowPosition * 2] = nowTime;
-					pq.add(new Edge(nowPosition * 2, nowTime));
-				}
+			if(nowPosition * 2 <= MAX && dist[nowPosition * 2] > nowTime) {
+				dist[nowPosition * 2] = nowTime;
+				deque.addFirst(new Edge(nowPosition * 2, nowTime));
+			}
+			
+			if(nowPosition - 1 >= 0 && dist[nowPosition - 1] > nowTime + 1) {
+				dist[nowPosition - 1] = nowTime + 1;
+				deque.addLast(new Edge(nowPosition - 1, nowTime + 1));
+			}
+			
+			if(nowPosition + 1 <= MAX && dist[nowPosition + 1] > nowTime + 1) {
+				dist[nowPosition + 1] = nowTime + 1;
+				deque.addLast(new Edge(nowPosition + 1, nowTime + 1));
 			}
 		}
 	}
