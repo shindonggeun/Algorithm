6b63d78a6d376a5fe3fa96d539776b4f8c83edc4
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-12-28 15:39:17 +0900

    [Gold IV] Title: 운동, Time: 492 ms, Memory: 58496 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -33,10 +33,6 @@ public class Main {
 		for(int k=1; k<=V; k++) {
 			for(int i=1; i<=V; i++) {
 				for(int j=1; j<=V; j++) {
-					if(i == j) {
-						continue;
-					}
-					
 					dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
 				}
 			}
