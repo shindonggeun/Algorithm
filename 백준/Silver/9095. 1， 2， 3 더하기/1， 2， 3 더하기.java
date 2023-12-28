1e7b42a0744a59af53a8cf5b9394a4c0dd79214b
동긔 <42789808+shindonggeun@users.noreply.github.com>
2023-12-29 00:13:46 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 76 ms, Memory: 11448 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,22 +1,23 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
 
-	static int N;
-	static int[] dp;	// 각 해당하는 수를 1, 2, 3의 합으로 나타내는 방법의 수 (dp배열)
+	static int N;	
+	static int[] dp;	// 각 숫자를 1, 2, 3의 합으로 나타내는 방법의 수를 저장한 dp 배열 
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int testCase = Integer.parseInt(br.readLine());
 		
-		dp = new int[11];	// [0] ~ [10]
-		dp[1] = 1;
-		dp[2] = 2;
-		dp[3] = 4;
+		// dp 배열 초기화
+		dp = new int[11];	// [0] ~ [10] (문제에서 n은 11보다 작음)
+		dp[1] = 1;	// 1을 만드는 방법은 1가지
+		dp[2] = 2;	// 2을 만드는 방법은 2가지 (1+1, 2)
+		dp[3] = 4;	// 3을 만드는 방법은 4가지 (1+1+1, 1+2, 2+1, 3)
 		
+		// 4부터 10까지 각 숫자를 만드는 방법의 수 계산
 		for(int i=4; i<=10; i++) {
-			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];	// 점화식 적용
 		}
 		
 		StringBuilder sb = new StringBuilder();
