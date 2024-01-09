7b5c8b55cade58f94802db3ed5848a1e6513484b
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-01-10 02:02:36 +0900

    [Gold III] Title: 택배, Time: 308 ms, Memory: 19064 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.java
+++ 백준/Gold/1719. 택배/택배.java
@@ -3,37 +3,31 @@ import java.io.*;
 
 public class Main {
 	
-	static class Edge {
-		int toVetex;
-		int weight;
-		
-		public Edge(int toVertex, int weight) {
-			this.toVetex = toVertex;
-			this.weight = weight;
-		}
-	}
-	
-	static int N;
-	static int M;
-	static int[] dist;
-	static boolean[] visited;
-	static final int INF = Integer.MAX_VALUE;
-	static ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
-	static int[][] pathMap;
-	
+	static int N;	// 집하장의 개수
+	static int M;	// 집하장 간 경로의 개수
+	static int[][] dist;	// 각 집하장 간 최단 거리를 저장할 배열
+	// 각 집하장에서 다음 집하장까지 최단 경로를 통해 이동할 때 제일 먼저 이동해야활 집하장을 저장할 배열 (경로 저장)
+	static int[][] path;	
+	static final int INF = 987654321;	// 무한대를 나타내는 값
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		dist = new int[N+1];
-		visited = new boolean[N+1];
-		pathMap = new int[N+1][N+1];
+		dist = new int[N+1][N+1];	// [1][1] ~ [N][N]
+		path = new int[N+1][N+1];
 		
-		for(int i=0; i<=N; i++) {
-			graph.add(new ArrayList<>());
+		for(int i=1; i<=N; i++) {
+			Arrays.fill(dist[i], INF);	// 각 집하장의 모든 경로 무한대로 초기화
+			dist[i][i] = 0;	// 자기 자신으로의 거리는 0으로 초기화
+			for(int j=1; j<=N; j++) {
+				if(i != j) {
+					path[i][j] = j;	// 다른 집하장으로 가는 최초 경로를 해당 집하장으로 초기화
+				}
+			}
 		}
 		
 		for(int i=0; i<M; i++) {
@@ -42,50 +36,37 @@ public class Main {
 			int toVertex = Integer.parseInt(st.nextToken());
 			int weight = Integer.parseInt(st.nextToken());
 			
-			graph.get(fromVertex).add(new Edge(toVertex, weight));
-			graph.get(toVertex).add(new Edge(fromVertex, weight));
+			dist[fromVertex][toVertex] = weight;
+			dist[toVertex][fromVertex] = weight;
 		}
 		
-		for(int i=1; i<=N; i++) {
-			Arrays.fill(dist, INF);
-			Arrays.fill(visited, false);
-			dijkstra(i);
-		}
+		floydWarshall();	// 플로이드-워셜 알고리즘을 이용한 메서드 호출
 		
 		StringBuilder sb = new StringBuilder();
 		for(int i=1; i<=N; i++) {
 			for(int j=1; j<=N; j++) {
 				if(i == j) {
-					sb.append("-").append(" ");
+					sb.append("-").append(" ");	// 자기 자신으로의 경로는 '-'로 표시
 				}
 				else {
-					sb.append(pathMap[i][j]).append(" ");
+					sb.append(path[i][j]).append(" ");	// 최단 경로의 첫 번째 집하장 출력
 				}
 			}
 			sb.append("\n");
 		}
+		
 		System.out.print(sb);
 	}
 	
-	public static void dijkstra(int start) {
-		PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
-		pq.add(new Edge(start, 0));
-		dist[start] = 0;
-		
-		while(!pq.isEmpty()) {
-			Edge now = pq.poll();
-			int to = now.toVetex;
-			int weight = now.weight;
-			
-			if(!visited[to]) {
-				visited[to] = true;
-				
-				for(Edge next: graph.get(to)) {
-					int cost = next.weight + weight;
-					if(cost < dist[next.toVetex]) {
-						dist[next.toVetex] = cost;
-						pq.add(new Edge(next.toVetex, dist[next.toVetex]));
-						pathMap[next.toVetex][start] = to;
+	// 각 집하장들의 최단거리를 구해주는 메서드 (플로이드-워셜 알고리즘 이용)
+	public static void floydWarshall() {
+		for(int k=1; k<=N; k++) {
+			for(int i=1; i<=N; i++) {
+				for(int j=1; j<=N; j++) {
+					// i에서 j로 가는 최단 거리가 i에서 k를 거쳐 k에서 j로 가는 거리보다 긴 경우
+					if(dist[i][k] + dist[k][j] < dist[i][j]) {
+						dist[i][j] = dist[i][k] + dist[k][j];	// 최단 경로 (거리)를 업데이트
+						path[i][j] = path[i][k];	// 최단 경로의 첫 번째 집하장을 업데이트
 					}
 				}
 			}
