e479a6a11b0aa56477804600a2e869776df39c45
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-03-24 00:00:04 +0900

    [Gold IV] Title: 부분합, Time: 248 ms, Memory: 22544 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -2,45 +2,55 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	
+	static int N;
+	static int S;
+	static int[] numArr;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		S = Integer.parseInt(st.nextToken());
+		numArr = new int[N+1];
 		
-		int N = Integer.parseInt(st.nextToken());
-		int S = Integer.parseInt(st.nextToken());
-		
-		int[] numArr = new int[N+1];
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
+		for (int i=0; i<N; i++) {
 			numArr[i] = Integer.parseInt(st.nextToken());
 		}
 		
 		// 투포인터 알고리즘 이용
-		int minLength = Integer.MAX_VALUE;
-		int left = 0;
-		int right = 0;
-		int sum = 0;
-		int tempLength = 0;
+		int leftIdx = 0;	// 왼쪽 인덱스
+		int rightIdx = 0;	// 오른쪽 인덱스
+		// 연속된 수들의 부분합 중 그 합이 S 이상이 될 때의 가장 짧은 길이 (결과값)
+		int minLength = Integer.MAX_VALUE;	
+		int sum = 0;	// 연속된 수들의 부분합 (누적합)
+		int tempLength = 0;	 // 연속된 수들의 부분합 중 그 합이 S 이상이 될 때의 길이
 		
-		while(right <= N) {
-			// 합이 S 이상인 경우
-			if(sum >= S) {
-				sum -= numArr[left++]; 
-				tempLength = right - left + 1;	// 길이를 구하기
-				minLength = Math.min(minLength, tempLength);	// 길이 최소값 갱신
+		// 오른쪽 인덱스가 끝까지 탐색할 수 있도록
+		while (rightIdx <= N) {
+			// 연속된 수들의 부분합이 S 미만인 경우
+			if (sum < S) {
+				sum += numArr[rightIdx];	// 누적합
+				rightIdx++;	// 오른쪽 인덱스 증가
 			}
-			else if(sum < S) {
-				sum += numArr[right++];
+			// 연속된 수들의 부분합이 S 이상인 경우
+			else {
+				sum -= numArr[leftIdx];	// 누적합에서 왼쪽 인덱스가 가리키는 값 빼줌
+				leftIdx++;	// 왼쪽 인덱스 증가
+				tempLength = rightIdx - leftIdx + 1;	// 길이 구하기
+				minLength = Math.min(minLength, tempLength);	// 가장 짧은 길이 갱신
 			}
 		}
-		if(minLength == Integer.MAX_VALUE) {
+		
+		// 연속된 수들의 부분합 중 그 합이 S 이상이 되는거 못만드는 경우
+		if (minLength == Integer.MAX_VALUE) {
 			System.out.println(0);
 		}
 		else {
 			System.out.println(minLength);
 		}
-
 	}
 
 }
