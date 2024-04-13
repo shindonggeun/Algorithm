2066eaebdb6c013ae1c96e9184fb82b8f9638bd8
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-04-13 17:33:52 +0900

    [Gold V] Title: 적록색약, Time: 92 ms, Memory: 13072 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -3,7 +3,6 @@ import java.io.*;
 
 public class Main {
 	
-	// 좌표정보를 담은 내부 클래스
 	static class Position {
 		int x;
 		int y;
@@ -14,96 +13,98 @@ public class Main {
 		}
 	}
 	
-	static int N;	// 맵의 크기
-	static char[][] colorMap;	// 각 칸의 컬러비트를 담은 맵
-	static boolean[][] visited;	// 각 좌표 방문여부 배열
+	static int N;
+	static char[][] map;
+	static boolean[][] visited;
+	static int normalCount;	// 정상인 사람이 봤을 때 구역의 개수
+	static int abnormalCount;	// 적록색약인 사람이 봤을 때 구역의 개수
 	// 4가지 방향 배열 (하, 상, 좌, 우)
-	static int[] dx = {-1, 1, 0, 0};
+	static int[] dx = {1, -1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
-		colorMap = new char[N][N];	// [0][0] ~ [N-1][N-1]
+		
+		map = new char[N][N];
+		visited = new boolean[N][N];
+		normalCount = 0;
+		abnormalCount = 0;
+		
+		for (int i=0; i<N; i++) {
+			String input = br.readLine();
+			for (int j=0; j<N; j++) {
+				map[i][j] = input.charAt(j);
+			}
+		}
+		
+		// 1. 정상적인 사람이 봤을 때 구역의 개수 구하기
+		for (int i=0; i<N; i++) {
+			for (int j=0; j<N; j++) {
+				if (!visited[i][j]) {
+					bfs(i, j);
+					normalCount++;
+				}
+			}
+		}
+		
+		// 2. 빨간색과 초록색 같게끔 만들어주기 (R == G)
+		for (int i=0; i<N; i++) {
+			for (int j=0; j<N; j++) {
+				// 해당 좌표가 빨간색인 경우
+				if (map[i][j] == 'R') {
+					map[i][j] = 'G';	// 초록색으로 만들어주기
+				}
+			}
+		}
+		
+		// 3. 적록색약인 사람이 봤을 때 구역의 개수 구하기 
 		visited = new boolean[N][N];
 		
-		for(int i=0; i<N; i++) {
-			String input = br.readLine();
-			for(int j=0; j<N; j++) {
-				colorMap[i][j] = input.charAt(j);
-			}
-		}
-		
-		int normalAreaCount = 0;	// 적록색약이 아닌 정상인 사람이 봤을 때의 구역의 개수
-		
-		// 적록색약이 아닌 정상인 사람이 그리드를 봤을 때의 구역의 개수 찾는 과정
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				// 해당 좌표가 방문처리 안된 경우
-				if(!visited[i][j]) {
-					colorBfs(i, j, colorMap[i][j]);	// 해당 구역의 색상 찾는 너비우선탐색 메서드 호출
-					normalAreaCount++;	// 정상인 사람이 봤을 때의 구역의 개수 증가
+		for (int i=0; i<N; i++) {
+			for (int j=0; j<N; j++) {
+				if (!visited[i][j]) {
+					bfs(i, j);
+					abnormalCount++;
 				}
 			}
 		}
 		
-		// 적록색약이 구별할 수 없게끔 초록 색상을 빨강색상으로 만드는 과정
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				if(colorMap[i][j] == 'G') {
-					colorMap[i][j] = 'R';
-				}
-			}
-		}
 		
-		visited = new boolean[N][N];	// 다시 방문배열 초기화
-		int abnormalAreaCount = 0;	// 적록색약인 사람이 봤을 때의 구역의 개수
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<N; j++) {
-				// 해당 좌표가 방문처리 안된 경우
-				if(!visited[i][j]) {
-					colorBfs(i, j, colorMap[i][j]);	// 해당 구역의 색상 찾는 너비우선탐색 메서드 호출
-					abnormalAreaCount++;	// 적록색약인 사람이 봤을 때의 구역의 개수 증가
-				}
-			}
-		}
-		
-		System.out.println(normalAreaCount + " " + abnormalAreaCount);
-		
+		System.out.println(normalCount + " " + abnormalCount);
+
 	}
 	
-	// 해당 구역의 색상 찾는 메서드 (너비우선탐색 알고리즘 이용)
-	public static void colorBfs(int startX, int startY, char colorBit) {
-		// 너비우선탐색을 이용하기 위해 큐 선언 및 생성
+	// 너비우선탐색 메서드
+	public static void bfs(int startX, int startY) {
 		Queue<Position> queue = new LinkedList<>();
-		queue.add(new Position(startX, startY));	// 해당 시작좌표의 정보 큐에 저장
-		visited[startX][startY] = true;	// 해당 시작 좌표 방문처리
+		queue.add(new Position(startX ,startY));
+		visited[startX][startY] = true;	// 시작좌표 방문 처리
+		char color = map[startX][startY];	// 해당 시작좌표의 색상 추출
 		
-		// 너비우선탐색 알고리즘 이용
-		while(!queue.isEmpty()) {
-			// 현재 좌표 정보 뽑아냄
+		while (!queue.isEmpty()) {
 			Position now = queue.poll();
 			int nowX = now.x;
 			int nowY = now.y;
 			
-			// 4가지 방향 탐색 (하 ,상, 좌, 우)
-			for(int i=0; i<4; i++) {
+			// 4가지 방향 탐색
+			for (int i=0; i<4; i++) {
 				int nextX = nowX + dx[i];
 				int nextY = nowY + dy[i];
 				
-				// 탐색한 좌표가 [0][0] ~ [N-1][N-1] 이외의 좌표인 경우
-				if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) {
-					continue;	// 넘어감
+				// [0][0] ~ [N-1][N-1] 이외의 좌표인 경우
+				if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) {
+					continue;	// 다음 좌표 탐색하게끔 넘어감
 				}
 				
-				// 탐색한 좌표가 방문처리 됐거나 또는 탐색한 좌표의 색상이 시작했던 컬러 색상 비트와 다른 경우
-				if(visited[nextX][nextY] || colorMap[nextX][nextY] != colorBit) {
-					continue;	// 넘어감
+				// 탐색한 좌표가 이미 방문했거나 또는 탐색한 좌표가 해당 시작좌표의 색상과 다른 경우
+				if (visited[nextX][nextY] || map[nextX][nextY] != color) {
+					continue;	// 다음 좌표 탐색하게끔 넘어감
 				}
 				
-				// 위의것들의 경우가 아닌 경우 탐색한 좌표 정보 큐에 저장
+				// 그 이외의 경우는 큐에 좌표정보 집어넣음
 				queue.add(new Position(nextX, nextY));
-				visited[nextX][nextY] = true;	// 탐색한 좌표 방문 처리
+				visited[nextX][nextY] = true;
 			}
 		}
 	}
