f30a048e0fec6d4c8f8f00c9dd0c2b0c65690827
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-04-23 02:11:34 +0900

    [Gold V] Title: 토마토, Time: 612 ms, Memory: 122160 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -3,23 +3,26 @@ import java.io.*;
 
 public class Main {
 	
+	// 좌표 정보를 담고있는 내부 클래스
 	static class Position {
-		int x, y;
+		int x;
+		int y;
 		
 		public Position(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
 	}
-
-	static int M;	// 가로 칸 수(x좌표)
-	static int N;	// 세로 칸 수(y좌표)
-	static int[][] map;
-	static boolean[][] visited;
-	// 4가지 방향 (상, 하, 좌, 우) 
-	static int[] dx = {1, -1, 0, 0};	// x축이 고정되어 있을 때 y좌표가 움직이는 방향 배열 (상, 하)
-	static int[] dy = {0, 0, -1, 1};	// y축이 고정되어 있을 때 x좌표가 움직이는 방향 배열 (좌, 우)
-	static Queue<Position> queue = new LinkedList<>();
+	
+	static int M;	// 가로 칸 수
+	static int N;	// 세로 칸 수
+	static int[][] map;	// 토마토 상자의 상태를 담고있는 배열
+	static boolean[][] visited;	// 방문 배열
+	// 4가지 방향 배열 (하, 상, 좌, 우)
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	static Queue<Position> queue;	// 너비우선탐색을 이용하기 위한 큐 선언
+	static int maxTomato;	// 모든 토마토가 익는데 걸리는 최대 일수
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,92 +30,86 @@ public class Main {
 		
 		M = Integer.parseInt(st.nextToken());
 		N = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][M];	// (0, 0) ~ (M-1, N-1)
+		map = new int[N][M];	// [0][0] ~ [N-1][M-1]
 		visited = new boolean[N][M];
+		queue = new LinkedList<>();	// 큐 생성 및 초기화
+		maxTomato = Integer.MIN_VALUE;	// 최대 일수를 저장하기 위해 일단 최소값으로 초기화
 		
-		for(int i=0; i<N; i++) {
+		for (int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<M; j++) {
+			for (int j=0; j<M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
-				// 토마토가 들어있는 칸이면 큐에 집어넣음(너비우선탐색 돌리기 위해)
-				if(map[i][j] == 1) {
+				// 해당 좌표가 토마토가 익은 경우(1)
+				if (map[i][j] == 1) {
+					// 큐에 현재 좌표정보 추가
 					queue.add(new Position(i, j));
 				}
 			}
 		}
-		int minDays = bfs();
 		
-		System.out.println(minDays);
+		int minDay = bfs();	// 너비우선탐색 메서드 실행하여 토마토가 익는데 최소 일수 계산
+		System.out.println(minDay);
 	}
 	
+	// 너비우선탐색 메서드
 	public static int bfs() {
-		while(!queue.isEmpty()) {
+		while (!queue.isEmpty()) {
 			Position now = queue.poll();
 			int nowX = now.x;
 			int nowY = now.y;
 			visited[nowX][nowY] = true;
 			
-			// 4가지 방향 탐색
-			for(int i=0; i<4; i++) {
+			// 4가지 방향 탐색 (하, 상, 좌, 우)
+			for (int i=0; i<4; i++) {
 				int nextX = nowX + dx[i];
 				int nextY = nowY + dy[i];
 				
-				// 탐색한 좌표가 (0, 0) ~ (M-1, N-1) 이외의 좌표인 경우 넘어감
-				if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
-					continue;
+				// 탐색한 좌표가 [0][0] ~ [N-1][M-1] 이외의 좌표인 경우
+				if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
+					continue;	// 다음 방향 탐색하도록 넘어감
 				}
 				
-				// 탐색한 좌표가 이미 방문한 좌표거나 토마토가 들어있지 않은 칸(-1)인 경우
-				if(visited[nextX][nextY] || map[nextX][nextY] == -1) {
-					continue;
+				// 탐색한 좌표가 이미 방문처리 됐거나 또는 토마토가 들어있지 않은 경우(-1)
+				if (visited[nextX][nextY] || map[nextX][nextY] == -1) {
+					continue;	// 다음 방향 탐색하도록 넘어감
 				}
 				
-				// 익지않은 토마토만 탐색해줘야한다!!
-				if(map[nextX][nextY] == 0) {
-					map[nextX][nextY] = map[nowX][nowY] + 1;
-					visited[nextX][nextY] = true;
-					queue.add(new Position(nextX, nextY));
+				// 탐색한 좌표가 익지 않은 토마토인 경우(0)
+				if (map[nextX][nextY] == 0) {
+					map[nextX][nextY] = map[nowX][nowY] + 1;	// 익은 토마토로 변경한 뒤 일수 증가
+					queue.add(new Position(nextX, nextY));	// 탐색한 좌표 정보 큐에 추가
 				}
-				
 			}
 		}
 		
-		// 위의 탐색과정 다 거치면 map에서 최대값을 뽑아내야 함
-		// 만약 map에서 0이 존재하면 -1을 출력해야함
-		if(!checkZero(map)) {
-			return -1;
+		// 토마토가 모두 익은 경우
+		if (checkAllTomato()) {
+			// 최대 일수에서 - 1 빼준 값 반환
+			return maxTomato - 1;
 		}
 		else {
-			int max = findMax(map);
-			return max-1;
+			// 토마토가 모두 익지 않은 경우 -1 반환
+			return -1;	
 		}
-	}
-	
-	// map에서 0이 존재하는지 여부를 판단해주는 메서드
-	public static boolean checkZero(int[][] map) {
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				// 토마토가 익지 않은게 존재하면 false
-				if(map[i][j] == 0) {
-					return false;
-				}
-			}
-		}
-		// 위 과정 다 탐색했는데도 0 없으면 true (토마토 모두 익은 상태임)
-		return true;
-	}
-	
-	// 2차원 배열에서 최대값 뽑아내는 메서드
-	public static int findMax(int[][] map) {
-		int max = Integer.MIN_VALUE;
 		
-		for(int[] array: map) {
-			for(int element: array) {
-				max = Math.max(max, element);
+	}
+	
+	// 토마토가 모두 익었는지 체크하는 메서드
+	public static boolean checkAllTomato() {
+		for (int i=0; i<N; i++) {
+			for (int j=0; j<M; j++) {
+				// 해당 좌표에서 토마토가 익지 않은 경우(0)
+				if (map[i][j] == 0) {
+					return false;	// 토마토 모두 익지 않았음
+				}
+				
+				// 최대 일수 갱신
+				maxTomato = Math.max(maxTomato, map[i][j]);
 			}
 		}
-		return max;
+		
+		// 모든 토마토가 익었음
+		return true;
 	}
 
 }
