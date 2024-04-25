a76437e09d4a29a8f7ac96e1d68fae4463df11bb
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-04-25 23:43:13 +0900

    [Gold V] Title: 보물섬, Time: 460 ms, Memory: 292560 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.java
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -3,8 +3,10 @@ import java.io.*;
 
 public class Main {
 	
+	// 좌표 정보 및 거리를 담고있는 내부 클래스
 	static class Position {
-		int x, y;
+		int x;
+		int y;
 		int distance;
 		
 		public Position(int x, int y, int distance) {
@@ -14,76 +16,87 @@ public class Main {
 		}
 	}
 	
-	static int N;	// 세로 길이 (y 좌표)
-	static int M;	// 가로 길이 (x 좌표)
+	static int L;	// 세로 크기
+	static int W;	// 가로 크기
 	static char[][] map;
-	static boolean[][] visited;
-	static int minDistance;
-	// 4가지 방향 배열 (상, 하, 좌, 우)
-	static int[] dx = {1, -1, 0, 0};	// x축이 고정되어 있을 때 y좌표가 움직이는 방향 배열 (상, 하)
-	static int[] dy = {0, 0, -1, 1};	// y축이 고정되어 있을 때 x좌표가 움직이는 방향 배열 (좌, 우)
-	
+	static boolean[][] visited;	// 방문배열
+	// 4가지 방향 배열 (하, 상, 좌, 우)
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+	static int resultDistance;	// 보물이 묻혀 있는 두 곳 사이의 최단 거리
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new char[N][M];	// (0, 0) ~ (M-1, N-1)
+		L = Integer.parseInt(st.nextToken());
+		W = Integer.parseInt(st.nextToken());
 		
-		for(int i=0; i<N; i++) {
+		map = new char[L][W];	// [0][0] ~ [L-1][W-1]
+		
+		for (int i=0; i<L; i++) {
 			String input = br.readLine();
-			for(int j=0; j<M; j++) {
+			for (int j=0; j<W; j++) {
 				map[i][j] = input.charAt(j);
 			}
 		}
 		
-		// 브루트포스 알고리즘 이용 (모든 좌표 완전탐색)
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				// 해당 맵의 좌표가 육지('L')인 경우
-				if(map[i][j] == 'L') {
-					bfs(i, j);	// 해당 좌표부터 너비우선 탐색 실시
+		resultDistance = 0;
+		
+		for (int i=0; i<L; i++) {
+			for (int j=0; j<W; j++) {
+				// 해당 좌표가 육지('L')인 경우
+				if (map[i][j] == 'L') {
+					visited = new boolean[L][W];	// 방문배열 초기화
+					int distance = bfs(i, j);	// 해당 좌표 너비우선탐색 실시
+					resultDistance = Math.max(resultDistance, distance);
 				}
 			}
 		}
 		
-		System.out.println(minDistance);
+		System.out.println(resultDistance);
+
 	}
 	
-	public static void bfs(int startX, int startY) {
+	// 보물이 묻혀 있는 두 곳 사이의 거리를 찾도록 해주는 너비우선탐색 메서드
+	public static int bfs(int startX, int startY) {
 		Queue<Position> queue = new LinkedList<>();
 		queue.add(new Position(startX, startY, 0));
-		visited = new boolean[N][M];
 		visited[startX][startY] = true;
+		// 보물이 묻혀 있는 두 곳 사이의 최단 거리 (문제에서는 최단거리라고 표현되어 있지만 가장 긴 길이 찾으면 됨)
+		int maxDistance = 0;	
 		
-		while(!queue.isEmpty()) {
+		// 너비우선탐색 시작
+		while (!queue.isEmpty()) {
 			Position now = queue.poll();
 			int nowX = now.x;
 			int nowY = now.y;
 			int nowDistance = now.distance;
 			
 			// 4가지 방향 탐색
-			for(int i=0; i<4; i++) {
+			for (int i=0; i<4; i++) {
 				int nextX = nowX + dx[i];
 				int nextY = nowY + dy[i];
 				
-				// 탐색한 좌표가 (0, 0) ~ (M-1, N-1) 이외의 좌표인 경우
-				if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
-					continue;
+				// 탐색한 좌표가 [0][0] ~ [L-1][W-1] 이외의 좌표인 경우
+				if (nextX < 0 || nextY < 0 || nextX >= L || nextY >= W) {
+					continue;	// 넘어감
 				}
 				
-				// 탐색한 좌표가 바다거나 또는 이미 방문한 좌표인 경우
-				if(map[nextX][nextY] == 'W' || visited[nextX][nextY]) {
-					continue;
+				// 탐색한 좌표가 이미 방문했거나 또는 바다('W')인 경우
+				if (visited[nextX][nextY] || map[nextX][nextY] == 'W') {
+					continue;	// 다음 방향 탐색하도록 넘어감
 				}
 				
+				// 탐색한 좌표 정보 큐에 저장
 				queue.add(new Position(nextX, nextY, nowDistance + 1));
-				visited[nextX][nextY] = true;
+				visited[nextX][nextY] = true;	// 탐색한 좌표 방문처리
+				maxDistance = Math.max(maxDistance, nowDistance + 1);	// 최단거리 갱신
 			}
-			
-			minDistance = Math.max(minDistance, nowDistance);	// 위의 4가지 방향 탐색 끝나면 최단거리 갱신
 		}
+		
+		// 너비우선탐색 끝났으면 보물이 묻혀있는 두 곳 사이의 최단거리 반환
+		return maxDistance;
 	}
 
 }
