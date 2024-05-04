c9b1adf2a261fd904ccd772810d5ae15bb11eff4
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-05-04 16:35:56 +0900

    [level 3] Title: 섬 연결하기, Time: 1.03 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,63 +2,81 @@ import java.util.*;
 
 class Solution {
     
+    // 간선의 정보를 담은 내부 클래스
     static class Edge {
+        int fromVertex;
         int toVertex;
         int weight;
         
-        public Edge(int toVertex, int weight) {
+        public Edge(int fromVertex, int toVertex, int weight) {
+            this.fromVertex = fromVertex;
             this.toVertex = toVertex;
             this.weight = weight;
         }
     }
     
-    static ArrayList<ArrayList<Edge>> graph;
-    static boolean[] visited;
+    static int[] parents;   // 크루스칼 알고리즘(유니온 파인드 응용)을 이용하기 위한 부모 배열
+    static ArrayList<Edge> edgeList;    // 간선 목록를 저장하는 리스트
     
     public int solution(int n, int[][] costs) {
         int answer = 0;
+        parents = new int[n+1]; // [1] ~ [n]
+        edgeList = new ArrayList<>();
         
-        graph = new ArrayList<>();
-        visited = new boolean[n];
-        for(int i=0; i<n; i++) {
-            graph.add(new ArrayList<>());
+        for (int i=1; i<=n; i++) {
+            parents[i] = i; // 각 정점의 부모는 자기 자신을 가리키도록 설정
         }
         
-        for(int[] cost: costs) {
+        for (int[] cost: costs) {
             int fromVertex = cost[0];
             int toVertex = cost[1];
             int weight = cost[2];
             
-            graph.get(fromVertex).add(new Edge(toVertex, weight));
-            graph.get(toVertex).add(new Edge(fromVertex, weight));
+            // 간선 정보를 간선 리스트에 저장
+            edgeList.add(new Edge(fromVertex, toVertex, weight));
         }
         
-        answer = prim(0);
-        return answer;
-    }
-    
-    public static int prim(int start) {
-        PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
-        pq.add(new Edge(start, 0));
-        int totalWeight = 0;
+        // 크루스칼 알고리즘을 이용하기 위해 (최소신장트리) 간선의 가중치 기준으로 오름차순 정렬
+        Collections.sort(edgeList, (a, b) -> a.weight - b.weight);
         
-        while(!pq.isEmpty()) {
-            Edge now = pq.poll();
-            int nowVertex = now.toVertex;
-            int nowWeight = now.weight;
-            if(!visited[nowVertex]) {
-                visited[nowVertex] = true;
-                totalWeight += nowWeight;
-                
-                for(Edge next: graph.get(nowVertex)) {
-                    if(!visited[next.toVertex]) {
-                        pq.add(next);
-                    }
-                }
-                
+        int totalWeight = 0;    // 최소신장트리를 형성할 때 드는 비용
+        
+        // 모든 간선에 대해서 순회
+        for (Edge edge: edgeList) {
+            int root1 = find(edge.fromVertex);  // 시작 정점의 루트
+            int root2 = find(edge.toVertex);    // 도착 정점의 루트
+            
+            // 두 정점의 루트가 다른 경우 
+            if (root1 != root2) {
+                union(edge.fromVertex, edge.toVertex);  // 두 트리를 합치게끔 유니온 연산 수행
+                totalWeight += edge.weight; // 최소신장트리를 형성할 때 드는 최소 비용에 해당 간선의 가중치 누적
             }
         }
         
-        return totalWeight;
+        answer = totalWeight;
+        
+        return answer;
+    }
+    
+    public int find(int a) {
+        if (a == parents[a]) {
+            return a;
+        }
+        return parents[a] = find(parents[a]);
+    }
+    
+    public void union(int a, int b) {
+        int aRoot = find(a);
+        int bRoot = find(b);
+        
+        if (aRoot == bRoot) {
+            return;
+        }
+        else if(aRoot > bRoot) {
+            parents[aRoot] = bRoot;
+        }
+        else {
+            parents[bRoot] = aRoot;
+        }
     }
 }
