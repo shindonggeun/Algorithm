7f160f0e0fc04d16497b1db5f58f32f44c133f6e
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-05-27 16:10:35 +0900

    [Silver III] Title: 수열, Time: 204 ms, Memory: 22340 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,41 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+	
+	static int N;
+	static int K;
+	static int[] temperature;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[N];
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		temperature = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i=0; i<N; i++) {
+			temperature[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		// 처음 K일 동안의 합을 미리 계산
         int sum = 0;
-        int cnt = 0;
-        int max = Integer.MIN_VALUE;
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-
-            arr[i] = num;
-
-            if (cnt == K) {
-                max = Math.max(max, sum);
-                sum -= arr[i - K];
-                cnt--;
-            }
-
-            if (cnt != K) {
-                sum += num;
-                cnt++;
-            }
+        for (int i = 0; i < K; i++) {
+            sum += temperature[i];
         }
-        System.out.println(Math.max(max, sum));
 
-    }
+        int max = sum;
+
+        // 슬라이딩 윈도우 적용
+        for (int i = K; i < N; i++) {
+        	sum += temperature[i] - temperature[i-K];
+            max = Math.max(max, sum);
+        }
+
+        System.out.println(max);
+	}
+
 }
