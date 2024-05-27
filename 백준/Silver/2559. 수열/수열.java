2d23ad3c5f1ce7d38b50506826001094d098fdb7
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-05-27 16:31:46 +0900

    [Silver III] Title: 수열, Time: 200 ms, Memory: 22104 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -3,9 +3,9 @@ import java.io.*;
 
 public class Main {
 	
-	static int N;
-	static int K;
-	static int[] temperature;
+	static int N;	// 전체 날짜 수
+	static int K;	// 연속적인 날짜의 수
+	static int[] temperature;	// 매일 측정한 온도를 저장할 배열
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,20 +14,20 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		
-		temperature = new int[N];
+		temperature = new int[N];	// [0] ~ [N]
 		
 		st = new StringTokenizer(br.readLine());
 		for (int i=0; i<N; i++) {
 			temperature[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		// 처음 K일 동안의 합을 미리 계산
+		// 처음 K일 동안의 온도의 합을 미리 계산
         int sum = 0;
         for (int i = 0; i < K; i++) {
             sum += temperature[i];
         }
 
-        int max = sum;
+        int max = sum;	// 연속적인 K일의 온도의 합이 최대가 되는 값
 
         // 슬라이딩 윈도우 적용
         for (int i = K; i < N; i++) {
