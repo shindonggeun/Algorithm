a6b3046257d5e773626f52841b7cab900cc0d7f1
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-05-28 11:04:31 +0900

    [Gold IV] Title: 네트워크 연결, Time: 640 ms, Memory: 51284 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -3,12 +3,14 @@ import java.io.*;
 
 public class Main {
 	
-	// 컴퓨터끼리 연결할 때 간선 정보를 담은 내부 클래스
+	// 간선 정보를 나타내는 내부 클래스
 	static class Edge {
-		int toVertex;	// 도착지 컴퓨터 (정점)
-		int weight;	// 컴퓨터를 연결하는데 필요한 비용 (가중치) 
+		int fromVertex;	// 시작 정점
+		int toVertex;	// 도착 정점
+		int weight;	// 가중치
 		
-		public Edge(int toVertex, int weight) {
+		public Edge(int fromVertex, int toVertex, int weight) {
+			this.fromVertex = fromVertex;
 			this.toVertex = toVertex;
 			this.weight = weight;
 		}
@@ -16,9 +18,9 @@ public class Main {
 	
 	static int N;	// 컴퓨터의 수 (정점의 수)
 	static int M;	// 연결할 수 있는 선의 수 (간선의 수)
-	static ArrayList<ArrayList<Edge>> graph = new ArrayList<>();	// 프림 알고리즘에 사용할 그래프 (인접 리스트 이용)
-	static boolean[] visited;	// 각 컴퓨터 (정점) 방문 여부
-	
+	static ArrayList<Edge> edgeList;	// 간선들을 저장하는 리스트
+	static int[] parents;	// 크루스칼 알고리즘(유니온 파인드 응용)을 이용하기 위한 부모 배열
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
@@ -26,61 +28,65 @@ public class Main {
 		N = Integer.parseInt(br.readLine());
 		M = Integer.parseInt(br.readLine());
 		
-		visited = new boolean[N+1];	// [1] ~ [N]
-		// 각 정점에 연결된 간선들 표현하기 위해 각 정점들 생성 (그래프 초기화 작업)
-		for(int i=0; i<=N; i++) {
-			graph.add(new ArrayList<>());
+		edgeList = new ArrayList<>();
+		parents = new int[N+1];	// [1] ~ [N]
+		
+		for (int i=1; i<=N; i++) {
+			parents[i] = i;	// 각 정점의 부모를 자기 자신으로 설정
 		}
 		
-		for(int i=0; i<M; i++) {
+		for (int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int fromVertex = Integer.parseInt(st.nextToken());
-			int toVertex = Integer.parseInt(st.nextToken());
-			int weight = Integer.parseInt(st.nextToken());
+			int fromVertex = Integer.parseInt(st.nextToken());	// 시작 컴퓨터 번호 (시작 정점)
+			int toVertex = Integer.parseInt(st.nextToken());	// 도착 컴퓨터 번호 (도착 정점)
+			int weight = Integer.parseInt(st.nextToken());	// 비용 (가중치)
 			
-			// 양방향 간선 연결 (프림 알고리즘 -> 최소 신장 트리에서는 무방향 그래프여야 한다, 즉 서로 연결시켜줘야 함)
-			graph.get(fromVertex).add(new Edge(toVertex, weight));
-			graph.get(toVertex).add(new Edge(fromVertex, weight));
+			// 간선 정보 간선 리스트에 저장
+			edgeList.add(new Edge(fromVertex, toVertex, weight));
 		}
 		
-		int minTotalWeight = prim(1);	// 프림 알고리즘을 이용하여 최소 신장 트리 비용 뽑아냄
-		// 최소 신장 트리 비용 (즉, 모든 컴퓨터를 연결하는데 필요한 최소 비용 출력)
-		System.out.println(minTotalWeight);	
-
-	}
-	
-	// 모든 컴퓨터를 연결하는데 필요한 최소 비용을 계산해주는 메서드 (최소신장트리 비용 -> 프림 알고리즘)
-	public static int prim(int start) {
-		// 프림 알고리즘을 사용하기 위해 우선순위 큐 선언 및 생성
-		// 연결된 간선들의 가중치 오름차순 정렬
-		PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
-		pq.add(new Edge(start, 0));	// 시작정점부터 시작하기 위해 우선순위 큐에 시작정점 저장
-		int totalWeigth = 0;	// 모든 컴퓨터를 연결할때 비용
+		// 크루스칼 알고리즘을 이용하기 위해 간선 리스트 가중치 기준으로 오름차순 정렬
+		Collections.sort(edgeList, (a, b) -> a.weight - b.weight);
 		
-		// 프림 알고리즘 이용
-		while(!pq.isEmpty()) {
-			// 현재 간선 정보 뽑아냄
-			Edge now = pq.poll();
-			int to = now.toVertex;
-			int weight = now.weight;
+		int minWeight = 0;	// 최소 신장 트리를 완성할 때 드는 최소 비용
+		// 모든 간선에 대해서 순회
+		for (Edge edge: edgeList) {
+			int root1 = find(edge.fromVertex);	// 시작 정점의 루트
+			int root2 = find(edge.toVertex);	// 도착 정점의 루트
 			
-			// 해당 정점이 방문되지 않은 경우
-			if(!visited[to]) {
-				visited[to] = true;	// 해당 정점 방문처리
-				totalWeigth += weight;	// 해당 정점에 연결된 간선의 가중치 누적
-				
-				// 해당 정점과 연결된 간선들 탐색 (연결된 컴퓨터들 탐색하기)
-				for(Edge next: graph.get(to)) {	
-					// 해당 정점과 연결된 정점이 방문처리 안된 경우
-					if(!visited[next.toVertex]) {
-						pq.add(next);	// 연결된 정점 우선순위 큐에 저장 (다음 정보 탐색하기 위해)
-					}
-				}
+			// 두 정점의 루트가 다른 경우
+			if (root1 != root2) {
+				// 두 트리를 합치게끔 하기 위해 유니온 연산 수행
+				union(edge.fromVertex, edge.toVertex);
+				minWeight += edge.weight;	// 최소 신장 트리를 형성할 때 드는 최소 비용에 해당 간선의 가중치 더해줌
 			}
 		}
 		
-		// 모든 컴퓨터를 연결할 때 비용 (최소 비용) 반환
-		return totalWeigth;
+		System.out.println(minWeight);
+	}
+	
+	// 유니온 파인드에서의 파인드 메서드
+	public static int find(int a) {
+		if (a == parents[a]) {
+			return a;
+		}
+		return find(parents[a]);
+	}
+	
+	// 유니온 파인드에서의 유니온 메서드
+	public static void union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		
+		if (aRoot == bRoot) {
+			return;
+		}
+		else if (aRoot > bRoot) {
+			parents[aRoot] = bRoot;
+		}
+		else {
+			parents[bRoot] = aRoot;
+		}
 	}
 
 }
