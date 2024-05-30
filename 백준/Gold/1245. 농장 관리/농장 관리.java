cc049dd8358eb6352496c625c0dba12f29ab0a22
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-05-31 00:36:57 +0900

    [Gold V] Title: 농장 관리, Time: 124 ms, Memory: 13416 KB -BaekjoonHub

--- 백준/Gold/1245. 농장 관리/농장 관리.java
+++ 백준/Gold/1245. 농장 관리/농장 관리.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 public class Main {
 	
+	// 산봉우리의 좌표 정보를 담고있는 내부 클래스 
 	static class Mountain {
 		int x;
 		int y;
@@ -13,10 +14,10 @@ public class Main {
 		}
 	}
 
-	static int N;
-	static int M;
+	static int N;	// 맵의 행의 개수
+	static int M;	// 맵의 열의 개수
 	static int[][] map;
-	static boolean[][] visited;
+	static boolean[][] visited;	// 각 좌표마다 방문 여부를 나타내는 배열
 	// 8가지 방향 배열 (맨 위부터 시계방향으로)
 	static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
 	static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
@@ -28,7 +29,7 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		map = new int[N][M];
+		map = new int[N][M];	// [0][0] ~ [N-1][M-1]
 		visited = new boolean[N][M];
 		
 		for (int i=0; i<N; i++) {
@@ -38,13 +39,15 @@ public class Main {
 			}
 		}
 		
-		int count = 0;
+		int count = 0;	// 산봉우리의 개수
 		
 		for (int i=0; i<N; i++) {
 			for (int j=0; j<M; j++) {
+				// 해당 좌표가 방문처리 되지 않았으면서 동시에 해당 좌표의 높이가 0이 아닌 경우
 				if (!visited[i][j] && map[i][j] != 0) {
+					// 해당 좌표부터 너비우선탐색을 실시하였을 때 문제 조건에 부합하는 산봉우리인 경우
 					if (bfs(i, j)) {
-						count++;
+						count++;	// 산봉우리의 개수 증가
 					}
 				}
 			}
@@ -54,38 +57,46 @@ public class Main {
 
 	}
 	
+	// 문제에 부합하는 산봉우리를 찾는 너비우선탐색 메서드
 	public static boolean bfs(int startX, int startY) {
+		// 너비우선탐색 알고리즘을 이용하기 위해 큐 선언 및 생성
 		Queue<Mountain> queue = new LinkedList<>();
-		queue.add(new Mountain(startX, startY));
-		visited[startX][startY] = true;
+		queue.add(new Mountain(startX, startY));	// 산봉우리의 시작좌표정보 큐에 저장
+		visited[startX][startY] = true;	// 시작 좌표 방문처리
 		
-		int currentHeight = map[startX][startY];
-		boolean isPeak = true;
+		int currentHeight = map[startX][startY];	// 시작좌표부터 시작하는 현재 산봉우리의 높이
+		boolean isPeak = true;	// 산봉우리 여부를 판단하는 변수
 		
 		while (!queue.isEmpty()) {
+			// 큐에서 현재 산봉우리의 정보 뽑아냄
 			Mountain now = queue.poll();
 			int nowX = now.x;
 			int nowY = now.y;
 			
+			// 8가지 방향 탐색
 			for (int i=0; i<8; i++) {
 				int nextX = nowX + dx[i];
 				int nextY = nowY + dy[i];
 				
+				// 탐색한 좌표가 [0][0] ~ [N-1][M-1] 이외의 좌표인 경우
 				if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
-					continue;
+					continue;	// 넘어감
 				}
 				
+				// 탐색한 좌표인 산봉우리의 높이가 현재 산봉우리의 높이보다 큰 경우
 				if (map[nextX][nextY] > currentHeight) {
-                    isPeak = false;
+                    isPeak = false;	// 산봉우리가 아님
                 }
                 
+				// 탐색한 좌표가 방문하지 않았으면서 동시에 탐색한 좌표의 산봉우리 높이가 현재 산봉우리의 높이와 같은 경우
                 if (!visited[nextX][nextY] && map[nextX][nextY] == currentHeight) {
-                    visited[nextX][nextY] = true;
-                    queue.add(new Mountain(nextX, nextY));
+                    visited[nextX][nextY] = true;	// 탐색한 좌표 방문처리
+                    queue.add(new Mountain(nextX, nextY));	// 큐에 탐색한 좌표 정보 저장
                 }
 			}
 		}
 		
+		// 산봉우리 여부 반환
 		return isPeak;
 	}
 
