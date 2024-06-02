2667efc58622d986514d09e1a7090d75196e992b
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-06-02 19:28:09 +0900

    [level 3] Title: 거스름돈, Time: 10.88 ms, Memory: 52.4 MB -BaekjoonHub

--- 프로그래머스/3/12907. 거스름돈/거스름돈.java
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -8,9 +8,11 @@ class Solution {
         // 해당 돈(원)을 만드는 방법의 수를 저장할 배열
         // dp[i]는 i원을 만드는 방법의 수
         int[] dp = new int[n+1]; // [0] ~ [n]
-        dp[0] = 1;
+        dp[0] = 1;  // 0원을 만드는 방법의 수는 아무것도 사용하지 않은 경우 즉, 공집합인 경우이므로 1가지
         
+        // money 배열에 있는 동전들 탐색
         for (int coin: money) {
+            // 해당 동전부터 사용해서 n원까지 1원씩 늘려가며 탐색
             for (int i=coin; i<=n; i++) {
                 dp[i] = (dp[i] + dp[i - coin]) % MOD;
             }
