27958ddb706fb26d2cdd1e10065d849af39a057c
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-06-05 23:48:19 +0900

    [Gold V] Title: 택배 배송, Time: 572 ms, Memory: 44612 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -15,11 +15,10 @@ public class Main {
 	
 	static int N;
 	static int M;
-	static ArrayList<ArrayList<Edge>> graph;
 	static int[] dist;
-	static boolean[] visited;
 	static final int INF = Integer.MAX_VALUE;
-
+	static ArrayList<ArrayList<Edge>> graph;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -27,19 +26,20 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		graph = new ArrayList<>();
 		dist = new int[N+1];
-		visited = new boolean[N+1];
-		for (int i=0; i<=N; i++) {
-			graph.add(new ArrayList<>());
+		graph = new ArrayList<>();
+		
+		for(int i=0; i<=N; i++) {
 			dist[i] = INF;
+			graph.add(new ArrayList<>());
 		}
 		
-		for (int i=0; i<M; i++) {
+		for(int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int fromVertex = Integer.parseInt(st.nextToken());
 			int toVertex = Integer.parseInt(st.nextToken());
 			int weight = Integer.parseInt(st.nextToken());
+			
 			graph.get(fromVertex).add(new Edge(toVertex, weight));
 			graph.get(toVertex).add(new Edge(fromVertex, weight));
 		}
@@ -47,28 +47,23 @@ public class Main {
 		dijkstra(1);
 		
 		System.out.println(dist[N]);
-
 	}
 	
-	public static void dijkstra(int vertex) {
+	public static void dijkstra(int start) {
 		PriorityQueue<Edge> pq = new PriorityQueue<>((a, b) -> a.weight - b.weight);
-		pq.add(new Edge(vertex, 0));
-		dist[vertex] = 0;
+		pq.add(new Edge(start, 0));
+		dist[start] = 0;
 		
-		while (!pq.isEmpty()) {
+		while(!pq.isEmpty()) {
 			Edge now = pq.poll();
 			int nowVertex = now.toVertex;
 			int nowWeight = now.weight;
 			
-			if (!visited[nowVertex]) {
-				visited[nowVertex] = true;
-				
-				for (Edge next: graph.get(nowVertex)) {
-					int cost = next.weight + nowWeight;
-					if (cost < dist[next.toVertex]) {
-						dist[next.toVertex] = cost;
-						pq.add(new Edge(next.toVertex, dist[next.toVertex]));
-					}
+			for(Edge next: graph.get(nowVertex)) {
+				int cost = nowWeight + next.weight;
+				if(cost < dist[next.toVertex]) {
+					dist[next.toVertex] = cost;
+					pq.add(new Edge(next.toVertex, dist[next.toVertex]));
 				}
 			}
 		}
