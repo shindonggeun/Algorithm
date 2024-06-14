306dd203d1793b625599ade5957c57a0c0076941
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-06-14 14:38:35 +0900

    [Gold III] Title: 미술 시간, Time: 824 ms, Memory: 70268 KB -BaekjoonHub

--- 백준/Gold/29618. 미술 시간/미술 시간.java
+++ 백준/Gold/29618. 미술 시간/미술 시간.java
@@ -6,46 +6,59 @@ public class Main {
 	static int N;
 	static int Q;
 	static int[] parents;
-	static int[] color;
+	static int[] paint;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
+		
 		N = Integer.parseInt(st.nextToken());
 		Q = Integer.parseInt(st.nextToken());
 		
-		TreeSet<Integer> uncolored = new TreeSet<>();
-        for (int i = 1; i <= N; i++) {
-        	uncolored .add(i);
-        }
-
-        int[] result = new int[N + 1];
-        
-        for (int i=0; i<Q; i++) {
-        	st = new StringTokenizer(br.readLine());
-        	int a = Integer.parseInt(st.nextToken());
-        	int b = Integer.parseInt(st.nextToken());
-        	int x = Integer.parseInt(st.nextToken());
-        	
-        	// 현재 구간에서 색칠되지 않은 칸들을 찾아서 색칠하고 TreeSet에서 제거
-            Iterator<Integer> it = uncolored.tailSet(a).iterator();
-            while (it.hasNext()) {
-                int idx = it.next();
-                if (idx > b) break;
-                result[idx] = x;
-                it.remove();
-            }
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        for (int i=1; i<=N; i++) {
-        	sb.append(result[i]).append(" ");
-        }
-        
-        System.out.println(sb);
+		parents = new int[N+2];
+		paint = new int[N+1];
 		
+		for (int i=1; i<=N+1; i++) {
+			parents[i] = i;
+		}
+		
+		for (int i=0; i<Q; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			
+			int start = find(a);
+			while (start <= b) {
+				paint[start] = x;
+				union(start, start+1);
+				start = find(start);
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i=1; i<=N; i++) {
+			sb.append(paint[i]).append(" ");
+		}
+		
+		System.out.println(sb);
 	}
 	
+	public static int find(int a) {
+		if (a == parents[a]) {
+			return a;
+		}
+		return parents[a] = find(parents[a]);
+	}
+	
+	public static void union(int a, int b) {
+		int aRoot = find(a);
+		int bRoot = find(b);
+		
+		if (aRoot != bRoot) {
+			parents[aRoot] = bRoot;
+		}
+	}
 
 }
