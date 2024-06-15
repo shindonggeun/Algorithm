37011a5b97077aee3359709900564c8df09e8ad3
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-06-15 13:43:34 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2452 ms, Memory: 439088 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -3,58 +3,77 @@ import java.io.*;
 
 public class Main {
 
+	static int T; // 테스트 케이스 개수
+	static int K; // 연산의 개수
+	static TreeMap<Integer, Integer> treeMap; // 이중우선순위큐를 대체할 트리맵 선언
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = null;
 		
-		int T = Integer.parseInt(st.nextToken());
+		T = Integer.parseInt(br.readLine());
+		
 		StringBuilder sb = new StringBuilder();
 		
-		for(int tc=0; tc<T; tc++) {
-			TreeMap<Integer, Integer> map = new TreeMap<>();	// key: 해당 값, value: 해당 값이 나온 횟수
-			int k = Integer.parseInt(br.readLine());
+		for (int tc=1; tc<=T; tc++) {
+			K = Integer.parseInt(br.readLine());
+			// 트리맵 생성
+			treeMap = new TreeMap<>(); // key: 해당 값, value: 해당 값이 나온 횟수
 			
-			for(int i=0; i<k; i++) {
+			for (int i=0; i<K; i++) {
 				st = new StringTokenizer(br.readLine());
-				String command = st.nextToken();
+				
+				char command = st.nextToken().charAt(0); // 연산 입력
 				int num = Integer.parseInt(st.nextToken());
 				
-				// 데이터 삽입하는 명령어인 경우
-				if(command.equals("I")) {
-					map.put(num, map.getOrDefault(num, 0) + 1);
+				// 삽입 연산인 경우
+				if (command == 'I') {
+					// 트리맵에 해당 값 저장 및 개수 1개 늘려줌
+					treeMap.put(num, treeMap.getOrDefault(num, 0) + 1);
 				}
-				// 데이터 삭제하는 명령어인 경우
-				else if(command.equals("D")) {
-					// 맵이 비어있는 경우 넘어감
-					if(map.size() == 0) {
+				// 삭제 연산인 경우
+				else {
+					if (treeMap.size() == 0) {
 						continue;
 					}
-					// 맵이 비어있지 않은 경우(최대값 또는 최소값 삭제)
+					
+					int key = 0; // 해당 key 값 (최대값 or 최소값)
+					// 최대값 삭제 연산(1)인 경우
+					if (num == 1) {
+						key = treeMap.lastKey(); // 해당 key값 최대값으로 설정
+					}
+					// 최소값 삭제 연산(-1)인 경우
 					else {
-						// num == 1인 경우 최대값 삭제 연산
-						// num == -1인 경우 최소값 삭제 연산
-						int key = num == 1 ? map.lastKey() : map.firstKey();
-						int cnt = map.get(key);
-						
-						if(cnt == 1) {
-							map.remove(key);
-						}
-						else {
-							map.put(key, cnt - 1);
-						}
+						key = treeMap.firstKey(); // 해당 key값 최소값으로 설정
+					}
+					
+					int count = treeMap.get(key); // 해당 key값에 해당하는 개수 뽑아내기
+					
+					// 해당 삭제할 값의 개수가 1개인 경우
+					if (count == 1) {
+						treeMap.remove(key); // 해당 값 트리맵에서 삭제
+					}
+					// 해당 삭제할 값의 개수가 2개 이상인 경우
+					else {
+						treeMap.put(key, count - 1); // 트리맵에서 해당 값의 개수 1개 줄여줌
 					}
 				}
 			}
 			
-			if(map.size() == 0) {
+			// 트리맵이 비어있는 경우
+			if (treeMap.isEmpty()) {
 				sb.append("EMPTY").append("\n");
 			}
 			else {
-				sb.append(map.lastKey() + " " + map.firstKey()).append("\n");
+				// 트리맵의 저장된 최대값과 최소값 뽑아내기
+				int maxValue = treeMap.lastKey();
+				int minValue = treeMap.firstKey();
+				
+				sb.append(maxValue).append(" ").append(minValue).append("\n");
 			}
 		}
+		
 		System.out.print(sb);
-
 	}
 
 }
