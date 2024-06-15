ab83315dfc6c01fc54651d5752557afd087b14db
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-06-16 00:17:41 +0900

    [Silver II] Title: 아기 상어 2, Time: 392 ms, Memory: 179168 KB -BaekjoonHub

--- 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
+++ 백준/Silver/17086. 아기 상어 2/아기 상어 2.java
@@ -3,8 +3,11 @@ import java.io.*;
 
 public class Main {
 	
+	// 좌표 정보 및 안전 거리 정보를 담고 있는 내부 클래스
 	static class Position {
-		int x, y, distance;
+		int x;
+		int y;
+		int distance;
 		
 		public Position(int x, int y, int distance) {
 			this.x = x;
@@ -13,15 +16,14 @@ public class Main {
 		}
 	}
 	
-	static int N;	// y축을 의미 (세로 길이)
-	static int M; 	// x축을 의미 (가로 길이)
+	static int N; // 세로 크기
+	static int M; // 가로 크기
 	static int[][] map;
-	static boolean[][] visited;
-	static int max = Integer.MIN_VALUE;
-	// 8가지 방향 (상, 하, 좌, 우, 오른쪽 위(우 + 상), 오른쪽 아래 (우 + 하), 왼쪽 위 (좌 + 상), 왼쪽 아래 (좌 + 하)
-	static int[] dx = {1, -1, 0, 0, 1, -1, 1, -1};	// x축 고정되어 있을 때 y좌표가 이동하는 방향배열 (상, 하)
-	static int[] dy = {0, 0, -1, 1, 1, 1, -1, -1};	// y축 고정되어 있을 때 x좌표가 이동하는 방향배열 (좌, 우)
-
+	static boolean[][] visited; // 방문 배열
+	// 8가지 방향 배열 (상, 상 + 우, 우, 하 + 우, 하, 하 + 좌, 좌, 상 + 좌)
+	static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+	static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -29,63 +31,73 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		map = new int[N][M];	// (0, 0) ~ (M-1, N-1)
+		map = new int[N][M]; // [0][0] ~ [N-1][M-1]
 		
-		
-		for(int i=0; i<N; i++) {
+		for (int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<M; j++) {
+			for (int j=0; j<M; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
 		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				// 모든 빈칸(0)을 너비우선탐색 해준다
-				if(map[i][j] == 0) {
-					visited = new boolean[N][M];	// 탐색할때마다 해당좌표 방문여부 배열 초기화시켜줌(중요!!)
-					int result = bfs(i, j);
-					max = Math.max(max, result); 
+		int safeMaxDistance = 0; // 안전 거리의 최대값
+		
+		// 완전 탐색 이용
+		for (int i=0; i<N; i++) {
+			for (int j=0; j<M; j++) {
+				// 해당 좌표가 빈칸(0)인 경우
+				if (map[i][j] == 0) {
+					visited = new boolean[N][M]; // 방문 배열 초기화
+					int safeDistance = bfs(i, j); // 해당 좌표에서부터 너비우선탐색 실시
+					safeMaxDistance = Math.max(safeMaxDistance, safeDistance); // 안전 거리의 최대값 갱신
 				}
 			}
 		}
-		System.out.println(max);
+		
+		System.out.println(safeMaxDistance);
 	}
 	
+	// 해당 칸에서부터 가장 가까운 아기상어가 있는 칸까지의 거리 (안전거리)를 구하는 너비우선탐색 메서드
 	public static int bfs(int startX, int startY) {
+		// 너비우선탐색 알고리즘을 이용하기 위해 큐 선언 및 생성
 		Queue<Position> queue = new LinkedList<>();
-		queue.add(new Position(startX, startY, 0));
-		visited[startX][startY] = true;
+		queue.add(new Position(startX, startY, 0)); // 시작좌표 및 안전거리 정보 큐에 저장
+		visited[startX][startY] = true; // 시작 좌표 방문처리
 		
-		while(!queue.isEmpty()) {
+		while (!queue.isEmpty()) {
+			// 큐에 저장된 현재 좌표 정보 및 안전거리 정보 뽑아냄
 			Position now = queue.poll();
 			int nowX = now.x;
 			int nowY = now.y;
-			int distance = now.distance;
+			int nowDistance = now.distance;
+			
+			// 현재 좌표가 아기 상어(1)가 있는 칸인 경우
+			if (map[nowX][nowY] == 1) {
+				return nowDistance; // 현재까지의 안전거리 반환
+			}
 			
 			// 8가지 방향 탐색
-			for(int i=0; i<8; i++) {
+			for (int i=0; i<8; i++) {
 				int nextX = nowX + dx[i];
 				int nextY = nowY + dy[i];
 				
-				// (0, 0) ~ (M-1, N-1) 이외의 좌표인 경우 넘어감
-				if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
-					continue;
-				}
-				// 방문한 좌표인경우 넘어감
-				if(visited[nextX][nextY]) { 
-					continue;
-				}
-				// 아기상어가 있는 칸인 경우 해당 거리를 return한다
-				if(map[nextX][nextY] == 1) {
-					return distance + 1;
+				// 탐색한 좌표가 [0][0] ~ [N-1][M-1] 이외의 좌표인 경우 
+				if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= M) {
+					continue; // 다음 방향 탐색하도록 넘어감
 				}
 				
+				// 탐색한 좌표가 이미 방문처리 되어 있는 경우
+				if (visited[nextX][nextY]) {
+					continue; // 다음 방향 탐색하도록 넘어감
+				}
 				
-				queue.add(new Position(nextX, nextY, distance + 1));
-				visited[nextX][nextY] = true;
+				// 큐에 탐색한 좌표 정보 및 안전거리 정보 저장
+				queue.add(new Position(nextX, nextY, nowDistance + 1));
+				visited[nextX][nextY] = true; // 탐색한 좌표 방문처리
 			}
 		}
+		
+		// 위의 너비우선탐색 과정 실시했는데도 아기상어가 있는 칸 (안전거리) 찾지 못하면 0 반환
 		return 0;
 	}
 
