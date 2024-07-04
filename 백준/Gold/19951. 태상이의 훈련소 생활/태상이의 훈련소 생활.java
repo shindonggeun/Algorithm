ab8fc454a921d3b6a51ef034ccc8c7567897f442
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-07-04 10:28:52 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 620 ms, Memory: 64500 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -3,11 +3,11 @@ import java.io.*;
 
 public class Main {
 	
-	static int N;
-	static int M;
-	static int[] heightArr;
-	static int[] diffArr; // 차분 배열 이용 (누적합 응용)
-	static int[] result;
+	static int N; // 연병장의 크기
+	static int M; // 조교의 수
+	static int[] heightArr; // 초기 연병장의 각 칸의 높이를 저장하는 배열
+	static int[] diffArr; // 차분 배열 이용 (누적합 응용) (각 칸의 높이 변화량)
+	static int[] result; // 차분 배열을 누적합한 결과 배열 (즉, 각 칸의 최종 높이를 저장하는 배열)
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,8 +16,8 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		heightArr = new int[N+1];
-		diffArr = new int[N+2];
+		heightArr = new int[N+1]; // [1] ~ [N]
+		diffArr = new int[N+2]; // 차분 배열의 크기는 N+2로 초기화 ([1] ~ [N+1])
 		
 		st = new StringTokenizer(br.readLine());
 		for (int i=1; i<=N; i++) {
@@ -26,21 +26,22 @@ public class Main {
 		
 		for (int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int k = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken()); // 시작 칸 입력
+			int b = Integer.parseInt(st.nextToken()); // 종료 칸 입력
+			int k = Integer.parseInt(st.nextToken()); // 높이 k 입력
 			
-			diffArr[a] += k;
-			diffArr[b+1] -= k;
+			diffArr[a] += k; // 차분 배열 해당 시작칸에 높이 k 만큼 더해줌
+			diffArr[b+1] -= k; // 차분 배열 종료 칸 다음 칸에 높이 k 만큼 빼줌
 		}
 		
-		result = new int[N+1];
+		result = new int[N+1]; // [1] ~ [N]
 		
 		// 차분배열에서 누적합을 이용하여 최종 높이 계산
-		int sum = 0;
+		int sum = 0; // 누적합 계산을 위한 변수
+		// 1번 칸부터 N번 칸까지 순회
 		for (int i=1; i<=N; i++) {
-			sum += diffArr[i];
-			result[i] = heightArr[i] + sum;
+			sum += diffArr[i]; // 해당 칸의 차분 배열에 저장된 값 누적합에 더해줌
+			result[i] = heightArr[i] + sum; // 해당 칸의 초기 높이 + 지금까지 누적합 된것을 저장
 		}
 		
 		StringBuilder sb = new StringBuilder();
