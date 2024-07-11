39364e063218955f18188b123fbf33727d7f2504
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-07-11 15:04:54 +0900

    [Gold III] Title: 불!, Time: 428 ms, Memory: 66640 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -2,9 +2,10 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
+
 	static class Position {
-		int x, y;
+		int x;
+		int y;
 		
 		public Position(int x, int y) {
 			this.x = x;
@@ -12,16 +13,15 @@ public class Main {
 		}
 	}
 	
-	static int R;	// 행의 개수(세로) (y 좌표)
-	static int C;	// 열의 개수(가로) (x 좌표)
-	static boolean[][] visited;
+	static int R;
+	static int C;
 	static char[][] map;
-	// 4가지 방향 배열 (상, 하, 좌, 우)
-	static int[] dx = {1, -1, 0, 0};	// x축이 고정되어 있을 떄 y좌표가 움직이는 방향 배열 (상, 하)
-	static int[] dy = {0, 0, -1, 1};	// y축이 고정되어 있을 때 x좌표가 움직이는 방향 배열 (좌, 우)
-	static Queue<Position> jihoonQueue = new LinkedList<>();
-	static Queue<Position> fireQueue = new LinkedList<>();
-	static int escapeTime = Integer.MIN_VALUE;
+	static boolean[][] visited;
+	static Queue<Position> fireQueue;
+	static Queue<Position> jihunQueue;
+	// 4가지 방향 배열 (하, 상, 좌, 우)
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,91 +30,94 @@ public class Main {
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
 		
-		map = new char[R][C];	// (0, 0) ~ (C-1, R-1)
+		map = new char[R][C]; // [0][0] ~ [R-1][C-1]
 		visited = new boolean[R][C];
 		
-		for(int i=0; i<R; i++) {
+		fireQueue = new LinkedList<>();
+		jihunQueue = new LinkedList<>();
+		
+		for (int i=0; i<R; i++) {
 			String input = br.readLine();
-			for(int j=0; j<C; j++) {
+			for (int j=0; j<C; j++) {
 				map[i][j] = input.charAt(j);
-				if(map[i][j] == 'J') {
-					jihoonQueue.add(new Position(i, j));
-					visited[i][j] = true;
-				}
-				else if(map[i][j] == 'F') {
+				if (map[i][j] == 'F') {
 					fireQueue.add(new Position(i, j));
 					visited[i][j] = true;
 				}
+				else if (map[i][j] == 'J') {
+					jihunQueue.add(new Position(i, j));
+					visited[i][j] = true;
+				}
 			}
 		}
 		
-		bfs();
+		int escapeTime = bfs();
 		
-		// 지훈이가 미로에 탈출할 수 없는 경우
-		if(escapeTime == Integer.MIN_VALUE) {
-			System.out.println("IMPOSSIBLE");
-		}
-		else {
-			System.out.println(escapeTime);
-		}
-		
+		System.out.println(escapeTime == -1 ? "IMPOSSIBLE" : escapeTime);
+
 	}
 	
-	public static void bfs() {
+	public static int bfs() {
 		int time = 0;
 		
-		while(!jihoonQueue.isEmpty()) {
-			// 먼저 불부터 퍼뜨리기
-			int fireCount = fireQueue.size();	// 불의 개수
-			for(int i=0; i<fireCount; i++) {
-				Position fireNow = fireQueue.poll();
-				// 4가지 방향 탐색 (상, 하, 좌, 우)
-				for(int j=0; j<4; j++) {
-					int fireNextX = fireNow.x + dx[j];
-					int fireNextY = fireNow.y + dy[j];
+		while (!jihunQueue.isEmpty()) {
+			// 1. 불 먼저 퍼트리기
+			int fireCount = fireQueue.size();
+			
+			for (int i=0; i<fireCount; i++) {
+				Position fire = fireQueue.poll();
+				int nowX = fire.x;
+				int nowY = fire.y;
+				
+				for (int k=0; k<4; k++) {
+					int nextX = nowX + dx[k];
+					int nextY = nowY + dy[k];
 					
-					// 탐색한 좌표가 (0, 0) ~ (C-1, R-1) 이외의 좌표인 경우
-					if(fireNextX < 0 || fireNextY < 0 || fireNextX >= R || fireNextY >= C) {
+					if (nextX < 0 || nextY < 0 || nextX >= R || nextY >= C) {
 						continue;
 					}
 					
-					if(visited[fireNextX][fireNextY] || map[fireNextX][fireNextY] == '#') {
+					if (visited[nextX][nextY] || map[nextX][nextY] != '.') {
 						continue;
 					}
 					
-					map[fireNextX][fireNextY] = 'F';
-					visited[fireNextX][fireNextY] = true;
-					fireQueue.add(new Position(fireNextX, fireNextY));
-					
+					map[nextX][nextY] = 'F';
+					visited[nextX][nextY] = true;
+					fireQueue.add(new Position(nextX, nextY));
 				}
 			}
 			
-			// 지훈이가 움직일 차례
-			int jihoonCount = jihoonQueue.size();	// 지훈이의 개수
-			for(int i=0; i<jihoonCount; i++) {
-				Position jihoonNow = jihoonQueue.poll();
-				// 4가지 방향 탐색 (상, 하, 좌, 우)
-				for(int j = 0; j<4; j++) {
-					int jihoonNextX = jihoonNow.x + dx[j];
-					int jihoonNextY = jihoonNow.y + dy[j];
+
+			// 2. 지훈이 이동하기
+			int jihunCount = jihunQueue.size();
+			
+			for (int i=0; i<jihunCount; i++) {
+				Position jihun = jihunQueue.poll();
+				int nowX = jihun.x;
+				int nowY = jihun.y;
+				
+				for (int k=0; k<4; k++) {
+					int nextX = nowX + dx[k];
+					int nextY = nowY + dy[k];
 					
-					// 탐색한 좌표가 (0, 0) ~ (C-1, R-1) 이외의 좌표인 경우 (가장자리이다)
-					if(jihoonNextX < 0 || jihoonNextY < 0 || jihoonNextX >= R || jihoonNextY >= C) {
-						escapeTime = time + 1;	// 탈출시간 저장한 뒤
-						return;	// 해당 bfs() 메서드 종료
+					if (nextX < 0 || nextY < 0 || nextX >= R || nextY >= C) {
+						return time + 1;
 					}
 					
-					if(visited[jihoonNextX][jihoonNextY] || map[jihoonNextX][jihoonNextY] != '.') {
+					if (visited[nextX][nextY] || map[nextX][nextY] != '.') {
 						continue;
 					}
 					
-					visited[jihoonNextX][jihoonNextY] = true;
-					jihoonQueue.add(new Position(jihoonNextX, jihoonNextY));
-					//map[jihoonNextX][jihoonNextY] = 'J';
+					jihunQueue.add(new Position(nextX, nextY));
+					visited[nextX][nextY] = true;
 				}
 			}
+			
+			
 			time++;
 		}
+		
+		return -1;
 	}
 
 }
