6824159fa2e8376a7aaee824f007d18e3be0c781
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-07-16 10:51:12 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 64 ms, Memory: 11472 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -2,9 +2,9 @@ import java.io.*;
 
 public class Main {
 
-	static int K;
-	static int N;
-	static int[][] dp;
+	static int K; // 층 수
+	static int N; // 호 수
+	static int[][] dp; // 해당 층수 및 호수에 거주하고 있는 사람의 수
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,7 +16,8 @@ public class Main {
 			K = Integer.parseInt(br.readLine());
 			N = Integer.parseInt(br.readLine());
 			
-			dp = new int[K+1][N+1];
+			// [K][N] = K층 N호수에 거주하고 있는 사람의 수
+			dp = new int[K+1][N+1]; // [0][1] ~ [K][N]
 			
 			for (int i=0; i<=K; i++) {
 				dp[i][1] = 1; // i층의 1호는 거주자 1명
@@ -26,12 +27,16 @@ public class Main {
 				dp[0][i] = i; // 0층의 i호는 거주자 i명
 			}
 			
+			// 1층부터 K층까지 순회
 			for (int i=1; i<=K; i++) {
+				// 2호부터 N호까지 순회
 				for (int j=2; j<=N; j++) {
+					// i층 j호에 거주하고 있는 사람의 수 = i층 j-1호에 거주하고 있는 사람의 수 + i-1층 j호에 거주하고 있는 사람의 수 저장 
 					dp[i][j] = dp[i][j-1] + dp[i-1][j];
 				}
 			}
 			
+			// K층 N호에 거주하고 있는 사람의 수 출력
 			sb.append(dp[K][N]).append("\n");
 		}
 		
