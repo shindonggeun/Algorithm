d89d66846e94ba4461c9205d3ba9554d328f330c
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-07-18 11:47:11 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 340 ms, Memory: 23736 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.java
@@ -3,29 +3,37 @@ import java.io.*;
 
 public class Main {
 
+	static int N;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		PriorityQueue<Long> pq = new PriorityQueue<>();	// 우선순위 큐 이용 (낮은 값이 우선순위 높은 것)
+		N = Integer.parseInt(br.readLine());
 		
-		for(int i=0; i<N; i++) {
-			long num = Long.parseLong(br.readLine());
-			pq.add(num);
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		
+		for (int i=0; i<N; i++) {
+			int cardSize = Integer.parseInt(br.readLine());
+			pq.add(cardSize);
 		}
 		
-		long sum = 0;
-		// 우선순위 큐에 2개이상 들어있어야 비교 가능하므로 2개이상 일떄만 반복문 돌리기
-		while(pq.size() > 1) {
-			long temp1 = pq.poll();
-			long temp2 = pq.poll();
-			long num = temp1 + temp2;
-			
-			sum += num;
-			pq.add(num);
+		int minCount = 0;
+		int tempTotal = 0;
+		int start = 0;
+		
+		while (!pq.isEmpty()) {
+			tempTotal += pq.poll();
+			start++;
+			if (start == 2) {
+				minCount += tempTotal;
+				pq.add(tempTotal);
+				tempTotal = 0;
+				start = 0;
+			}
 		}
-		System.out.println(sum);
+		
+		System.out.println(minCount);
+
 	}
 
 }
