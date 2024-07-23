b22219f4ac41ed5e72788f64e78c31607949b3ee
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-07-23 11:14:56 +0900

    [Silver II] Title: 쇠막대기, Time: 140 ms, Memory: 14700 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -3,29 +3,35 @@ import java.io.*;
 
 public class Main {
 	
-	static Stack<Character> stack;
+	static Stack<Character> stack; // 괄호들을 저장할 스택
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String input = br.readLine();
 		
-		stack = new Stack<>();
-		int totalPipeCount = 0;
+		stack = new Stack<>(); // 스택 초기화
+		int totalPipeCount = 0; // 잘려진 쇠막대기 조각의 총 개수
 		
+		// 입력값 한 문자씩 탐색
 		for (int i=0; i<input.length(); i++) {
-			char ch = input.charAt(i);
+			char ch = input.charAt(i); // 입력값의 해당 괄호 추출
 			
+			// 해당 괄호가 여는 괄호인 경우
 			if (ch == '(') {
-				stack.push(ch);
+				stack.push(ch); // 스택에 집어넣음
 			}
+			// 해당 괄호가 닫는 괄호인 경우
 			else {
+				// 스택이 비어있지 않은 경우
 				if (!stack.isEmpty()) {
-					stack.pop();
+					stack.pop(); // 스택의 최상단에 저장된 값 뽑아냄
+					// 입력값에서 전 글자(문자)가 여는 괄호인 경우 (즉, 레이저인 경우 -> "()")
 					if (input.charAt(i-1) == '(') {
-						totalPipeCount += stack.size();
+						totalPipeCount += stack.size(); // 스택의 사이즈를 잘려진 쇠막대기 조각의 총 개수에 누적
 					}
+					// 입력값에서 전 글자(문자)가 여는 괄호가 아닌 경우 (즉, 레이저가 아닌 경우)
 					else {
-						totalPipeCount += 1;
+						totalPipeCount += 1; // 쇠막대기 조각의 총 개수 증가 (단순히 쇠막대기가 하나 더 들어온거임)
 					}
 				}
 			}
