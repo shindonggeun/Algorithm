215ba3e027cecb96ee33e1df861917614938e7bd
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-08-13 15:35:49 +0900

    [Gold IV] Title: 사이클 게임, Time: 868 ms, Memory: 246260 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.java
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.java
@@ -2,62 +2,62 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
+
+	static int[] parents;
 	static int N;
 	static int M;
-	static int[] parents;
-
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		parents = new int[N]; // [0] ~ [N-1]
-		
-		for (int i=0; i<N; i++) {
+		parents = new int[N];
+		for(int i=0; i<N; i++) {
 			parents[i] = i;
 		}
 		
-		boolean isCycle = false;
-		int result = 0;
-		
-		for (int i=1; i<=M; i++) {
+		boolean cycleCheck = false;
+		int cycleNum = 0;
+		for(int i=1; i<=M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int fromVertex = Integer.parseInt(st.nextToken());
 			int toVertex = Integer.parseInt(st.nextToken());
 			
-			if (isCycle) {
+			if(cycleCheck) {
 				continue;
 			}
 			
-			if (find(fromVertex) != find(toVertex)) {
-				union(fromVertex, toVertex);
-			}
-			else {
-				isCycle = true;
-				result = i;
+			if(isCycle(fromVertex, toVertex)) {
+				cycleCheck = true;
+				cycleNum = i;
 			}
 		}
 		
-		System.out.println(isCycle == true ? result : 0);
+		System.out.println(cycleNum);
 	}
 	
 	public static int find(int a) {
-		if (a == parents[a]) {
+		if(a == parents[a]) {
 			return a;
 		}
 		return parents[a] = find(parents[a]);
 	}
 	
-	public static void union(int a, int b) {
+	
+	public static boolean isCycle(int a, int b) {
 		int aRoot = find(a);
 		int bRoot = find(b);
 		
-		if (aRoot != bRoot) {
-			parents[bRoot] = aRoot;
+		if(aRoot == bRoot) {
+			return true;
 		}
+		
+		parents[aRoot] = bRoot;
+		return false;
 	}
+	
+	
 
 }
