9703e67e65cb6fe90206eabc9a984a9e1ea88ad6
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-08-14 13:46:20 +0900

    [Silver III] Title: 수열, Time: 240 ms, Memory: 23896 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -3,39 +3,37 @@ import java.io.*;
 
 public class Main {
 	
-	static int N;	// 전체 날짜 수
-	static int K;	// 연속적인 날짜의 수
-	static int[] temperature;	// 매일 측정한 온도를 저장할 배열
+	static int N;
+	static int K;
+	static int[] temperatureArr;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+
 		N = Integer.parseInt(st.nextToken());
 		K = Integer.parseInt(st.nextToken());
 		
-		temperature = new int[N];	// [0] ~ [N]
+		temperatureArr = new int[N];
 		
 		st = new StringTokenizer(br.readLine());
 		for (int i=0; i<N; i++) {
-			temperature[i] = Integer.parseInt(st.nextToken());
+			temperatureArr[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		// 처음 K일 동안의 온도의 합을 미리 계산
-        int sum = 0;
-        for (int i = 0; i < K; i++) {
-            sum += temperature[i];
-        }
-
-        int max = sum;	// 연속적인 K일의 온도의 합이 최대가 되는 값
-
-        // 슬라이딩 윈도우 적용
-        for (int i = K; i < N; i++) {
-        	sum += temperature[i] - temperature[i-K];
-            max = Math.max(max, sum);
-        }
-
-        System.out.println(max);
+		int continuousSum = 0;
+		for (int i=0; i<K; i++) {
+			continuousSum += temperatureArr[i];
+		}
+		
+		int maxContinuousSum = continuousSum;
+		
+		for (int i=K; i<N; i++) {
+			continuousSum += temperatureArr[i] - temperatureArr[i-K];
+			maxContinuousSum = Math.max(maxContinuousSum, continuousSum);
+		}
+		
+		System.out.println(maxContinuousSum);
 	}
 
 }
