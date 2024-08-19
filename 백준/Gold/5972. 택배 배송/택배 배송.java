15b88a20a3f82dd78dbd9cf9dcb9d9546c843085
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-08-19 12:11:07 +0900

    [Gold V] Title: 택배 배송, Time: 392 ms, Memory: 40732 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -15,10 +15,11 @@ public class Main {
 	
 	static int N;
 	static int M;
+	static ArrayList<ArrayList<Edge>> graph;
+	static boolean[] visited;
 	static int[] dist;
 	static final int INF = Integer.MAX_VALUE;
-	static ArrayList<ArrayList<Edge>> graph;
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -26,15 +27,16 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		dist = new int[N+1];
 		graph = new ArrayList<>();
+		visited = new boolean[N+1];
+		dist = new int[N+1];
 		
-		for(int i=0; i<=N; i++) {
-			dist[i] = INF;
+		for (int i=0; i<=N; i++) {
 			graph.add(new ArrayList<>());
+			dist[i] = INF;
 		}
 		
-		for(int i=0; i<M; i++) {
+		for (int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int fromVertex = Integer.parseInt(st.nextToken());
 			int toVertex = Integer.parseInt(st.nextToken());
@@ -47,6 +49,7 @@ public class Main {
 		dijkstra(1);
 		
 		System.out.println(dist[N]);
+
 	}
 	
 	public static void dijkstra(int start) {
@@ -54,16 +57,20 @@ public class Main {
 		pq.add(new Edge(start, 0));
 		dist[start] = 0;
 		
-		while(!pq.isEmpty()) {
+		while (!pq.isEmpty()) {
 			Edge now = pq.poll();
 			int nowVertex = now.toVertex;
 			int nowWeight = now.weight;
 			
-			for(Edge next: graph.get(nowVertex)) {
-				int cost = nowWeight + next.weight;
-				if(cost < dist[next.toVertex]) {
-					dist[next.toVertex] = cost;
-					pq.add(new Edge(next.toVertex, dist[next.toVertex]));
+			if (!visited[nowVertex]) {
+				visited[nowVertex] = true;
+				
+				for (Edge next: graph.get(nowVertex)) {
+					int cost = nowWeight + next.weight;
+					if (cost < dist[next.toVertex]) {
+						dist[next.toVertex] = cost;
+						pq.add(new Edge(next.toVertex, cost));
+					}
 				}
 			}
 		}
