6f8e3dedc61829df45a686094037994ad679f4ac
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-08-19 14:23:17 +0900

    [Gold V] Title: 게임, Time: 468 ms, Memory: 28152 KB -BaekjoonHub

--- 백준/Gold/1584. 게임/게임.java
+++ 백준/Gold/1584. 게임/게임.java
@@ -13,29 +13,32 @@ public class Main {
 		}
 	}
 	
-	static int N; // 위험한 구역의 수
-	static int M; // 죽음의 구역의 수
+	static final int SIZE = 500;
+	static int N; // 위험한 구역의 개수
+	static int M; // 죽음의 구역의 개수
 	static int[][] map;
 	static int[][] dist;
 	static final int INF = Integer.MAX_VALUE;
+	static final int DANGER = 1;
+	static final int DEATH = 2;
 	// 4가지 방향 배열 (하, 상, 좌, 우)
 	static int[] dx = {1, -1, 0, 0};
 	static int[] dy = {0, 0, -1, 1};
-	
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
 		
-		N = Integer.parseInt(br.readLine());
+		map = new int[SIZE+1][SIZE+1]; // [0][0] ~ [500][500]
+		dist = new int[SIZE+1][SIZE+1];
 		
-		map = new int[500+1][500+1]; // [0][0] ~ [500][500]
-		dist = new int[500+1][500+1];
-		
-		for (int i=0; i<=500; i++) {
-			Arrays.fill(dist[i], INF);
+		for (int i=0; i<=SIZE; i++) {
+			for (int j=0; j<=SIZE; j++) {
+				dist[i][j] = INF;
+			}
 		}
 		
-		// 위험 구역 설정하는 과정
+		N = Integer.parseInt(br.readLine());
 		for (int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x1 = Integer.parseInt(st.nextToken());
@@ -43,12 +46,12 @@ public class Main {
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
 			
-			setupZone(x1, y1, x2, y2, 1);
+			// 위험한 구역 설정
+			setupZone(x1, y1, x2, y2, DANGER);
 		}
 		
-		M = Integer.parseInt(br.readLine());
 		
-		// 죽음 구역 설정하는 과정
+		M = Integer.parseInt(br.readLine());
 		for (int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x1 = Integer.parseInt(st.nextToken());
@@ -56,43 +59,35 @@ public class Main {
 			int x2 = Integer.parseInt(st.nextToken());
 			int y2 = Integer.parseInt(st.nextToken());
 			
-			setupZone(x1, y1, x2, y2, 2);
+			setupZone(x1, y1, x2, y2, DEATH);
 		}
 		
-		map[0][0] = 0;
 		dijkstra(0, 0);
 		
-		if (dist[500][500] == INF) {
-			System.out.println(-1);
-		}
-		else {
-			System.out.println(dist[500][500]);
-		}
+		System.out.println(dist[SIZE][SIZE] == INF ? -1 : dist[SIZE][SIZE]);
+
 	}
 	
-	// 구역 설정하는 메서드 (위험구역, 죽음 구역)
-	public static void setupZone(int x1, int y1, int x2, int y2, int set) {
+	public static void setupZone(int x1, int y1, int x2, int y2, int value) {
 		int minX = Math.min(x1, x2);
-		int maxX = Math.max(x1, x2);
-		
 		int minY = Math.min(y1, y2);
+		
+		int maxX = Math.max(x1, x2);
 		int maxY = Math.max(y1, y2);
 		
 		for (int i=minX; i<=maxX; i++) {
 			for (int j=minY; j<=maxY; j++) {
-				// 죽음 구역이 위험 구역보다 우선
-				if (map[i][j] < set) {
-					map[i][j] = set;
+				if (map[i][j] < value) {
+					map[i][j] = value;
 				}
 			}
 		}
-		
 	}
 	
 	public static void dijkstra(int startX, int startY) {
 		PriorityQueue<Position> pq = new PriorityQueue<>((a, b) -> dist[a.x][a.y] - dist[b.x][b.y]);
 		pq.add(new Position(startX, startY));
-		dist[startX][startY] = map[startX][startY];
+		dist[startX][startY] = 0;
 		
 		while (!pq.isEmpty()) {
 			Position now = pq.poll();
@@ -103,15 +98,17 @@ public class Main {
 				int nextX = nowX + dx[i];
 				int nextY = nowY + dy[i];
 				
-				if (nextX < 0 || nextY < 0 || nextX >= 501 || nextY >= 501) {
+				// 탐색한 좌표가 [0][0] ~ [500][500] 이외의 좌표인 경우
+				if (nextX < 0 || nextY < 0 || nextX > SIZE || nextY > SIZE) {
 					continue;
 				}
 				
-				if (map[nextX][nextY] == 2) {
+				if (map[nextX][nextY] == DEATH) {
 					continue;
 				}
 				
 				int cost = dist[nowX][nowY] + map[nextX][nextY];
+				
 				if (cost < dist[nextX][nextY]) {
 					dist[nextX][nextY] = cost;
 					pq.add(new Position(nextX, nextY));
