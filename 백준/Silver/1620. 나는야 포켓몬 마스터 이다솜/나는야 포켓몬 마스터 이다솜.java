9e396a836ddddbe54150b4c224084bee4f2f7d45
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-09-02 21:59:44 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 460 ms, Memory: 59460 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -3,10 +3,12 @@ import java.io.*;
 
 public class Main {
 	
-	static int N;
-	static int M;
-	static Map<Integer, String> numberMap;
-	static Map<String, Integer> nameMap;
+	static int N; // 포켓몬의 개수
+	static int M; // 맞춰야하는 문제 개수
+	// 해당 번호에 맞는 포켓몬의 이름
+	static Map<Integer, String> numberMap; // key: 해당 포켓몬 번호, value: 포켓몬 이름
+	// 해당 포멧몬 이름에 맞는 번호
+	static Map<String, Integer> nameMap; // key: 해당 포켓몬 이름, value: 포켓몬 번호
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,9 +17,11 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
+		// 해시맵 초기화
 		numberMap = new HashMap<>();
 		nameMap = new HashMap<>();
 		
+		// 해당 번호 및 포켓몬 이름 각 해시맵에 저장
 		for (int i=1; i<=N; i++) {
 			String name = br.readLine();
 			numberMap.put(i, name);
@@ -26,17 +30,19 @@ public class Main {
 		
 		StringBuilder sb = new StringBuilder();
 		for (int i=0; i<M; i++) {
-			String input = br.readLine();
-			char ch = input.charAt(0);
+			String input = br.readLine(); // 문제 입력
+			char ch = input.charAt(0); // 해당 문제의 첫번째 글자 추출
 			
+			// 해당 글자가 숫자인 경우
 			if (Character.isDigit(ch)) {
-				int number = Integer.parseInt(input);
-				String name = numberMap.get(number);
+				int number = Integer.parseInt(input); // 해당 문제의 숫자(번호) 추출
+				String name = numberMap.get(number); // 해당 번호에 따른 포켓몬 이름 해시맵에서 검색
 				
 				sb.append(name);
 			}
+			// 해당 글자가 숫자가 아닌 경우
 			else {
-				int number = nameMap.get(input);
+				int number = nameMap.get(input); // 해당 문제의 이름에 따른 포켓몬 번호 해시맵에서 검색
 				
 				sb.append(number);
 			}
