1cfa6e671d76029afc1e2e5c8e42ca598200c95d
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-09-02 21:50:53 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 460 ms, Memory: 51848 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -2,40 +2,49 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	
+	static int N;
+	static int M;
+	static Map<Integer, String> numberMap;
+	static Map<String, Integer> nameMap;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		Map<String, String> nameMap = new HashMap<>();	// key: 포켓몬 이름, value: 해당 포켓몬의 이름에 따른 번호(String)
-		Map<String, String> numberMap = new HashMap<>();	// key: 포켓몬 번호(String), value: 해당 포켓몬의 번호에 따른 이름
-		
-		for(int i=1; i<=N; i++) {
-			String name = br.readLine();
-			nameMap.put(name, String.valueOf(i));
-			numberMap.put(String.valueOf(i), name);
-		}
 
-		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<M; i++) {
-			String numberOrName = br.readLine();
-			
-			// 포켓몬 이름에 따른 번호를 저장한 맵에 해당 key값이 존재하면
-			if(nameMap.containsKey(numberOrName)) {
-				sb.append(nameMap.get(numberOrName)).append("\n");
-			}
-			// 포켓몬 번호에 따른 이름을 저장한 맵에 해당 key값이 존재하면
-			else if(numberMap.containsKey(numberOrName)) {
-				sb.append(numberMap.get(numberOrName)).append("\n");
-			}
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		numberMap = new HashMap<>();
+		nameMap = new HashMap<>();
+		
+		for (int i=1; i<=N; i++) {
+			String name = br.readLine();
+			numberMap.put(i, name);
+			nameMap.put(name, i);
 		}
 		
-		System.out.print(sb);
+		StringBuilder sb = new StringBuilder();
+		for (int i=0; i<M; i++) {
+			String input = br.readLine();
+			char ch = input.charAt(0);
+			
+			if (Character.isDigit(ch)) {
+				int number = Integer.parseInt(input);
+				String name = numberMap.get(number);
+				
+				sb.append(name);
+			}
+			else {
+				int number = nameMap.get(input);
+				
+				sb.append(number);
+			}
+			
+			sb.append("\n");
+		}
 		
-		
+		System.out.println(sb);
 	}
 
 }
