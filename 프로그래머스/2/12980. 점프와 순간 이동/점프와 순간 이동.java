bcf5582c569f4028b3410f48183e33a5f6aaccd6
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-10-14 23:39:28 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -7,7 +7,7 @@ public class Solution {
         int batteryUseCount = 0; // 건전지 사용량
         
         // 도착위치부터 탐색 (거꾸로 탐색) -> 도착위치에서 시작 위치까지 반복
-        while (n > 0) {
+        while (n != 0) {
             // n이 짝수가 아닌 경우 (즉, 홀수인 경우)
             if (n % 2 != 0) {
                 n--; // 해당 도착 위치 감소시킨 뒤
