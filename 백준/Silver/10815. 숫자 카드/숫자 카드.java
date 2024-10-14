9670f4ad0b0e0484a8bd4eea8d1fcdeb59e5a64c
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-10-14 22:25:23 +0900

    [Silver V] Title: 숫자 카드, Time: 760 ms, Memory: 173884 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,32 +1,46 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+	
+	static int N; // 상근이가 가지고 있는 숫자 카드의 개수
+	static int M; // 상근이가 가지고 있는 숫자 카드인지 아닌지를 구해야 할 개수
+	static Set<Integer> cardSet; // 상근이가 가지고 있는 숫자 카드들을 저장할 해시셋
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = null;
 		
-		int N = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(br.readLine());
+		cardSet = new HashSet<>(); // 숫자 카드들을 저장할 해시셋 초기화
+		
 		st = new StringTokenizer(br.readLine());
-		Map<Integer, Integer> map = new HashMap<>();	// key: 상근이가 가지고 있는 숫자 카드, value: 숫자카드 개수
-		
-		for(int i=0; i<N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			map.put(num, map.getOrDefault(num, 0) + 1);
+		for (int i=0; i<N; i++) {
+			int cardNum = Integer.parseInt(st.nextToken());
+			cardSet.add(cardNum); // 해시셋에 상근이가 가지고 있는 숫자 카드 저장
 		}
 		
-		st = new StringTokenizer(br.readLine());
-		int M = Integer.parseInt(st.nextToken());
-		st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
+		M = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder(); // 결과값들을 저장할 StringBuilder
 		
-		for(int i=0; i<M; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			sb.append(map.getOrDefault(num, 0) + " ");	// 상근이가 가지고있는 숫자카드의 없는 수면 0으로
+		st = new StringTokenizer(br.readLine());
+		for (int i=0; i<M; i++) {
+			int num = Integer.parseInt(st.nextToken()); // 숫자 입력
+			
+			// 해당 숫자가 상근이가 가지고 있는 숫자 카드인 경우
+			if (cardSet.contains(num)) {
+				sb.append(1);
+			}
+			// 해당 숫자가 상근이가 가지고 있는 숫자 카드가 아닌 경우
+			else {
+				sb.append(0);
+			}
+			
+			sb.append(" "); // 공백까지 같이 StringBuilder에 저장
 		}
-		System.out.println(sb);
 		
-		
-    }
+		System.out.println(sb); // 최종 결과값 출력
+
+	}
+
 }
