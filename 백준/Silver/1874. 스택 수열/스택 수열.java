effb6c88d44ab38d40518bb2582747b75a2a7d60
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-10-20 16:48:16 +0900

    [Silver II] Title: 스택 수열, Time: 300 ms, Memory: 27984 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -3,45 +3,39 @@ import java.io.*;
 
 public class Main {
 
+	static int N;
+	static Stack<Integer> stack;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		Stack<Integer> stack = new Stack<>();
 		
-		int start = 0;	// 시작변수 지정 
+		N = Integer.parseInt(br.readLine());
+		
+		stack = new Stack<>();
+		
 		StringBuilder sb = new StringBuilder();
-		boolean find = false;	// 스택수열 만들 수 있는지 없는지 판단해주는 변수
+		boolean stackCheck = true;
+		int start = 0;
 		
-		for(int i=0; i<N; i++) {
-			int num = Integer.parseInt(br.readLine());	// 입력받은 숫자
+		for (int i=0; i<N; i++) {
+			int num = Integer.parseInt(br.readLine());
 			
-			// 입력받은 숫자가 시작변수보다 큰 경우
-			if(num > start) {
-				// 시작변수+1부터 시작해서 입력받은 숫자까지 stack에 push해줌
-				for(int k=start+1; k<=num; k++) {
-					stack.push(k);
-					sb.append("+").append("\n");	// 스택 push 표시 StringBuilder에 저장
+			if (num > start) {
+				for (int j=start+1; j<=num; j++) {
+					stack.push(j);
+					sb.append("+").append("\n");
 				}
-				// 위의 과정을 거쳐 스택에 다 저장했으면 입력받은 수를 시작변수로 초기화해줌
-				start = num;	
+				start = num;
 			}
-			// 스택의 최상단 수가 입력 받은 수가 아닌 경우 (입력된 수열을 만들 수 없는 경우)
-			else if(stack.peek() != num){
-				find = true;	// 스택수열 만들 수 없음
+			else if (stack.peek() != num) {
+				stackCheck = false;
 			}
 			
-			stack.pop();	// 스택에 최상단에 저장된 숫자 빼냄
+			stack.pop();
 			sb.append("-").append("\n");
 		}
 		
-		// 스택수열 만들 수 없는 경우
-		if(find) {
-			System.out.println("NO");	// 입력된 수열 만들 수 없으므로 "NO" 출력한 뒤
-			return;	// 메인 메서드 종료
-		}
-		
-		// 스택수열 만들 수 있으면 StringBuilder 출력
-		System.out.print(sb);
+		System.out.println(stackCheck == true ? sb : "NO");
 
 	}
 
