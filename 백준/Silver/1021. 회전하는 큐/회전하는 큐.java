e68674f44b8de8a9a29afbd391b62be41589a7ce
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-10-20 18:13:24 +0900

    [Silver III] Title: 회전하는 큐, Time: 100 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.java
@@ -2,56 +2,65 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	
+	static int N; // 큐의 크기
+	static int M; // 뽑아내려고 하는 수의 개수
+	static Deque<Integer> deque;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int resultCount = 0;
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
-		Deque<Integer> deque = new ArrayDeque<>();
-		
-		for(int i=1; i<=N; i++) {
+		deque = new ArrayDeque<>();
+		for (int i=1; i<=N; i++) {
 			deque.add(i);
 		}
 		
+		List<Integer> targetList = new ArrayList<>();
+		
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<M; i++) {
-			int targetNum = Integer.parseInt(st.nextToken());
+		for (int i=0; i<M; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			targetList.add(num);
+		}
+		
+		int totalMoveCount = 0;
+		
+		for (int target: targetList) {
+			int idx = 0;
 			
-			while(true) {
-				int idx = 0;
-				for(int value: deque) {
-					if(value == targetNum) {
-						break;
-					}
-					idx++;
-				}
-				
-				// 앞쪽으로의 이동이 뒷쪽으로의 이동보다 적거나 같은경우
-				if(idx <= deque.size() / 2) {
-					for(int j=0; j<idx; j++) {
-						deque.addLast(deque.pollFirst());
-						resultCount++;
-					}
-				}
-				else {
-					for(int j=0; j<deque.size() - idx; j++) {
-						deque.addFirst(deque.pollLast());
-						resultCount++;
-					}
-				}
-				
-				if(deque.peekFirst() == targetNum) {
-					deque.pollFirst();
+			for (int num: deque) {
+				if (num == target) {
 					break;
 				}
+				idx++;
 			}
-		
+			
+			int leftMoveCount = idx;
+			int rightMoveCount = deque.size() - idx;
+			
+			if (leftMoveCount <= rightMoveCount) {
+				for (int i=0; i<leftMoveCount; i++) {
+					int leftNum = deque.pollFirst();
+					deque.addLast(leftNum);
+				}
+				totalMoveCount += leftMoveCount;
+			}
+			else {
+				for (int i=0; i<rightMoveCount; i++) {
+					int rightNum = deque.pollLast();
+					deque.addFirst(rightNum);
+				}
+				totalMoveCount += rightMoveCount;
+			}
+			
+			deque.pollFirst();
 		}
-		System.out.println(resultCount);
+		
+		System.out.println(totalMoveCount);
 
 	}
 
