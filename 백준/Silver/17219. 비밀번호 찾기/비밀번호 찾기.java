2a3cea90a1647e9dc595d2c022cf18822137d96d
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-10-28 00:02:45 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 556 ms, Memory: 78772 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -3,9 +3,9 @@ import java.io.*;
 
 public class Main {
 	
-	static int N;
-	static int M;
-	static Map<String, String> sitePasswordMap;
+	static int N; // 사이트 주소의 수
+	static int M; // 비밀번호를 찾으려는 사이트 주소의 수
+	static Map<String, String> sitePasswordMap; // key: 사이트 주소, value: 해당 사이트의 비밀번호
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,11 +14,11 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		
-		sitePasswordMap = new HashMap<>();
+		sitePasswordMap = new HashMap<>(); // 해시맵 생성
 		
 		for (int i=0; i<N; i++) {
 			String input = br.readLine();
-			String[] inputSplit = input.split(" ");
+			String[] inputSplit = input.split(" "); // 해당 입력값 공백단위로 끊음
 			
 			sitePasswordMap.put(inputSplit[0], inputSplit[1]);
 		}
@@ -27,6 +27,7 @@ public class Main {
 		for (int i=0; i<M; i++) {
 			String site = br.readLine();
 			
+			// 해당 입력한 사이트의 패스워드를 출력하게끔
 			sb.append(sitePasswordMap.get(site)).append("\n");
 		}
 		
