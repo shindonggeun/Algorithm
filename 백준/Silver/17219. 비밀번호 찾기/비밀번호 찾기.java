61d2d523f82ddd9bca60a65798564a48dcb61572
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-10-28 00:01:16 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 616 ms, Memory: 64416 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -2,32 +2,36 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	
+	static int N;
+	static int M;
+	static Map<String, String> sitePasswordMap;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());	// 저장된 사이트의 수
-		int M = Integer.parseInt(st.nextToken());	// 비밀번호 찾으려는 사이트 수
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
 		
-		Map<String, String> map = new HashMap<>();	// key: 사이트 주소, value: 비밀번호
+		sitePasswordMap = new HashMap<>();
 		
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine(),"\n");	// 입력값 공백단위로 끊는게 아닌 한줄단위로 끊음(한줄 다 읽음)
-			String str = st.nextToken();					
-			String[] site = str.split(" ");					// 입력값 공백단위로 쪼개기
+		for (int i=0; i<N; i++) {
+			String input = br.readLine();
+			String[] inputSplit = input.split(" ");
 			
-			map.put(site[0], site[1]);
+			sitePasswordMap.put(inputSplit[0], inputSplit[1]);
 		}
 		
 		StringBuilder sb = new StringBuilder();
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			String str = st.nextToken();
+		for (int i=0; i<M; i++) {
+			String site = br.readLine();
 			
-			sb.append(map.get(str)).append("\n");
+			sb.append(sitePasswordMap.get(site)).append("\n");
 		}
-		System.out.println(sb);
+		
+		System.out.print(sb);
+
 	}
 
 }
