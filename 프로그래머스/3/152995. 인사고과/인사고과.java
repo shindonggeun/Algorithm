4694f7e57455ae4ed6eb270f3a7465876c37e256
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-11-10 15:51:38 +0900

    [level 3] Title: 인사고과, Time: 78.17 ms, Memory: 109 MB -BaekjoonHub

---
+++ 프로그래머스/3/152995. 인사고과/인사고과.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] scores) {
+        int answer = 0;
+        
+        int[] wanhoScore = scores[0];
+        int rank = 1;
+        int maxScore = 0;
+        int wanhoTotalScore = wanhoScore[0] + wanhoScore[1];
+        
+        Arrays.sort(scores, (a, b) -> {
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
+            }
+            return b[0] - a[0]; 
+        });
+        
+        for (int[] score: scores) {
+            if (maxScore <= score[1]) {
+                maxScore = score[1];
+                int totalScore = score[0] + score[1];
+                if (totalScore > wanhoTotalScore) {
+                    rank++;
+                }
+            }
+            else {
+                if (score.equals(wanhoScore)) {
+                    rank = -1;
+                    break;
+                }
+            }
+        }
+        
+        answer = rank;
+        return answer;
+    }
+}
