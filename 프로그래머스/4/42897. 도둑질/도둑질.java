4681e102289011d4fc102c425eb00a9187e08409
동긔 <42789808+shindonggeun@users.noreply.github.com>
2024-11-10 16:44:28 +0900

    [level 4] Title: 도둑질, Time: 29.18 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -3,27 +3,20 @@ class Solution {
         int answer = 0;
         
         int n = money.length;
+        int[][] dp = new int[2][n];
         
-        if (n == 3) {
-            return Math.max(Math.max(money[0], money[1]), money[2]);
-        }
+        dp[0][0] = money[0];
+        dp[0][1] = Math.max(money[0], money[1]);
         
-        int[] dp1 = new int[n];
-        dp1[0] = money[0];
-        dp1[1] = Math.max(money[0], money[1]);
-        
-        for (int i=2; i<n-1; i++) {
-            dp1[i] = Math.max(dp1[i-1], dp1[i-2] + money[i]);
-        }
-        
-        int[] dp2 = new int[n];
-        dp2[1] = money[1];
+        dp[1][0] = 0;
+        dp[1][1] = money[1];
         
         for (int i=2; i<n; i++) {
-            dp2[i] = Math.max(dp2[i-1], dp2[i-2] + money[i]);
+            dp[0][i] = Math.max(dp[0][i-1], dp[0][i-2] + money[i]);
+            dp[1][i] = Math.max(dp[1][i-1], dp[1][i-2] + money[i]);
         }
         
-        answer = Math.max(dp1[n-2], dp2[n-1]);
+        answer = Math.max(dp[0][n-2], dp[1][n-1]);
         return answer;
     }
 }
