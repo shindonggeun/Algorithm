86d020c040d40a9c115d5d59a5019fa35cc424a8
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-02-18 16:39:59 +0900

    [level 2] Title: 서버 증설 횟수, Time: 0.04 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/2/389479. 서버 증설 횟수/README.md
+++ 프로그래머스/2/389479. 서버 증설 횟수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 서버 증설 횟수 - 389479 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/389479) 
 
 ### 성능 요약
 
-메모리: 72.2 MB, 시간: 0.04 ms
+메모리: 84.6 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 18일 16:26:56
+2025년 02월 18일 16:39:58
 
 ### 문제 설명
 
--- 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
+++ 프로그래머스/2/389479. 서버 증설 횟수/서버 증설 횟수.java
@@ -1,24 +1,25 @@
 class Solution {
     
-    static int currentServerCount; // 현재 증설된 서버의 수
+    static int currentServerCount; // 현재 운영 중인 서버의 개수 (시간이 지나면서 감소)
     static int[] expires = new int[24]; // 각 시간별 서버 만료 수 [0] ~ [23]
     
     public int solution(int[] players, int m, int k) {
-        int answer = 0; // 결과값 (증설 횟수)
+        int answer = 0; // 결과값 (서버 증설 횟수)
         
+        // 24시간 동안 시뮬레이션 진행
         for (int time=0; time<players.length; time++) {
             currentServerCount -= expires[time]; // 시간별 만료된 서버 제거
             
-            // 현재 필요한 서버 수 계산
-            int needServerCount = players[time] / m; // 올림 연산으로 필요 서버 수 계산
+            int needServerCount = players[time] / m; // 현재 필요한 서버 수 계산
             
-            // 현재 증설된 서버 수가 현재 필요한 서버 수보다 작은 경우
+            // 현재 운영 중인 서버보다 더 많은 서버가 필요한 경우 증설해야 함
             if (currentServerCount < needServerCount) {
+                // 부족한 서버 개수 계산
                 int addServerCount = needServerCount - currentServerCount;
-                answer += addServerCount;
-                currentServerCount += addServerCount;
+                answer += addServerCount; // 총 증설 횟수 갱신 (새로운 서버를 추가한 횟수 더해줌)
+                currentServerCount += addServerCount; // 현재 운영 중인 서버 개수 갱신
                 
-                // k시간 뒤에 서버 만료 예정
+                // 추가한 서버는 k 시간동안 유지 됨 (k 시간 뒤에는 반납해야함)
                 if (time + k < 24) {
                     expires[time + k] += addServerCount;
                 }
