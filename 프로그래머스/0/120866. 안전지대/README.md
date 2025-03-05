3d3e130be029d568195016b598195eaa7398c6dc
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-03-05 23:01:12 +0900

    [level 0] Title: 안전지대, Time: 0.46 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/README.md
+++ 프로그래머스/0/120866. 안전지대/README.md
@@ -1,10 +1,10 @@
 # [level 0] 안전지대 - 120866 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120866?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120866) 
 
 ### 성능 요약
 
-메모리: 72.2 MB, 시간: 2.21 ms
+메모리: 84 MB, 시간: 0.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 05일 22:52:39
+2025년 03월 05일 23:01:10
 
 ### 문제 설명
 
--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     
+    // 좌표 정보를 저장하기 위해 사용할 내부 클래스
     static class Position {
         int x;
         int y;
@@ -12,60 +13,70 @@ class Solution {
         }
     }
     
-    static int n;
+    static int n; // 지도의 크기
+    // 8가지 방향 배열
     // 상, 상 + 우, 우, 하 + 우, 하, 하 + 좌, 좌, 상 + 좌
     static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
     static int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
-    static boolean[][] visited;
-    static List<Position> positionList;
+    static boolean[][] visited; // 해당 좌표의 방문 여부를 저장하는 배열 
+    static List<Position> positionList; // 좌표 정보들을 저장할 리스트
     
     public int solution(int[][] board) {
         int answer = 0;
         
-        n = board.length;
+        n = board.length; // 지도의 크기 저장
         visited = new boolean[n][n]; // [0][0] ~ [n-1][n-1]
-        positionList = new ArrayList<>();
+        positionList = new ArrayList<>(); // 좌표 정보들을 저장할 리스트 초기화
         
+        // 1. 지도에서 지뢰 찾아서 좌표 정보 리스트에 저장하는 과정
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
+                // 해당 좌표가 지뢰(1)인 경우
                 if (board[i][j] == 1) {
                     positionList.add(new Position(i, j));
-                    visited[i][j] = true;
+                    visited[i][j] = true; // 해당 좌표 방문 처리
                 }
             }
         }
         
+        // 2. 좌표 정보들을 저장한 리스트에서 지뢰와 인접한 좌표들을 위험 지역으로 표시하는 과정
         for (Position pos: positionList) {
-            mineCheck(pos.x, pos.y);
+            mineCheck(pos.x, pos.y); // 위험 지역 체크
         }
         
-        answer = dangerCountCheck();
+        answer = safeAreaCalculate(); // 안전 지역 개수 구하기
         
         return answer;
     }
     
+    // 지뢰와 인접한 좌표들을 위험 지역으로 표시하는 메서드
     public void mineCheck(int x, int y) {
+        // 8가지 방향 체크
         for (int i=0; i<8; i++) {
             int nextX = x + dx[i];
             int nextY = y + dy[i];
             
+            // 탐색한 좌표가 [0][0] ~ [n-1][n-1] 이외의 좌표인 경우
             if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= n) {
-                continue;
+                continue; // 다음 방향 탐색하게끔 넘어감
             }
             
+            // 탐색한 좌표가 이미 방문한 경우
             if (visited[nextX][nextY]) {
-                continue;
+                continue; // 다음 방향 탐색하게끔 넘어감
             }
             
-            visited[nextX][nextY] = true;
+            visited[nextX][nextY] = true; // 탐색한 좌표 방문 처리
         }
     }
     
-    public int dangerCountCheck() {
+    // 안전 지역의 개수 구하는 메서드
+    public int safeAreaCalculate() {
         int count = 0;
         
         for (int i=0; i<n; i++) {
             for (int j=0; j<n; j++) {
+                // 해당 좌표가 방문처리 되지 않은 경우 (즉, 안전한 구역인 경우)
                 if (!visited[i][j]) {
                     count++;
                 }
