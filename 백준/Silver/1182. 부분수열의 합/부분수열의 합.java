18cecd50f3776a8f385fcd4f2c1d903ad2447a7c
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-03-16 23:44:16 +0900

    [Silver II] Title: 부분수열의 합, Time: 116 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -2,12 +2,11 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
+	
 	static int N;
 	static int S;
-	static int[] arr;
-	static int[] output;
-	static int count = 0;
+	static int[] nums;
+	static int resultCount;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,42 +15,34 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		S = Integer.parseInt(st.nextToken());
 		
-		arr = new int[N];
-		output = new int[N];
+		nums = new int[N];
 		
 		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
+		for (int i=0; i<N; i++) {
+			nums[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		// 모든 조합 다 뽑아내기
-		for(int i=0; i<N; i++) {
-			dfs(0, i+1, 0);
-		}
-		System.out.println(count);
+		dfs(0, 0);
 		
+		if (S == 0) {
+			resultCount--;
+		}
+		
+		System.out.println(resultCount);
+
 	}
 	
-	// 조합 메서드
-	public static void dfs(int depth, int limit ,int idx) {
-		// 해당 깊이가 선택횟수와 같아지면 결과를 출력하고 재귀 종료
-		if(depth == limit) {
-			int sum = 0;
-			// 해당 부분수열을 다 더해줌
-			for(int i=0; i<limit; i++) {
-				sum += output[i];
-			}
-			// 부분수열 다 더했을 때 S인 경우 경우의 수 증가
-			if(sum == S) {
-				count++;
+	public static void dfs(int depth, int sum) {
+		if (depth == N) {
+			if (sum == S) {
+				resultCount++;
 			}
 			return;
 		}
 		
-		for(int i=idx; i<N; i++) {
-			output[depth] = arr[i];
-			dfs(depth+1, limit, i+1);
-		}
+		dfs(depth + 1, sum + nums[depth]);
+		
+		dfs(depth + 1, sum);
 	}
 
 }
