05e1a93196a39c3ea1697e690024ef261e8fde01
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-03-16 22:50:43 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 27.27 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,24 +2,27 @@ import java.util.*;
 
 public class Solution {
     
-    static Stack<Integer> stack;
+    static List<Integer> list; // 연속적으로 중복된 숫자를 제거한 값을 저장할 리스트
     
     public int[] solution(int[] arr) {
         int[] answer = {};
         
-        stack = new Stack<>();
-        stack.add(arr[0]);
+        list = new ArrayList<>();
+        list.add(arr[0]); // 첫 번째 숫자는 무조건 추가
         
+        // 1. 배열의 1번방부터 순회하며 이전 값과 비교하며 다른 값만 추가하는 과정
         for (int i=1; i<arr.length; i++) {
-            if (stack.peek() != arr[i]) {
-                stack.add(arr[i]);
+            // 해당 숫자가 이전 숫자와 다른 경우
+            if (arr[i] != arr[i - 1]) {
+                list.add(arr[i]); // 리스트에 추가
             }
         }
         
-        answer = new int[stack.size()];
+        // 2. 해당 리스트를 결과 배열로 변환하는 과정
+        answer = new int[list.size()]; // 결과 배열 리스트의 사이즈만큼 배열 방 개수 생성
         
-        for (int i=answer.length-1; i>=0; i--) {
-            answer[i] = stack.pop();
+        for (int i=0; i<list.size(); i++) {
+            answer[i] = list.get(i);
         }
 
         return answer;
