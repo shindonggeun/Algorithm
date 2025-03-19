298d9a8df7d9080d72a0d246b06a8d9b1ed68799
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-03-19 23:27:52 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.56 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,47 +2,54 @@ import java.util.*;
 
 class Solution {
     
-    static final int ONE_YEAR_OF_MONTH = 12;
-    static final int START_YEAR = 2000;
-    static final int MONTH_OF_DAY = 28;
-    static Map<String, Integer> map; // key: 약관 종류, value: 유효기간 달에 따른 일수 계산 (달 * 28)
-    static List<Integer> list;
+    static final int MONTHS_IN_YEAR = 12; // 1년은 12개월
+    static final int START_YEAR = 2000; // 기준이 되는 연도 (2000 <= YYYY <= 2022)
+    static final int DAYS_IN_MONTH = 28; // 모든 달은 28일까지
+    static Map<String, Integer> termMap; // key: 약관 종류, value: 유효기간 달에 따른 일수 계산 (달 * 28일)
+    static List<Integer> expiredList; // 파기해야할 개인 정보 번호 저장 리스트
     
     public int[] solution(String today, String[] terms, String[] privacies) {
         int[] answer = {};
         
-        map = new HashMap<>();
-        list = new ArrayList<>();
+        termMap = new HashMap<>();
+        expiredList = new ArrayList<>();
         
-        int nowDays = calculateDays(today);
+        int nowDays = calculateDays(today); // 오늘 날짜를 총 일수로 변환
         
+        // 1. 약관 종류에 따른 유효기간 일수를 해시맵에 저장하는 과정
         for (String term: terms) {
-            String[] termSplit = term.split(" ");
-            map.put(termSplit[0], Integer.parseInt(termSplit[1]) * MONTH_OF_DAY);
+            String[] termSplit = term.split(" "); // [0] = 약관 종류 (A, B, ...), [1] = 유효 기간
+            
+            // 해당 약관 종류에 따른 유효기간을 일(day)수로 변환하여 해시맵에 저장
+            termMap.put(termSplit[0], Integer.parseInt(termSplit[1]) * DAYS_IN_MONTH);
         }
         
+        // 2. 개인정보별 유효기간 검사하는 과정
         for (int i=0; i<privacies.length; i++) {
             String[] privacySplit = privacies[i].split(" "); // [0] = yyyy.mm.dd, [1] = 약관 종류 (A, B, ...)
             
-            int resultDays = calculateDays(privacySplit[0]);
-            int mapGetDays = map.get(privacySplit[1]);
+            int totalDays = calculateDays(privacySplit[0]); // 개인정보 수집 날짜를 총 일수로 변환
+            int validDays = termMap.get(privacySplit[1]); // 해당 약관의 유효기간을 일수로 변환
             
-            resultDays += (mapGetDays - 1);
+            totalDays += (validDays - 1); // 유효기간 계산 (마지막 보관 가능 날짜까지 포함하려면 - 1)
             
-            if (resultDays < nowDays) {
-                list.add(i + 1);
+            // 오늘 날짜가 유효기간을 넘은 경우 (파기해야 할 대상)
+            if (totalDays < nowDays) {
+                expiredList.add(i + 1); // 개인정보 번호 저장 
             }
         }
         
-        answer = new int[list.size()];
+        // 3. 해당 리스트를 결과 배열로 변환하는 과정
+        answer = new int[expiredList.size()];
         
-        for (int i=0; i<list.size(); i++) {
-            answer[i] = list.get(i);
+        for (int i=0; i<expiredList.size(); i++) {
+            answer[i] = expiredList.get(i);
         }
         
         return answer;
     }
     
+    // 날짜(YYYY.MM.DD)를 총 일수로 변환하는 메서드
     public int calculateDays(String yyyymmdd) {
         String[] yyyymmddSplit = yyyymmdd.split("[.]"); // [0] = yyyy, [1] = mm, [2] = dd
         
@@ -50,8 +57,9 @@ class Solution {
         int month = Integer.parseInt(yyyymmddSplit[1]);
         int day = Integer.parseInt(yyyymmddSplit[2]);
         
-        int days = day + month * MONTH_OF_DAY + (year - START_YEAR) * ONE_YEAR_OF_MONTH * MONTH_OF_DAY;
+        // 총 일수 = (년 - 기준연도) * 12개월 + 28일 + (월 * 28일) + 일
+        int totalDays = (year - START_YEAR) * MONTHS_IN_YEAR * DAYS_IN_MONTH + (month * DAYS_IN_MONTH) + day;
         
-        return days;
+        return totalDays;
     }
 }
