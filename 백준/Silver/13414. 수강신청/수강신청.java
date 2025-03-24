44bd53762d4bb72c8c71a5a97d2e7a9222ad6227
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-03-24 23:11:58 +0900

    [Silver III] Title: 수강신청, Time: 680 ms, Memory: 65744 KB -BaekjoonHub

--- 백준/Silver/13414. 수강신청/수강신청.java
+++ 백준/Silver/13414. 수강신청/수강신청.java
@@ -2,38 +2,41 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	
+	static int K;
+	static int L;
+	static Set<String> classRegisterSet;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int K = Integer.parseInt(st.nextToken());	// 최대 수강 인워
-		int L = Integer.parseInt(st.nextToken());	// 수강 대기목록 길이
+		K = Integer.parseInt(st.nextToken());
+		L = Integer.parseInt(st.nextToken());
 		
-		Set<String> set = new LinkedHashSet<>();	// 순서 보장된 HashSet (수강신청 대기 인원들을 담은 자료구조)
+		classRegisterSet = new LinkedHashSet<>();
 		
-		for(int i=0; i<L; i++) {
-			st = new StringTokenizer(br.readLine());
-			String classNumber = st.nextToken();
-			// 이미 수강신청한 경우 수강신청 대기명단에서 삭제한뒤 가장 뒤로 밀어남
-			if(set.contains(classNumber)) {
-				set.remove(classNumber);
+		for (int i=0; i<L; i++) {
+			String classNum = br.readLine();
+			if (classRegisterSet.contains(classNum)) {
+				classRegisterSet.remove(classNum);
 			}
-			set.add(classNumber);
+			classRegisterSet.add(classNum);
 		}
+		
+		int count = 0;
 		
 		StringBuilder sb = new StringBuilder();
-		int count = 0;	// 수강인원 카운트해주는 변수
-		for(String s: set) {
-			// 최대 수강신청 인원까지 카운트 됐으면 반복문 빠져나오게끔
-			if(count == K) {
+		for (String classNum: classRegisterSet) {
+			if (count == K) {
 				break;
 			}
-			sb.append(s).append("\n");
+			
+			sb.append(classNum).append("\n");
 			count++;
 		}
+		
 		System.out.print(sb);
-
 	}
 
 }
