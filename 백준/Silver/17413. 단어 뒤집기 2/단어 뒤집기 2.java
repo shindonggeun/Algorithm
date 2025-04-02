5902f0fc9419d075a04fc7d2ddc12cfe24e7d9dc
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-04-03 00:27:06 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 184 ms, Memory: 24124 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.java
@@ -3,8 +3,8 @@ import java.io.*;
 
 public class Main {
 
-	static String S;
-	static Stack<Character> stack;
+	static String S; // 입력 문자열
+	static Stack<Character> stack; // 단어 뒤집어서 출력하기 위해 스택 자료구조 이용
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -17,34 +17,41 @@ public class Main {
 		for (int i=0; i<S.length(); i++) {
 			char ch = S.charAt(i);
 			
+			// 해당 문자가 '<'(열린 태그)인 경우
 			if (ch == '<') {
+				// 열린 태그 전까지 쌓은 문자 뒤집어서 처리
 				while (!stack.isEmpty()) {
 					sb.append(stack.pop());
 				}
 				
+				// 해당 문자가 '>'(닫힌 태그)가 아닌 경우까지 반복
 				while (ch != '>') {
 					sb.append(ch);
-					i++;
-					ch = S.charAt(i);
+					i++; // 문자열의 다음 문자 탐색하도록 
+					ch = S.charAt(i); // 다음 문자 추출
 				}
-				sb.append(ch);
+				sb.append(ch); // '>' 닫힌 태그 출력
 			}
+			// 해당 문자가 공백인 경우
 			else if (ch == ' ') {
+				// 단어 뒤집기
 				while (!stack.isEmpty()) {
 					sb.append(stack.pop());
 				}
-				sb.append(ch);
+				sb.append(ch); // 공백 추가 
 			}
+			// 그 이외의 다른 문자인 경우
 			else {
-				stack.push(ch);
+				stack.push(ch); // 스택에 해당 문자 저장
 			}
 		}
 		
+		// 마지막 단어가 있을 경우를 대비해 스택 비우기 (단어 뒤집기)
 		while (!stack.isEmpty()) {
 			sb.append(stack.pop());
 		}
 		
-		System.out.print(sb);
+		System.out.println(sb);
 
 	}
 
