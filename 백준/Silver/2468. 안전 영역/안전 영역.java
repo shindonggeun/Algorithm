1c5ffd3059e663773f98dd7469ebb914ca314e13
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-04-25 01:24:23 +0900

    [Silver I] Title: 안전 영역, Time: 260 ms, Memory: 55888 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -4,7 +4,8 @@ import java.io.*;
 public class Main {
 	
 	static class Position {
-		int x, y;
+		int x;
+		int y;
 		
 		public Position(int x, int y) {
 			this.x = x;
@@ -15,78 +16,74 @@ public class Main {
 	static int N;
 	static int[][] map;
 	static boolean[][] visited;
-	static int maxHeight = 1;
-	static int maxSafeArea = 0;
-	
-	// 4가지 방향 배열(상, 하, 좌, 우)
-	static int[] dx = {1, -1, 0, 0};	// x축이 고정되어 있을 때 y좌표가 이동하는 방향 배열 (상, 하)
-	static int[] dy = {0, 0, -1, 1};	// y축이 고정되어 있을 때 x좌표가 이동하는 방향 배열 (좌, 우)
-	
+	static int maxHeight;
+	static int maxSafeAreaCount;
+	// 4가지 방향 배열 (상, 하, 좌, 우)
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = null;
 		
-		N = Integer.parseInt(st.nextToken());
-		map = new int[N][N];	// (0, 0) ~ (N-1, N-1)
+		N = Integer.parseInt(br.readLine());
+		map = new int[N][N]; // [0][0] ~ [N-1][N-1]
 		
-		for(int i=0; i<N; i++) {
+		maxHeight = 0;
+		maxSafeAreaCount = 0;
+		
+		for (int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<N; j++) {
+			for (int j=0; j<N; j++) {
 				map[i][j] = Integer.parseInt(st.nextToken());
 				maxHeight = Math.max(maxHeight, map[i][j]);
 			}
 		}
 		
-		// 침수높이(최대 높이)부터 시작해서 높이를 1씩 줄여나가면서 탐색해나간다
-		while(maxHeight-- > 0) {
-			visited = new boolean[N][N];
-			int tempArea = 0;
-			
-			for(int i=0; i<N; i++) {
-				for(int j=0; j<N; j++) {
-					// 방문하지 않은 좌표면서 좌표의 높이가 해당 높이보다 큰 경우인 경우(침수되지 않은 영역)
-					if(!visited[i][j] && (map[i][j] > maxHeight)) {
-						bfs(i, j);
-						tempArea++;
-					}
-				}
-			}
-			
-			maxSafeArea = Math.max(maxSafeArea, tempArea);
-		}
-		System.out.println(maxSafeArea);
+		for (int h=0; h<=maxHeight; h++) {
+		    visited = new boolean[N][N];
+		    int safeAreaCount = 0;
 
+		    for (int i=0; i<N; i++) {
+		        for (int j=0; j<N; j++) {
+		            if (!visited[i][j] && map[i][j] > h) {
+		                bfs(i, j, h);  // h 기준으로 물에 잠기는 지역 무시
+		                safeAreaCount++;
+		            }
+		        }
+		    }
+
+		    maxSafeAreaCount = Math.max(maxSafeAreaCount, safeAreaCount);
+		}
+		
+		System.out.println(maxSafeAreaCount);
 	}
 	
-	public static void bfs(int startX, int startY) {
+	public static void bfs(int startX, int startY, int height) {
 		Queue<Position> queue = new LinkedList<>();
 		queue.add(new Position(startX, startY));
 		visited[startX][startY] = true;
 		
-		while(!queue.isEmpty()) {
+		while (!queue.isEmpty()) {
 			Position now = queue.poll();
 			int nowX = now.x;
 			int nowY = now.y;
 			
-			// 4가지 방향 탐색 (상, 하, 좌, 우)
-			for(int i=0; i<4; i++) {
+			for (int i=0; i<4; i++) {
 				int nextX = nowX + dx[i];
 				int nextY = nowY + dy[i];
 				
-				// 탐색한 좌표가 (0, 0) ~ (N-1, N-1) 이외의 범위인 경우
-				if(nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) {
+				if (nextX < 0 || nextY < 0 || nextX >= N || nextY >= N) {
 					continue;
 				}
 				
-				// 탐색한 좌표가 이미 방문했거나 또는 침수되는 높이인 경우
-				if(visited[nextX][nextY] || map[nextX][nextY] <= maxHeight) {
+				if (visited[nextX][nextY] || map[nextX][nextY] <= height) {
 					continue;
 				}
 				
 				queue.add(new Position(nextX, nextY));
 				visited[nextX][nextY] = true;
 			}
-			
 		}
 	}
 
