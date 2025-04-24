c0220ab36f644dae932c2ee071cdfb48e20ceb14
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-04-25 01:07:53 +0900

    [Silver II] Title: 촌수계산, Time: 68 ms, Memory: 11652 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -3,64 +3,64 @@ import java.io.*;
 
 public class Main {
 	
-	static int N;	// 사람 수(정점의 수)
-	static int M;	// 부모 자식들간의 관계의 수 (간선의 수)
-	static ArrayList<Integer>[] arr;
+	static int N; // 전체 사람의 수
+	static int M; // 부모 자식들 간의 관계의 수
+	static List<List<Integer>> graph;
 	static boolean[] visited;
-	static int count;
-	
+	static int resultCount;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringTokenizer st = null;
 		
-		N = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(br.readLine());
+		
+		graph = new ArrayList<>();
+		visited=  new boolean[N+1];
+		resultCount = -1;
+		
+		for (int i=0; i<=N; i++) {
+			graph.add(new ArrayList<>());
+		}
+		
 		st = new StringTokenizer(br.readLine());
 		int start = Integer.parseInt(st.nextToken());
 		int end = Integer.parseInt(st.nextToken());
-		arr = new ArrayList[N+1];
-		visited = new boolean[N+1];
-		count = -1;
 		
-		for(int i=0; i<arr.length; i++) {
-			arr[i] = new ArrayList<>();
-		}
+		M = Integer.parseInt(br.readLine());
 		
-		st = new StringTokenizer(br.readLine());
-		M = Integer.parseInt(st.nextToken());
-		
-		
-		for(int i=0; i<M; i++) {
+		for (int i=0; i<M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int fromVertex = Integer.parseInt(st.nextToken());
+			int toVertex = Integer.parseInt(st.nextToken());
 			
-			arr[a].add(b);
-			arr[b].add(a);
+			graph.get(fromVertex).add(toVertex);
+			graph.get(toVertex).add(fromVertex);
 		}
 		
-		for(int i=0; i<arr.length; i++) {
-			Collections.sort(arr[i]);	// 정점에 연결된 간선들 오름차순 정렬
+		for (int i=1; i<=N; i++) {
+			Collections.sort(graph.get(i));
 		}
 		
-		dfs(start, end, 0);	// 해당 정점부터 시작, 종료정점
-		System.out.println(count);
+		dfs(0, start, end);
+
+		System.out.println(resultCount);
 	}
 	
-	public static void dfs(int index, int end, int cnt) {
-		if(index == end) {
-			count = cnt;
-			return;
+	public static void dfs(int depth, int vertex, int end) {
+		// 기저조건
+		if (vertex == end) {
+			resultCount = depth;
+			return; // 메서드 종료
 		}
-		visited[index] = true;
 		
-		// 현재 방문한 노드에 해당하는 리스트에 담겨있는 값들 하나씩 뽑아내기
-		for(int i: arr[index]) {
-			// 방문안한 노드인 경우
-			if(!visited[i]) {
-				dfs(i, end, cnt+1);	// 그 노드에 해당하는 리스트들 탐색할 수 있도록 재귀 호출
+		visited[vertex] = true;
+		
+		for (int next: graph.get(vertex)) {
+			if (!visited[next]) {
+				dfs(depth + 1, next, end);
 			}
-		}	
-		
+		}
 	}
 
 }
