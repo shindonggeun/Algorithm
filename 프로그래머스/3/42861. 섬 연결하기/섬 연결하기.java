c493f86b9680c82102ddfa92168b5bbc28bfe977
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-05-18 22:19:49 +0900

    [level 3] Title: 섬 연결하기, Time: 1.27 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -2,10 +2,11 @@ import java.util.*;
 
 class Solution {
     
+    // 간선 정보를 담은 내부 클래스
     static class Edge {
-        int fromVertex;
-        int toVertex;
-        int weight;
+        int fromVertex; // 시작 정점
+        int toVertex; // 도착 정점
+        int weight; // 가중치
         
         public Edge(int fromVertex, int toVertex, int weight) {
             this.fromVertex = fromVertex;
@@ -14,19 +15,21 @@ class Solution {
         }
     }
     
-    static List<Edge> edgeList;
-    static int[] parents;
+    static List<Edge> edgeList; // 간선들을 저장한 리스트
+    static int[] parents; // 각 정점들의 부모 정점을 저장하는 배열 (유니온 파인드) 
     
     public int solution(int n, int[][] costs) {
         int answer = 0;
         
-        edgeList = new ArrayList<>();
+        edgeList = new ArrayList<>(); // 리스트 초기화
         parents = new int[n]; // [0] ~ [n-1]
         
+        // 1. 각 정점들 초기 부모 정점은 자기 자신으로 초기화
         for (int i=0; i<n; i++) {
             parents[i] = i;
         }
         
+        // 2. 정점들간의 연결 (간선)
         for (int i=0; i<costs.length; i++) {
             int fromVertex = costs[i][0];
             int toVertex = costs[i][1];
@@ -35,21 +38,24 @@ class Solution {
             edgeList.add(new Edge(fromVertex, toVertex, weight));
         }
         
+        // 3. 크루스칼 알고리즘을 이용한 최소 신장 트리 만들기
         Collections.sort(edgeList, (a, b) -> a.weight - b.weight);
         
-        int totalMinWeight = 0;
+        int totalMinWeight = 0; // MST 비용
         for (Edge edge: edgeList) {
+            // 두 정점의 부모가 다른 경우
             if (find(edge.fromVertex) != find(edge.toVertex)) {
-                union(edge.fromVertex, edge.toVertex);
-                totalMinWeight += edge.weight;
+                union(edge.fromVertex, edge.toVertex); // 두 정점 합치기
+                totalMinWeight += edge.weight; // 최소 신장 트리 만드는데 드는 최소 비용 해당 간선의 가중치로 누적
             }
         }
         
-        answer = totalMinWeight;
+        answer = totalMinWeight; // 결과값
         
         return answer;
     }
     
+    // 유니온 파인드에서 파인드를 담당하는 메서드
     public int find(int a) {
         if (a == parents[a]) {
             return a;
@@ -57,6 +63,7 @@ class Solution {
         return parents[a] = find(parents[a]);
     }
     
+    // 유니온 파인드에서 유니온을 담당하는 메서드
     public void union(int a, int b) {
         int aRoot = find(a);
         int bRoot = find(b);
