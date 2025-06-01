f467722b8909a229c1b0f0da04b8a18c9cc6c2b0
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-06-01 23:16:36 +0900

    [Gold V] Title: 두 용액, Time: 248 ms, Memory: 31232 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -3,56 +3,48 @@ import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int[] arr = new int[N];
-		
-		st = new StringTokenizer(br.readLine());
-		for(int i=0; i<N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		// 용액들의 값들을 저장한 배열 오름차순 정렬
-		// 오름차순 정렬시 왼쪽에는 매우 낮은 음수값들이, 오른쪽에는 매우 큰 양수값들이 분포
-		// 그러므로 가운데로 갈수록 절대값은 점점 작아진다
-		// 투 포인터 알고리즘 사용하기 용이해짐
-		Arrays.sort(arr);	
-		
-		// 투 포인터 알고리즘 이용
-		int left = 0;
-		int right = arr.length - 1;
-		
-		int min = Integer.MAX_VALUE;	// 두 용액을 더했을 때 최소값
-		int sum = 0;		// 두 용액을 합한 값
-		int result1 = 0;	// 두 용액중 작은 값
-		int result2 = 0;	// 두 용액중 큰값
-		
-		while(left < right) {
-			sum = arr[left] + arr[right];	// 두 용액 더해줌
-			int abs = Math.abs(sum);
-			// 두 용액을 더한 절대값이 최소값보다 작은 경우 최소값 재할당 시켜줌
-			if(abs < min) {
-				min = abs;
-				result1 = arr[left];
-				result2 = arr[right];
-			}
-			
-			// 두 포인터가 가리키는 것들의 합이 0보다 큰 경우 
-			// 0에 한없이 가깝게 만들기 위해 오른쪽 포인터를 왼쪽으로 옮겨줌
-			if(sum > 0) {
-				right--;
-			}
-			// 두 포인터가 가리키는 것들의 합이 0보다 작거나 같은 경우 
-			// 0에 한없이 가깝게 만들기 위해 왼쪽 포인터를 오른쪽으로 옮겨줌 
-			else {
-				left++;
-			}
-		}
-		System.out.println(result1 + " " + result2);
+    static int N;
+    static int[] nums; // 용액들의 특성값들
 
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
 
+        N = Integer.parseInt(br.readLine());
+        nums = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums); // 오름차순 정렬
+
+        // 투포인터 알고리즘 이용 (양 쪽으로 좁혀오기 - 각 왼쪽, 오른쪽 인덱스 설정)
+        int left = 0;
+        int right = N - 1;
+
+        int minAbsSum = Integer.MAX_VALUE; // 두 용약을 더했을 때 최소값
+        int resultA = 0; // 두 용액 중 작은 값
+        int resultB = 0; // 두 용액 중 큰 값
+
+        // 투 포인터가 만나기 전까지 반복
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            
+            if (Math.abs(sum) < minAbsSum) {
+                minAbsSum = Math.abs(sum);
+                resultA = nums[left];
+                resultB = nums[right];
+            }
+
+            if (sum > 0) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+
+        System.out.println(resultA + " " + resultB);
+    }
 }
