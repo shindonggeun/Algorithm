c967ac5a105cc7876b1fbae77e5c330a497c9f01
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-06-15 20:52:12 +0900

    [Gold IV] Title: 공유기 설치, Time: 276 ms, Memory: 29244 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.java
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.java
@@ -2,59 +2,66 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
-	static int N;	// 집의 개수
-	static int C;	// 설치해야 할 공유기의 개수
-	static int[] houseArr;	// 각 집에 위치를 저장할 배열
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		
-		houseArr = new int[N];	
-		
-		for(int i=0; i<N; i++) {
-			houseArr[i] = Integer.parseInt(br.readLine());
-		}
-		
-		Arrays.sort(houseArr);	// 집의 위치 x 좌표 오름차순 정렬
-		
-		// 이분탐색 알고리즘 이용
-		int low = 1;	// 가능한 최소 거리
-		int high = houseArr[N-1] - houseArr[0];	// 가능한 최대 거리
-		int result = 0;	// 결과값
-		
-		// 이분탐색 실시
-		while(low <= high) {
-			int mid = (low + high) / 2;	// 중간 거리 계산
-			int install = 1;	// 첫 번째 집에 공유기 설치
-			int lastInstall = houseArr[0];	// 마지막으로 설치한 집의 위치
-			
-			// 공유기 설치 가능 여부를 탐색 (두번재 집부터 탐색)
-			for(int i=1; i<N; i++) {
-				// 현재 집과 마지막으로 설치한 집 사이의 거리가 mid 이상인 경우
-				if(houseArr[i] - lastInstall >= mid) {
-					install++;	// 공유기 설치
-					lastInstall = houseArr[i];	// 마지막으로 설치한 위치 업데이트
-				}
-			}
-			
-			// 필요한 개수 이상의 공유기를 설치할 수 있는 경우
-			if(install >= C) {
-				result = mid;	// 결과값 갱신
-				low = mid + 1;	// 탐색 범위 좁히기 (하한 증가)
-			}
-			// 필요한 개수 이상의 공유기 설치할 수 없는 경우
-			else {
-				high = mid - 1;	// 탐색 범위 좁히기 (상한 감소)
-			}
-		}
-		
-		System.out.println(result);
+    static int N;
+    static int C;
+    static int[] housePositions;
 
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        N = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+
+        housePositions = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            housePositions[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(housePositions);
+
+        int result = binarySearch();
+        System.out.println(result);
+    }
+
+    // 이분 탐색 알고리즘을 통해 가장 인접한 두 공유기 사이의 최대 거리를 구하는 메서드
+    public static int binarySearch() {
+        int start = 1; // 시작점 (가능한 최소 거리)
+        int end = housePositions[N - 1] - housePositions[0]; // 끝점 (가능한 최대 거리: 마지막 집의 좌표 - 첫 시작 집의 좌표)
+        int result = 0; // 결과값 (가장 인접한 두 공유기 사이의 최대 거리)
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            // 가능한 거리로 공유기를 설치할 수 있는지 확인
+            // 해당 거리로 공유기 C대 이상 설치 가능한 경우
+            if (checkWifiInstall(mid) >= C) {
+                start = mid + 1;
+                result = mid;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    // 해당 거리를 통해 공유기를 설치할 수 있는지 확인하는 메서드
+    public static int checkWifiInstall(int distance) {
+        int count = 1; // 공유기 설치 가능한 개수 (첫 번째 집에 공유기 설치하므로 일단 개수 1)
+        int lastHouse = housePositions[0]; // 마지막으로 공유기를 설치한 집의 위치 (첫 번째 집의 위치로 초기화)
+
+        // 두 번째 집의 위치부터 탐색 시작
+        for (int i = 1; i < N; i++) {
+            // 해당 집과 마지막으로 설치한 집 사이의 거리가 주어진 거리보다 크거나 같은 경우
+            if (distance <= housePositions[i] - lastHouse) {
+                count++; // 공유기 설치 가능한 개수 증가
+                lastHouse = housePositions[i]; // 마지막으로 공유기를 설치한 집의 위치 갱신
+            }
+        }
+
+        return count;
+    }
 }
