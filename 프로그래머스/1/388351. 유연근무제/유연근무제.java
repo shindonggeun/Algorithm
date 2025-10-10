5ec9a7b1e967f1c9ebbf7d909d658f12f8494e36
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-10-10 15:50:47 +0900

    [level 1] Title: 유연근무제, Time: 1.43 ms, Memory: 80.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int solution(int[] schedules, int[][] timelogs, int startday) {
+        int answer = 0;
+        
+        int humanCount = schedules.length;
+        
+        for (int i = 0; i < humanCount; i++) {
+            int deadline = addMinutes(schedules[i], 10);
+            boolean allOnTime = true;
+            
+            for (int d = 0; d < 7; d++) {
+                int dayOfWeek = getDayOfWeek(startday, d);
+                
+                if (!isWeekday(dayOfWeek)) {
+                    continue;
+                }
+                
+                if (timelogs[i][d] > deadline) {
+                    allOnTime = false;
+                    break;
+                }
+            }
+            
+            if (allOnTime) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    private int addMinutes(int time, int minutes) {
+        int hour = time / 100;
+        int min = time % 100;
+        
+        min += minutes;
+        if (min >= 60) {
+            hour += min / 60;
+            min = min % 60;
+        }
+        
+        return hour * 100 + min;
+    }
+    
+    private boolean isWeekday(int day) {
+        return day >= 1 && day <= 5;
+    }
+    
+    private int getDayOfWeek(int startday, int dayIndex) {
+        int day = (startday + dayIndex - 1) % 7 + 1;
+        return day;
+    }
+}
