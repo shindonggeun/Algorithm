0c6c64d870a3a6899e68cd4959bbde20b6c07aed
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-11-01 15:52:23 +0900

    [Gold V] Title: ABCDE, Time: 180 ms, Memory: 18860 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -2,79 +2,65 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
-	static int N;	// 사람의 수 (정점의 수)
-	static int M;	// 친구 관계의 수 (간선의 수)
-	static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-	static boolean[] visited;	// 방문배열
-	static boolean friendRelation;	// 친구관계가 존재하는지 여부를 판단해주는 변수
-	
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		// 그래프에서 정점(친구들) 만들어주기
-		for(int i=0; i<N; i++) {
-			graph.add(new ArrayList<>());
-		}
-	
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int fromVertex = Integer.parseInt(st.nextToken());
-			int toVertex = Integer.parseInt(st.nextToken());
-			
-			// 친구 관계 연결해주기 (양방향 간선)
-			graph.get(fromVertex).add(toVertex);
-			graph.get(toVertex).add(fromVertex);
-		}
-		friendRelation = false;	
-		
-		// 친구번호 0번부터 N-1번까지 탐색
-		for(int i=0; i<N; i++) {
-			visited = new boolean[N];	// 방문배열 초기화
-			dfs(0, i);	// 시작정점부터 깊이우선탐색 시작 (depth는 0)
-		}
-		
-		// 친구관계 성립하면
-		if(friendRelation) {
-			System.out.println(1);	
-		}
-		// 친구관계 성립 안되면
-		else {
-			System.out.println(0);
-		}
-		
-	}
-	
-	// 깊이우선탐색 메서드
-	public static void dfs(int depth, int vertex) {
-		visited[vertex] = true;	// 해당 정점(친구) 방문 처리
-		
-		// 깊이가 4가 된 경우 (즉, 문제에서 주어진 친구관계 성립 된 경우) (종료 조건)
-		if(depth == 4) {
-			friendRelation = true;	// 친구관계 찾음
-			return;	// 메서드 종료
-		}
-		
-		// 친구관계 찾았으면 더이상 탐색할 필요도 없으므로
-		if(friendRelation) {
-			return;	// 메서드 종료
-		}
-		
-		for(int i: graph.get(vertex)) {
-			// 해당 정점이 방문처리 안된 경우
-			if(!visited[i]) {
-				visited[i] = true;	// 방문처리 해준 뒤
-				dfs(depth+1, i);	// 깊이우선탐색 실시
-				visited[i] = false;	// 방문처리 해제 (백트래킹)
-			}
-		}
-		
-		
-	}
 
+    static int N;
+    static int M;
+    static List<List<Integer>> graph; // 각 친구 (노드)마다 친구 관계 (연결 관계)를 나타내는 그래프
+    static boolean[] visited; // 각 친구 (노드)의 방문 여부를 나타내는 배열
+    static boolean isFind; // 친구관계 A, B, C, D, E를 찾았는지 여부
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList<>();
+        visited = new boolean[N + 1];
+        isFind = false;
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            // 친구 관계 연결 (양방향 간선)
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+
+        // 모든 사람들 자기 자신부터 시작해서 탐색
+        for (int i = 1; i <= N; i++) {
+            // 이미 해당 친구 관계 A, B, C, D, E 찾은 경우 탐색 종료
+            if (isFind) {
+                break;
+            }
+            dfs(i, 1); // 친구 관계 1부터 시작
+        }
+
+        System.out.println(isFind ? 1 : 0);
+    }
+
+    private static void dfs(int now, int depth) {
+        // 현재 친구관계가 5 (A, B, C, D, E 존재)하는 경우 (기저 조건)
+        if (depth == 5) {
+            isFind = true;
+            return;
+        }
+
+        visited[now] = true;
+
+        for (int next : graph.get(now)) {
+            if (!visited[next]) {
+                dfs(next, depth + 1);
+            }
+        }
+
+        visited[now] = false;
+    }
 }
