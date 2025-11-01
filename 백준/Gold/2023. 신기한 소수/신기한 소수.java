1b2b66450cc7540d77df41efa7a9d20a6b9a48c9
동긔 <42789808+shindonggeun@users.noreply.github.com>
2025-11-01 15:31:05 +0900

    [Gold V] Title: 신기한 소수, Time: 68 ms, Memory: 11508 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -1,47 +1,64 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
 
-	static int N;
-	static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		backTracking(0, 0);
-		System.out.print(sb);
-	}
-	
-	public static boolean isPrime(int num) {
-		// 숫자 2 미만인거는 소수 아님
-		if(num < 2) {
-			return false;
-		}
-		// 소수인지 아닌지 판별하는 과정
-		for(int i=2; i*i<=num; i++) {
-			if(num % i == 0) {
-				return false;	// num이 i의 배수인 경우 소수 아니므로 false
-			}
-		}
-		return true;
-	}
-	
-	// 백트래킹 
-	public static void backTracking(int num, int idx) {
-		// N자리수인 경우 (해당 깊이까지 도달한 경우)
-		if(idx == N) {
-			sb.append(num).append("\n");
-		}
-		
-		// 각 자리수마다 1부터 9까지 올수 있으므로 반복문 1 ~ 9까지 설정
-		for(int i=1; i<10; i++) {
-			int temp = 10 * num + i;
-			// 소수인 경우
-			if(idx >= 0 && isPrime(temp)) {
-				backTracking(temp, idx+1);	// 재귀함수 호출 (해당 깊이 1 증가)
-			}
-		}
-	}
+    static int N;
+    static StringBuilder sb;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        sb = new StringBuilder();
+
+        // 소수 2, 3, 5, 7 시작
+        dfs(2, 1);
+        dfs(3, 1);
+        dfs(5, 1);
+        dfs(7, 1);
+
+        System.out.print(sb);
+    }
+
+    private static void dfs(int num, int jarisu) {
+        // 해당 자리수가 N자리수인 경우 (기저 조건)
+        if (jarisu == N) {
+            // 해당 수가 소수인 경우 출력
+            if (isPrime(num)) {
+                sb.append(num).append("\n");
+            }
+            return;
+        }
+
+        // 각 자리수마다 1부터 9까지 올 수 있음
+        for (int i = 1; i < 10; i++) {
+            // 짝수인 경우 더 이상 탐색 할 필요 없으므로 스킵
+            if (i % 2 == 0) {
+                continue;
+            }
+
+            int next = num * 10 + i;
+
+            // 소수인 경우 재귀 함수로 자리수 늘림
+            if (isPrime(next)) {
+                dfs(next, jarisu + 1);
+            }
+        }
+    }
+
+    private static boolean isPrime(int num) {
+        // 숫자 2 미만은 소수 아님
+        if (num < 2) {
+            return false;
+        }
+
+        // 소우인지 아닌지 판별하는 과정
+        for (int i = 2; i * i <= num; i++) {
+            // num이 i의 배수인 경우 소수 아님
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
